75d49d8c3f1fb78f5dcc55c3d7fde4fbd8f00083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:09 +0000

    Add package acdh-cidoc-pyutils/0.9/acdh_cidoc_pyutils-0.9-py3-none-any.whl

---
+++ packages/acdh-cidoc-pyutils/acdh_cidoc_pyutils-0.9-py3-none-any.whl/acdh_cidoc_pyutils/namespaces.py
@@ -0,0 +1,9 @@
+from rdflib import Namespace
+
+CIDOC = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+FRBROO = Namespace("http://iflastandards.info/ns/fr/frbr/frbroo#")
+
+NSMAP = {
+    "tei": "http://www.tei-c.org/ns/1.0",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+}
