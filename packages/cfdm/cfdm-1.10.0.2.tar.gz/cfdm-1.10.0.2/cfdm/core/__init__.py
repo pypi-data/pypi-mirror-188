67050c6ceaf56db95866e9378d25f3d794ae440c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package cfdm/1.10.0.2/cfdm-1.10.0.2.tar.gz

---
+++ packages/cfdm/cfdm-1.10.0.2.tar.gz/cfdm-1.10.0.2/cfdm/core/__init__.py
@@ -0,0 +1,80 @@
+""""""
+
+__date__ = "2023-01-26"
+__cf_version__ = "1.10"
+__version__ = "1.10.0.2"
+
+from packaging.version import Version
+import platform
+
+_requires = ("numpy", "netCDF4")
+
+_error0 = f"cfdm.core requires the modules {', '.join(_requires)}. "
+
+try:
+    import netCDF4
+except ImportError as error1:
+    raise ImportError(_error0 + str(error1))
+
+try:
+    import numpy
+except ImportError as error1:
+    raise ImportError(_error0 + str(error1))
+
+# Check the version of python
+_minimum_vn = "3.7.0"
+if Version(platform.python_version()) < Version(_minimum_vn):
+    raise ValueError(
+        f"Bad python version: cfdm.core requires python>={_minimum_vn}. "
+        f"Got {platform.python_version()}"
+    )
+
+# Check the version of netCDF4
+_minimum_vn = "1.5.4"
+if Version(netCDF4.__version__) < Version(_minimum_vn):
+    raise ValueError(
+        f"Bad netCDF4 version: cfdm.core requires netCDF4>={_minimum_vn}. "
+        f"Got {netCDF4.__version__} at {netCDF4.__file__}"
+    )
+
+# Check the version of numpy
+_minimum_vn = "1.15"
+if Version(numpy.__version__) < Version(_minimum_vn):
+    raise ValueError(
+        f"Bad numpy version: cfdm.core requires numpy>={_minimum_vn}. "
+        f"Got {numpy.__version__} at {numpy.__file__}"
+    )
+
+from .constructs import Constructs
+
+from .functions import CF, environment
+
+from .data import Data, Array, NumpyArray
+
+from .bounds import Bounds
+from .coordinateconversion import CoordinateConversion
+from .datum import Datum
+from .domain import Domain
+from .interiorring import InteriorRing
+
+from .auxiliarycoordinate import AuxiliaryCoordinate
+from .cellmeasure import CellMeasure
+from .cellmethod import CellMethod
+from .coordinatereference import CoordinateReference
+from .dimensioncoordinate import DimensionCoordinate
+from .domainancillary import DomainAncillary
+from .domainaxis import DomainAxis
+from .field import Field
+from .fieldancillary import FieldAncillary
+
+from .abstract import (
+    Container,
+    Properties,
+    PropertiesData,
+    PropertiesDataBounds,
+    Coordinate,
+    Parameters,
+    ParametersDomainAncillaries,
+)
+
+from .meta import DocstringRewriteMeta
