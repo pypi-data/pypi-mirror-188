39e5740e559bee6248f11ef5e6b6c022b4345bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:49 +0000

    Add package amasel/2.3.7.1/amasel-2.3.7.1-py3-none-any.whl

---
+++ packages/amasel/amasel-2.3.7.1-py3-none-any.whl/amasel/extract.py
@@ -0,0 +1,14 @@
+import time
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+
+def scrape(link):
+    url = link
+    driver_path = "./chromedriver.exe"
+    browser = Chrome(executable_path = driver_path)
+    browser.get(url)
+    data = browser.find_element(By.ID,"aplus_feature_div")
+    data = data.text
+    data = data.split("\n")
+    time.sleep(1)
+    return data
