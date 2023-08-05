a0d320ff031e893e1f3fd0b76020d2d0c28f0c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:35 +0000

    Add package dfkv/0.0.2/dfkv-0.0.2.tar.gz

---
+++ packages/dfkv/dfkv-0.0.2.tar.gz/dfkv-0.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from io import open
+from setuptools import setup, find_packages
+setup(
+    name='dfkv',
+    version='0.0.2',
+    description='a expand for dict',
+    long_description='expand dict, can use a.b, can set value more than 1-level a.b.c.d="xyz"',
+    author='fred deng',
+    author_email='dfgeoff@qq.com',
+    license='Apache License 2.0',
+    url='https://gitee.com/hifong45/dkv.git',
+    download_url='https://gitee.com/hifong45/dkv/master.zip',
+    packages=find_packages(),
+    install_requires=[]
+)
