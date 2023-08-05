30e5676d00bcf30a054c74e6f805355df582c711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:00 +0000

    Add package looqbox/3.12.2/looqbox-3.12.2-py3-none-any.whl

---
+++ packages/looqbox/looqbox-3.12.2-py3-none-any.whl/looqbox/objects/api.py
@@ -0,0 +1,31 @@
+from looqbox.objects.visual.looq_image import ObjImage
+from looqbox.objects.media.looq_video import ObjVideo
+from looqbox.objects.visual.looq_list import ObjList
+from looqbox.objects.looq_html import ObjHTML
+from looqbox.objects.looq_pdf import ObjPDF
+from looqbox.objects.visual.looq_message import ObjMessage
+from looqbox.objects.looq_file_upload import ObjFileUpload
+from looqbox.objects.visual.looq_web_frame import ObjWebFrame
+from looqbox.objects.visual.looq_table import ObjTable
+from looqbox.objects.plotly.looq_plotly import ObjPlotly
+from looqbox.objects.looq_form import ObjForm
+from looqbox.objects.looq_image_capture import ObjImageCapture
+from looqbox.objects.looq_embed import ObjEmbed
+from looqbox.objects.media.looq_audio import ObjAudio
+from looqbox.objects.looq_simple import ObjSimple
+from looqbox.objects.looq_form_html import ObjFormHTML
+from looqbox.objects.visual.looq_query import ObjQuery
+from looqbox.objects.container.positional.looq_row import ObjRow
+from looqbox.objects.container.positional.looq_column import ObjColumn
+from looqbox.objects.container.looq_switch import ObjSwitch
+from looqbox.objects.container.looq_tooltip import ObjTooltip
+from looqbox.objects.container.looq_link import ObjLink
+from looqbox.objects.visual.looq_text import ObjText
+from looqbox.objects.visual.looq_gauge import ObjGauge
+from looqbox.objects.visual.shape.looq_line import ObjLine
+from looqbox.objects.component_utility.css_option import CssOption
+
+__all__ = ["ObjTable", "ObjWebFrame", "ObjPlotly", "ObjPDF", "ObjList", "ObjHTML", "ObjImage",
+           "ObjMessage", "ObjVideo", "ObjForm", "ObjFileUpload", "ObjImageCapture", "ObjEmbed",
+           "ObjAudio", "ObjSimple", "ObjFormHTML", "ObjQuery", "ObjRow", "ObjColumn", "ObjSwitch",
+           "ObjTooltip", "ObjLink", "ObjText", "ObjGauge", "ObjLine", "CssOption"]
