fdf6860f016de865c6ed007a036693723c9fdf67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:05 +0000

    Add package mage-ai/0.7.78/mage_ai-0.7.78-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.7.78-py3-none-any.whl/mage_ai/io/file.py
@@ -0,0 +1,59 @@
+from mage_ai.io.base import BaseFile, FileFormat
+from pandas import DataFrame
+from typing import Union
+import os
+
+
+class FileIO(BaseFile):
+    """
+    Handles data transfer between the filesystem and the Mage app.
+    """
+
+    def load(
+        self,
+        filepath: Union[str, os.PathLike],
+        format: Union[FileFormat, str, None] = None,
+        **kwargs,
+    ) -> DataFrame:
+        """
+        Loads the data frame from the filepath specified.
+
+        Args:
+            filepath (os.PathLike): Filepath to load data frame from.
+            format (Union[FileFormat, str], Optional): Format of the file to load data frame from.
+            Defaults to None, in which case the format is inferred.
+
+        Returns:
+            DataFrame: Data frame object loaded from the specified data frame.
+        """
+        if format is None:
+            format = self._get_file_format(filepath)
+        with self.printer.print_msg(f'Loading data frame from \'{filepath}\''):
+            return self._read(filepath, format, **kwargs)
+
+    def export(
+        self,
+        df: DataFrame,
+        filepath: Union[str, os.PathLike],
+        format: Union[FileFormat, str, None] = None,
+        **kwargs,
+    ) -> None:
+        """
+        Exports the input dataframe to the file specified.
+
+        Args:
+            df (DataFrame): Data frame to export.
+            filepath (os.PathLike): Filepath to export data frame to.
+            format (Union[FileFormat, str], Optional): Format of the file to export data frame to.
+            Defaults to None, in which case the format is inferred.
+        """
+        if format is None:
+            format = self._get_file_format(filepath)
+        with self.printer.print_msg(f'Exporting data frame to \'{filepath}\''):
+            self._write(df, format, filepath, **kwargs)
+
+    def exists(
+        self,
+        filepath: Union[os.PathLike, str],
+    ) -> bool:
+        return os.path.exists(filepath)
