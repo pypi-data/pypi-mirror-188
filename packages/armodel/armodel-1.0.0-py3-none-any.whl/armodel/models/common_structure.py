4a13a32f929476fbab8259812fdbf406c994b6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:49 +0000

    Add package armodel/1.0.0/armodel-1.0.0-py3-none-any.whl

---
+++ packages/armodel/armodel-1.0.0-py3-none-any.whl/armodel/models/common_structure.py
@@ -0,0 +1,126 @@
+#from __future__ import annotations
+
+from abc import ABCMeta
+from typing import List
+
+from .general_structure import ARObject, ARElement, Identifiable
+from .data_dictionary import SwDataDefProps
+from .ar_ref import RefType
+
+class ValueSpecification(ARObject, metaclass=ABCMeta):
+    def __init__(self):
+        if type(self) == ValueSpecification:
+            raise NotImplementedError("ValueSpecification is an abstract class.")
+        super().__init__()
+
+        self.short_label = None
+
+
+class ConstantSpecification(ARElement):
+    def __init__(self, parent, short_name):
+        super().__init__(parent, short_name)
+
+        self.value_spec = None  # type: ValueSpecification
+
+
+class ConstantReference(ValueSpecification):
+    def __init__(self):
+        super().__init__()
+
+        self.constant_ref = None
+
+
+class AbstractImplementationDataTypeElement(Identifiable):
+    def __init__(self, parent, short_name: str):
+        super().__init__(parent, short_name)
+
+
+class ImplementationDataTypeElement(AbstractImplementationDataTypeElement):
+    ARRAY_SIZE_SEMANTICS_FIXED_SIZE = "FIXED-SIZE"
+    ARRAY_SIZE_SEMANTICS_VARIABLE_SIZE = "VARIABLE_SIZE"
+
+    def __init__(self, parent, short_name: str):
+        super().__init__(parent, short_name)
+
+        self.array_size = None    # type: int
+        self.array_size_semantics = None  # type: str
+        self.is_optional = None    # type: bool
+        self.sw_data_def_props = None    # type: SwDataDefProps
+
+    
+    def createImplementationDataTypeElement(self, short_name: str): # type: (...) -> ImplementationDataTypeElement
+        if (short_name not in self.elements):
+            event = ImplementationDataTypeElement(self, short_name)
+            self.elements[short_name] = event
+        return self.elements[short_name]
+    
+    def getImplementationDataTypeElements(self):                    # type：(...) -> List[ImplementationDataTypeElement]:
+        return list(filter(lambda c: isinstance(c, ImplementationDataTypeElement), self.elements.values()))
+
+class ExclusiveArea(Identifiable):
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+
+class InternalBehavior(Identifiable, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == InternalBehavior:
+            raise NotImplementedError("InternalBehavior is an abstract class.")
+        super().__init__(parent, short_name)
+
+        self._data_type_mapping_refs = []      # type: List[RefType]
+
+    def addDataTypeMappingRef(self, ref: RefType):
+        self._data_type_mapping_refs.append(ref)
+
+    def getDataTypeMappingRefs(self) -> List[RefType]:
+        return self._data_type_mapping_refs
+
+    def createExclusiveArea(self, short_name: str) -> ExclusiveArea:
+        if (short_name not in self.elements):
+            event = ExclusiveArea(self, short_name)
+            self.elements[short_name] = event
+        return self.elements[short_name]
+
+    def getExclusiveAreas(self) -> List[ExclusiveArea]:
+        return list(filter(lambda c: isinstance(c, ExclusiveArea), self.elements.values()))
+
+class ExecutableEntity(Identifiable, metaclass=ABCMeta):
+    def __init__(self, parent: ARObject, short_name: str):
+        if type(self) == ExecutableEntity:
+            raise NotImplementedError("ExecutableEntity is an abstract class.")
+        super().__init__(parent, short_name)
+    
+        self.activation_reason = None           # *
+        self.minimum_start_interval = 0.0       # 0..1
+        self.reentrancy_level = None            # 
+        self.can_enter_exclusive_area_refs = [] # type: List[RefType]  
+
+    @property
+    def minimum_start_interval_ms(self) -> int:
+        return int(self.minimum_start_interval * 1000)
+
+    def addCanEnterExclusiveAreaRef(self, ref: RefType):
+        self.can_enter_exclusive_area_refs.append(ref)
+
+    def getCanEnterExclusiveAreaRefs(self):
+        return self.can_enter_exclusive_area_refs
+    
+class ModeDeclarationGroupPrototype(Identifiable):
+    """
+    The ModeDeclarationGroupPrototype specifies a set of Modes (ModeDeclarationGroup) which is provided or required in the given context.
+    """
+
+    def __init__(self, parent: ARObject, short_name: str):
+        super().__init__(parent, short_name)
+        self._sw_calibration_access = ""    # 0..1
+        self.type_tref = None                    # tref 0..1
+
+    @property
+    def sw_calibration_access(self):
+        return self._sw_calibration_access
+
+    @sw_calibration_access.setter
+    def sw_calibration_access(self, value):
+        if (value not in ("notAccessible", "readOnly", "readWrite")):
+            raise ValueError("Invalid SwCalibrationAccess <%s> of ModeDeclarationGroupPrototype <%s>" % (value, self.short_name))
+        self._sw_calibration_access = value
