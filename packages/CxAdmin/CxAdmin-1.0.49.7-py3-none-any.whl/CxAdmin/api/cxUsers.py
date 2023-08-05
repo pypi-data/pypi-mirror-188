6e5ec9c22267092ad01aa3cfb0c746b545a2b2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:33 +0000

    Add package CxAdmin/1.0.49.7/CxAdmin-1.0.49.7-py3-none-any.whl

---
+++ packages/CxAdmin/CxAdmin-1.0.49.7-py3-none-any.whl/CxAdmin/api/cxUsers.py
@@ -0,0 +1,15 @@
+from typing import Any
+from src.CxAdmin.api.cxItem import CxItem
+from src.CxAdmin.objects import CxUser
+
+
+class CxUsers(CxItem):
+    def getAllUsers(self) -> list[CxUser]:
+        usersJson: list[dict[str, Any]] = self._httpClient.get(self._path).json()[
+            "result"
+        ]
+        users = [CxUser.from_json(userJson) for userJson in usersJson]
+        return users
+
+    def get(self) -> Any:
+        raise NotImplementedError()
