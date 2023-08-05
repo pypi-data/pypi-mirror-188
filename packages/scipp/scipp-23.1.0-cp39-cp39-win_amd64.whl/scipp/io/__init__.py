bb57f097365ccdd1125e4f58fc3abf24c1a26f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:12 +0000

    Add package scipp/23.1.0/scipp-23.1.0-cp39-cp39-win_amd64.whl

---
+++ packages/scipp/scipp-23.1.0-cp39-cp39-win_amd64.whl/scipp/io/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @file
+# @author Simon Heybrock
+
+# flake8: noqa
+
+from .hdf5 import open_hdf5
