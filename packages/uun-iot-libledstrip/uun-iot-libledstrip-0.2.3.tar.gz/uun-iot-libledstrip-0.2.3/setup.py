264d56caa06d937206b8e959b6699c3e3a99ebb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:24 +0000

    Add package uun-iot-libledstrip/0.2.3/uun-iot-libledstrip-0.2.3.tar.gz

---
+++ packages/uun-iot-libledstrip/uun-iot-libledstrip-0.2.3.tar.gz/uun-iot-libledstrip-0.2.3/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+import shutil
+import os
+import re
+
+version = "0.2.3"
+name = "uun-iot-libledstrip"
+name_=name.replace('-', '_')
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author="(UUN) Tomáš Faikl",
+    author_email="tomas.faikl@unicornuniversity.net",
+    description="Library for managing LED strips for infographic purposes.",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        ## default: [dev]
+        #"colored",
+    ],
+    extras_require={
+        "neopixel": [
+             "rpi_ws281x",
+             "adafruit-circuitpython-neopixel",
+             "adafruit-blinka"
+         ],
+        "gpio": [
+            "RPi.GPIO"
+        ],
+        "i2c": [
+            "smbus2"
+        ],
+        "dev": [
+            "colored",
+        ]
+    },
+    package_data={
+        name_: ["data/*"]
+    }
+)
