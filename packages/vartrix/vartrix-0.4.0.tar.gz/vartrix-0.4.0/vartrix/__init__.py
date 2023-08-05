64b2f8f7ca4e04b886c04c71a07534a7b0ed9963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:13 +0000

    Add package vartrix/0.4.0/vartrix-0.4.0.tar.gz

---
+++ packages/vartrix/vartrix-0.4.0.tar.gz/vartrix-0.4.0/vartrix/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 16:12:03 2019
+
+@author: Reuben
+"""
+
+from . import aliases
+from . import utils
+from . import settings
+from . import persist
+from . import container
+from . import automate
+from . import namespace
+
+
+from .persist import load, save
+from .namespace import Name_Space, get_container
+from .container import Container
+from .automate import Automator
+from .aliases import Aliases
+from .utils import Factory, Simple_Factory
