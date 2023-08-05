5fd633ffda315a05d9953611167c3e5417d90c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:04 +0000

    Add package Aritmeticas/1.0/Aritmeticas-1.0.tar.gz

---
+++ packages/Aritmeticas/Aritmeticas-1.0.tar.gz/Aritmeticas-1.0/Mate/__init__.py
@@ -0,0 +1,2 @@
+
+print("cargando atirmetica")
---
+++ packages/Aritmeticas/Aritmeticas-1.0.tar.gz/Aritmeticas-1.0/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup
+
+setup(
+    name =  "Aritmeticas",
+    version = '1.0',
+    description = "Paquete que calcula funciones aritmaticas",
+    author  = "Martin Aguilar",
+    author_email="martin@gmail.com",
+    url='https://www.hektor.dev',
+    packages = [ 'Mate']  ,
+    scripts =  [ "test.py"]    
+)
+
