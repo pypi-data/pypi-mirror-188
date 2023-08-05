0f7bc974c17f8037c1d594540d7f30e11760d807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:01 +0000

    Add package middleware-apm/0.1.18/middleware-apm-0.1.18.tar.gz

---
+++ packages/middleware-apm/middleware-apm-0.1.18.tar.gz/middleware-apm-0.1.18/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+
+    name="middleware-apm",
+    version="0.1.18",
+
+    author="middleware-dev",
+    description="This package is use to check the RAM and CPU Usage of Current Device.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/plain',
+    packages=["apmpythonpackage"],
+    url = "https://github.com/middleware-labs/agent-apm-python.git",
+)
