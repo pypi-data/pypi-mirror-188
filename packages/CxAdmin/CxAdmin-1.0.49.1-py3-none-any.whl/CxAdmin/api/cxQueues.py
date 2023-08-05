12dfe90cd22386f02a5764f0d5cbe76c7385006a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:38 +0000

    Add package CxAdmin/1.0.49.1/CxAdmin-1.0.49.1-py3-none-any.whl

---
+++ packages/CxAdmin/CxAdmin-1.0.49.1-py3-none-any.whl/CxAdmin/api/cxQueues.py
@@ -0,0 +1,15 @@
+from typing import Any
+from CxAdmin.api.cxItem import CxItem
+from CxAdmin.objects.cxQueue import CxQueue
+
+
+class CxQueues(CxItem):
+    def getQueues(self) -> list[CxQueue]:
+        queuesJson: list[dict[str, Any]] = self._httpClient.get(self._path).json()[
+            "result"
+        ]
+        queues = [CxQueue.from_json(queueJson) for queueJson in queuesJson]
+        return queues
+
+    def get(self) -> Any:
+        raise NotImplementedError()
