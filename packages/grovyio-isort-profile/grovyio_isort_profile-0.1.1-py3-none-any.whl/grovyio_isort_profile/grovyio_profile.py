54b18c4f31e559c94fbc3d0e72ef3035e8c157a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:09 +0000

    Add package grovyio-isort-profile/0.1.1/grovyio_isort_profile-0.1.1-py3-none-any.whl

---
+++ packages/grovyio-isort-profile/grovyio_isort_profile-0.1.1-py3-none-any.whl/grovyio_isort_profile/grovyio_profile.py
@@ -0,0 +1,14 @@
+PROFILE = {
+    "multi_line_output": 3,
+    "include_trailing_comma": True,
+    "force_grid_wrap": 0,
+    "use_parentheses": True,
+    "ensure_newline_before_comments": True,
+    "split_on_trailing_comma": True,
+    "force_single_line": True,
+    "atomic": True,
+    "lines_after_imports": 2,
+    "lines_between_types": 1,
+    "filter_files": True,
+    "line_length": 120,
+}
