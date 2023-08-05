bd5ff32f3e005c351449272c2b58195408c320de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:39 +0000

    Add package simplePacking/1.0.1/simplePacking-1.0.1.tar.gz

---
+++ packages/simplePacking/simplePacking-1.0.1.tar.gz/simplePacking-1.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='simplePacking',
+    packages=['simplePacking'],
+    version='1.0.1',
+    license='MIT',
+    python_requires='>=3.6',
+    description='Simple Python packing example, calling to flask service to execute',
+    author="Noy Sartori"
+)
