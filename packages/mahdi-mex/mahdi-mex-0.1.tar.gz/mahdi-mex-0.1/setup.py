480c885d2ade9ffdf330535b68e3c627b3a6572d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:06 +0000

    Add package mahdi-mex/0.1/mahdi-mex-0.1.tar.gz

---
+++ packages/mahdi-mex/mahdi-mex-0.1.tar.gz/mahdi-mex-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="mahdi-mex",
+version='0.1',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['mahdi'],
+install_requires=[])
