0b909b890756fe97fcd5bddc77905b8282b20593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:19 +0000

    Add package tuxrun/0.34.0/tuxrun-0.34.0.tar.gz

---
+++ packages/tuxrun/tuxrun-0.34.0.tar.gz/tuxrun-0.34.0/tuxrun/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# vim: set ts=4
+#
+# Copyright 2021-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Command line tool for testing Linux under QEMU
+"""
+__version__ = "0.34.0"
