9146687afddfa80adafbf8359185d0e087fe86f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:51 +0000

    Add package voicevox-client/0.1.1/voicevox-client-0.1.1.tar.gz

---
+++ packages/voicevox-client/voicevox-client-0.1.1.tar.gz/voicevox-client-0.1.1/voicevox/client.py
@@ -0,0 +1,122 @@
+# voicevox - Client
+
+from typing import Optional, List
+from typing_extensions import Self
+
+from .audio_query import AudioQuery
+from .http import HttpClient
+from .speakers import Speaker
+
+
+class Client:
+    """Voicevox client class
+
+    Wrap voicevox engine api.
+
+    Parameters
+    ----------
+    base_url: str
+        Voicevox engine endpoint uri.
+
+    Attributes
+    ----------
+    http: HttpClient
+        Http client attribute.
+    """
+
+    def __init__(self, base_url: str = "http://localhost:50021"):
+        self.http = HttpClient(base_url=base_url)
+
+    async def close(self) -> None:
+        """Close http client
+
+        You must run this function, when you finish process.
+        """
+        await self.http.close()
+
+    async def __aenter__(self) -> Self:
+        return self
+
+    async def __aexit__(self, *args) -> None:
+        await self.close()
+
+    async def create_audio_query(
+        self, text: int, speaker: int, *, core_version: Optional[str] = None
+    ) -> AudioQuery:
+        """Create audio query
+
+        If you want do tts, you must run first.
+
+        Parameters
+        ----------
+        text: str
+            Text message
+        speaker: int
+            Speaker id
+        core_version: str
+            Core version
+
+        Returns
+        -------
+        AudioQuery
+            Audio query, that run synthesis.
+        """
+        params = {
+            "text": text,
+            "speaker": speaker
+        }
+        if core_version is not None:
+            params["core_version"] = core_version
+        audio_query = await self.http.create_audio_query(params)
+        return AudioQuery(self.http, audio_query, speaker)
+
+    async def create_audio_query_from_preset(
+        self, text: str, preset_id: int, *, core_version: Optional[str] = None
+    ) -> AudioQuery:
+        params = {
+            "text": text,
+            "preset_id": preset_id
+        }
+        if core_version is not None:
+            params["core_version"] = core_version
+        audio_query = await self.http.create_audio_query_from_preset(params)
+        return AudioQuery(self, audio_query, preset_id)
+
+    async def fetch_engine_version(self) -> str:
+        """Fetch engine version
+
+        This can fetch voicevox engine version.
+        
+        Returns
+        -------
+        str
+            Engine version
+        """
+        return await self.http.get_version()
+
+    async def fetch_core_versions(self) -> List[str]:
+        """Fetch core versions
+
+        This can fetch voicevox core versions.
+
+        Returns
+        -------
+        List[str]
+            Core versions
+        """
+        return await self.http.get_core_versions()
+
+    async def fetch_speakers(
+        self, core_version: Optional[str] = None
+    ) -> List[Speaker]:
+        """Fetch speakers
+
+        This can fetch voicevox speakers.
+
+        Returns
+        -------
+        List[Speaker]
+            Speakers
+        """
+        speakers = await self.http.get_speakers(core_version)
+        return [Speaker(speaker) for speaker in speakers]
