9f86c282e0407ceca5da393786d000bea3d5d960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:01 +0000

    Add package chickatrice/0.1.0/chickatrice-0.1.0.tar.gz

---
+++ packages/chickatrice/chickatrice-0.1.0.tar.gz/chickatrice-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['chickatrice']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'chickatrice',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Adit RP',
+    'author_email': 'aditya.putra@pitik.id',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
