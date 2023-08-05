a5fcceb688aaf63a7fea6c5009f6189823740c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:56 +0000

    Add package Escher-legacy/1.7.4/Escher-legacy-1.7.4.tar.gz

---
+++ packages/Escher-legacy/Escher-legacy-1.7.4.tar.gz/Escher-legacy-1.7.4/setup.py
@@ -0,0 +1,68 @@
+import sys
+from os.path import join, dirname, realpath
+
+from setuptools import setup, find_packages
+
+directory = dirname(realpath(__file__))
+sys.path.insert(0, join(directory, 'escher'))
+version = __import__('version').__version__
+full_version = __import__('version').__full_version__
+package = __import__('version').package
+port = 8789
+
+with open(file="README.md", mode="r") as f:
+    readme = "".join(f.readlines())
+
+setup(
+    name='Escher-legacy',
+    version=full_version,
+    author=package['author'],
+    url=package['homepage'],
+    description=package['description'],
+    long_description=readme,
+    long_description_content_type= "text/markdown",
+    keywords=', '.join(package['keywords']),
+    license=package['license'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    data_files=[
+        (
+            'share/jupyter/nbextensions/escher',
+            [
+                'escher/static/extension.js',
+                'escher/static/escher.min.js',
+                'escher/static/escher.min.js.map',
+            ]
+        ),
+        (
+            'etc/jupyter/nbconfig/notebook.d',
+            ['escher.json'],
+        )
+    ],
+    install_requires=[
+        'Jinja2>=2.7.3',
+        'pytest>=4.0.1',
+        'cobra>=0.5.0',
+        'jsonschema>=3.0.1',
+        'ipywidgets>=7.4.0',
+        'pandas>=0.18'
+    ],
+    extras_require={
+        'docs': [
+            'sphinx>=2.1.1',
+            'sphinx-rtd-theme>=0.4.3',
+            'nbsphinx>=0.4.2'
+        ],
+    },
+)
