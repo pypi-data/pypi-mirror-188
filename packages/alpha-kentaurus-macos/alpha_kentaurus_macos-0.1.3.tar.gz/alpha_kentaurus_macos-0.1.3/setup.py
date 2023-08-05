62ac2543a718304f08fbba6b6dd5cd087c59a9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:50 +0000

    Add package alpha-kentaurus-macos/0.1.3/alpha_kentaurus_macos-0.1.3.tar.gz

---
+++ packages/alpha-kentaurus-macos/alpha_kentaurus_macos-0.1.3.tar.gz/alpha_kentaurus_macos-0.1.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['alpha_kentaurus']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'alpha-kentaurus-macos',
+    'version': '0.1.3',
+    'description': "Not what you're looking for",
+    'long_description': "## Not what you're looking for\n![nope](https://cultfilmclub.com/wp-content/uploads/2015/04/Nedry.png)  \n",
+    'author': 'nope',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
