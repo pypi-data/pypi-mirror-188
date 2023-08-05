53fca7342908703d245301f307ea8af952af928b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:51 +0000

    Add package anish-temperature/1.0.0/anish_temperature-1.0.0-py3-none-any.whl

---
+++ packages/anish-temperature/anish_temperature-1.0.0-py3-none-any.whl/anish_temperature/temperature.py
@@ -0,0 +1,22 @@
+def celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5.0 / 9
+
+
+def celcius_to_kelvin(celsius):
+    return celsius + 273.15
+
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+
+def fahrenheit_to_kelvin(fahrenheit):
+    return (fahrenheit + 459.67) * 5.0 / 9
+
+
+def kelvin_to_fahrenheit(kelvin):
+    return kelvin * 9.0 / 5 - 459.67
