cfa17bbf0aec89701a2b4ccc9b71c6e6ae14020e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:58 +0000

    Add package ofrak-capstone/1.0.0/ofrak_capstone-1.0.0.tar.gz

---
+++ packages/ofrak-capstone/ofrak_capstone-1.0.0.tar.gz/ofrak_capstone-1.0.0/setup.py
@@ -0,0 +1,62 @@
+import setuptools
+from setuptools.command.egg_info import egg_info
+
+
+class egg_info_ex(egg_info):
+    """Includes license file into `.egg-info` folder."""
+
+    def run(self):
+        # don't duplicate license into `.egg-info` when building a distribution
+        if not self.distribution.have_run.get("install", True):
+            # `install` command is in progress, copy license
+            self.mkpath(self.egg_info)
+            self.copy_file("LICENSE", self.egg_info)
+
+        egg_info.run(self)
+
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="ofrak_capstone",
+    version="1.0.0",
+    packages=setuptools.find_packages(exclude=["ofrak_capstone_test", "ofrak_capstone_test.*"]),
+    package_data={"ofrak_capstone": ["py.typed"]},
+    install_requires=["capstone==4.0.2", "ofrak"],
+    extras_require={
+        "test": [
+            "fun-coverage==0.2.0",
+            "pytest",
+            "pytest-cov",
+            "pytest-asyncio==0.19.0",
+            "requests",
+            "ofrak[test]",
+        ]
+    },
+    author="Red Balloon Security",
+    author_email="ofrak@redballoonsecurity.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://ofrak.com/",
+    download_url="https://github.com/redballoonsecurity/ofrak",
+    project_urls={
+        "Documentation": "https://ofrak.com/docs/",
+        "Community License": "https://github.com/redballoonsecurity/ofrak/blob/master/LICENSE",
+        "Commercial Licensing Information": "https://ofrak.com/license/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: Other/Proprietary License",
+        "License :: Free To Use But Restricted",
+        "License :: Free For Home Use",
+        "Topic :: Security",
+        "Typing :: Typed",
+    ],
+    python_requires=">=3.7",
+    license="Proprietary",
+    license_files=["LICENSE"],
+    cmdclass={"egg_info": egg_info_ex},
+    entry_points={"ofrak.packages": ["ofrak_capstone_pkg = ofrak_capstone"]},
+)
