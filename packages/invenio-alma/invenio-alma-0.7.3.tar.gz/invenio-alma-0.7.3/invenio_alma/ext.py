b28070f0bbad6ed3b0757fe2067ead764431b8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package invenio-alma/0.7.3/invenio-alma-0.7.3.tar.gz

---
+++ packages/invenio-alma/invenio-alma-0.7.3.tar.gz/invenio-alma-0.7.3/invenio_alma/ext.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2022 Graz University of Technology.
+#
+# invenio-alma is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio module to connect InvenioRDM to Alma."""
+
+from .services import AlmaRESTService
+
+
+class InvenioAlma:
+    """invenio-alma extension."""
+
+    def __init__(self, app=None):
+        """Extension initialization."""
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization."""
+        self.init_services(app)
+        app.extensions["invenio-alma"] = self
+
+    def init_services(self, app):
+        """Initialize service."""
+        api_key = app.config.get("ALMA_API_KEY", "")
+        api_host = app.config.get("ALMA_API_HOST", "")
+
+        self.alma_rest_service = AlmaRESTService.build(api_key, api_host)
