7ac145356a0e1c3f75d0cc4b6a775fa560e12a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package megaboost/0.1.3/megaboost-0.1.3.tar.gz

---
+++ packages/megaboost/megaboost-0.1.3.tar.gz/megaboost-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='megaboost',
+    version='0.1.3',    
+    description='Semi-supervised AutoML library on top of Pytorch',
+    url='https://github.com/max-ng/megaboost',
+    author='Max Ng',
+    author_email='maxnghello@gmail.com',
+    license='',
+    packages=['megaboost'],
+    install_requires=['numpy>=1.19.2',
+                        'Pillow>=9.2.0',
+                        'scikit_learn>=1.2.0',
+                        'torch>=1.12.1',
+                        'torchvision>=0.13.1'                   
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.10'
+    ],
+)
