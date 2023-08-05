028bd2f695b0432f14f6b439d43378b4fb16d4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:17 +0000

    Add package aternos-client/0.1.4/aternos_client-0.1.4.tar.gz

---
+++ packages/aternos-client/aternos_client-0.1.4.tar.gz/aternos_client-0.1.4/README.md
@@ -0,0 +1,25 @@
+# Hi mineguys  👋
+
+
+## 🛠️ Install     
+```bash
+pip install aternos
+```
+
+## 🧑🏻‍💻 Usage
+```js
+aternos start
+aternos stop
+aternos info
+```
+
+## Contributing
+
+If you want participate project
+```bash
+poetry install
+```
+
+Depot coming soon
+if you don't have [poetry](https://python-poetry.org/docs/#installation)
+        
---
+++ packages/aternos-client/aternos_client-0.1.4.tar.gz/aternos_client-0.1.4/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aternos']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['asyncio>=3.4.3,<4.0.0',
+ 'python-aternos>=2.1.3,<3.0.0',
+ 'typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['aternos = aternos.main:app']}
+
+setup_kwargs = {
+    'name': 'aternos-client',
+    'version': '0.1.4',
+    'description': 'A simple client to connect on Aternos server',
+    'long_description': "# Hi mineguys  👋\n\n\n## 🛠️ Install     \n```bash\npip install aternos\n```\n\n## 🧑🏻\u200d💻 Usage\n```js\naternos start\naternos stop\naternos info\n```\n\n## Contributing\n\nIf you want participate project\n```bash\npoetry install\n```\n\nDepot coming soon\nif you don't have [poetry](https://python-poetry.org/docs/#installation)\n        ",
+    'author': 'qrillet',
+    'author_email': 'quentin.rillet@externe.covage.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
