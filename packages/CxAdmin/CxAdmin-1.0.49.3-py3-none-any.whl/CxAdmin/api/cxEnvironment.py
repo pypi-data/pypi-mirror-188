24ec51474f52aa87b0bb26355fc753a7baf55958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:14 +0000

    Add package CxAdmin/1.0.49.3/CxAdmin-1.0.49.3-py3-none-any.whl

---
+++ packages/CxAdmin/CxAdmin-1.0.49.3-py3-none-any.whl/CxAdmin/api/cxEnvironment.py
@@ -0,0 +1,13 @@
+from typing import Any
+from src.CxAdmin.api import CxItem
+
+
+class CxEnvironment(CxItem):
+    def getTenant(self) -> Any:
+        raise NotImplementedError()
+
+    def getRegions(self) -> Any:
+        raise NotImplementedError()
+
+    def get(self) -> Any:
+        return self._httpClient.get(self._path).json()
