93c081f8eee203627ec3d166cf857918f3e5c58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:48 +0000

    Add package Mahdihh/0.1/Mahdihh-0.1.tar.gz

---
+++ packages/Mahdihh/Mahdihh-0.1.tar.gz/Mahdihh-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="Mahdihh",
+version='0.1',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['mahdihh'],
+install_requires=[])
+
+# python setup.py bdist_wheel
+#  python setup.py pytest
