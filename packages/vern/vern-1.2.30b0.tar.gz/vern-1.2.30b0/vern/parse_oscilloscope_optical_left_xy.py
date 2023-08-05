fc6091aa9b44d316bb704b5530088cdf6cf09fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package vern/1.2.30b0/vern-1.2.30b0.tar.gz

---
+++ packages/vern/vern-1.2.30b0.tar.gz/vern-1.2.30b0/vern/parse_oscilloscope_optical_left_xy.py
@@ -0,0 +1,29 @@
+# profilometer
+import numpy as np
+import pandas as pd
+from .misc import *
+import mat73
+import scipy.io
+__all__ = ['parse_oscilloscope_optical_left_xy']
+
+def read_files(input_path, txt_path, plot_path, hist_path, smooth_plot_path, interactive=False):
+    try:
+        mat = mat73.loadmat(input_path)
+    except:
+        mat = scipy.io.loadmat(input_path)
+    data = {
+        "Time (ns)": mat["x"][0,:]*1e9,
+        "Voltage (mV)": mat["y"][0,:]*1e3,
+    }
+    df = pd.DataFrame.from_dict(data)
+    df.to_csv(txt_path, index=False)
+    p = Plot(df, interactive=interactive)
+    p.plot(plot_path)
+    p.hist(hist_path)
+    smooth_df = df.copy()
+    smooth_df["Voltage (mV)"] = smooth_df["Voltage (mV)"].rolling(100).mean()
+    p = Plot(smooth_df, interactive=interactive)
+    p.plot(smooth_plot_path)
+
+def parse_oscilloscope_optical_left_xy(**kwargs):
+    read_files(kwargs["input_path"], kwargs["txt_path"], kwargs["plot_path"], kwargs["hist_path"], kwargs["smooth_plot_path"])
