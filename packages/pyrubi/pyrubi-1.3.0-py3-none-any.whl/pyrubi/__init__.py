8f6c133f2983efad2a9eeea26640c313f05c6f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:20 +0000

    Add package pyrubi/1.3.0/pyrubi-1.3.0-py3-none-any.whl

---
+++ packages/pyrubi/pyrubi-1.3.0-py3-none-any.whl/pyrubi/__init__.py
@@ -0,0 +1,7 @@
+from .rubika import bot as Bot, bot_async as Bot_async , message as Message
+library_info = {
+    "version": "1.3.0",
+    "author": "Ali Ganji zadeh",
+    "github": "https://github.com/AliGanji1/pyrubi",
+    "rubika": "@pyrubika"
+}
