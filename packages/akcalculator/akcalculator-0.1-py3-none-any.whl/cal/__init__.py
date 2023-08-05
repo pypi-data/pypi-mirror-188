643811bc8ee346a22cf469dd19e53b4b796b8b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:16 +0000

    Add package akcalculator/0.1/akcalculator-0.1-py3-none-any.whl

---
+++ packages/akcalculator/akcalculator-0.1-py3-none-any.whl/cal/__init__.py
@@ -0,0 +1,32 @@
+def add_numbers(a,b):
+    return a+b
+
+def mul_numbers(a,b):
+    return a*b
+
+def div_numbers(a,b):
+    return a/b
+
+def sub_numbers(a,b):
+    return a-b
+
+def sqaure_numbers(a):
+    return a**2
+
+def cube_numbers(a):
+    return a**3
+
+def percentage_numbers(a,b):
+    return (a/b)*100
+
+def persqaure_numbers(a):
+    return 4(a)
+
+def perrect_numbers(a,b):
+    return 2(a + b)
+
+def areatri_numbers(a,b):
+    return 1/2 * a * b
+
+def sareac_numbers(a,b):
+    return 6*(a**2)
