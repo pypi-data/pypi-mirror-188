295043e1ff4e5dd8340e8732dac8ecf45dfe54c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:09 +0000

    Add package streamlit-file-upload-ven/2.0.1/streamlit-file-upload-ven-2.0.1.tar.gz

---
+++ packages/streamlit-file-upload-ven/streamlit-file-upload-ven-2.0.1.tar.gz/streamlit-file-upload-ven-2.0.1/st_file_upload_ven/frontend/build/precache-manifest.80e36d0fddebfd9d25e8109d34b7c42a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f816acc5d6247d20dd5e747bee63a0a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b9e0a108aac057d61ca",
+    "url": "./static/css/main.239f2e9a.chunk.css"
+  },
+  {
+    "revision": "0383329742007e33ec5c",
+    "url": "./static/js/2.7fb634a3.chunk.js"
+  },
+  {
+    "revision": "bf56b6cdf7ded6c60e4459c9d99a2449",
+    "url": "./static/js/2.7fb634a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9e0a108aac057d61ca",
+    "url": "./static/js/main.3cac141e.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
