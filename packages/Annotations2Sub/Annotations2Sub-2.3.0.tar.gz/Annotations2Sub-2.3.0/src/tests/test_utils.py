7684982571a10e4539146480b6ec76d5922e4ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:36 +0000

    Add package Annotations2Sub/2.3.0/Annotations2Sub-2.3.0.tar.gz

---
+++ packages/Annotations2Sub/Annotations2Sub-2.3.0.tar.gz/Annotations2Sub-2.3.0/src/tests/test_utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import gettext
+import os
+import sys
+
+import pytest
+
+from Annotations2Sub.utils import MakeSureStr, RedText, YellowText, internationalization
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+def test_YellowText():
+    assert YellowText("Test") == "\033[33mTest\033[0m"
+
+
+def test_RedText():
+    assert RedText("Test") == "\033[31mTest\033[0m"
+
+
+def test_MakeSureStr():
+    with pytest.raises(TypeError):
+        MakeSureStr(0)  # type: ignore
+
+
+def test_internationalization():
+    internationalization()
+
+
+def test_internationalization2():
+    def a(*args, **kwargs):
+        raise Exception
+
+    m = pytest.MonkeyPatch()
+    m.setattr(gettext, "translation", a)
+
+    internationalization()
