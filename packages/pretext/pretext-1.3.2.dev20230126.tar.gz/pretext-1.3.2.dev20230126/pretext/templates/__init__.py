0ffb73ed57f6a8329babd5df184e80011be8d77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:54 +0000

    Add package pretext/1.3.2.dev20230126/pretext-1.3.2.dev20230126.tar.gz

---
+++ packages/pretext/pretext-1.3.2.dev20230126.tar.gz/pretext-1.3.2.dev20230126/pretext/templates/__init__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import importlib.resources
+
+
+def resource_path(filename: str) -> Path:
+    """
+    Returns resource manager
+    Usage:
+    with resource_path('foo.bar') as filepath:
+        # do things
+    """
+    from . import resources
+
+    return importlib.resources.path(resources, filename)
