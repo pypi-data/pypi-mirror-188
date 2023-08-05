3b713305f48289c9d1a4967ab5ed3ad2651370c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:39 +0000

    Add package gpt-traceback/0.1.18/gpt_traceback-0.1.18.tar.gz

---
+++ packages/gpt-traceback/gpt_traceback-0.1.18.tar.gz/gpt_traceback-0.1.18/src/gpt_traceback/prompt.py
@@ -0,0 +1,38 @@
+import requests
+import os
+import json
+from getpass import getpass
+import warnings
+
+def predict_prompt(prompt):
+  apiKey = os.environ.get("OPENAI_API_KEY")
+  if apiKey is None:
+    print("""Api Key is missing. Head over to:
+https://beta.openai.com/account/api-keys
+Please enter OPENAI API KEY:""")
+    apiKey = input()
+    apiKey = apiKey.strip()
+    assert "sk-" in apiKey, "API Key needs to start with sk-..."
+    os.environ["OPENAI_API_KEY"] = apiKey
+  model = os.environ.get("OPENAI_MODEL",'text-davinci-003')
+  temperature = 0.
+  maxTokens = 150
+
+  url = 'https://api.openai.com/v1/completions'
+  headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + apiKey
+  }
+  payload = {
+      'model': model,
+      'prompt': prompt,
+      'temperature': temperature,
+      'max_tokens': maxTokens
+  }
+
+  response = requests.post(url, headers=headers, data=json.dumps(payload))
+  response_json = response.json()
+  try:
+    return response_json['choices'][0]['text']
+  except KeyError:
+    warnings.warn(response_json)
