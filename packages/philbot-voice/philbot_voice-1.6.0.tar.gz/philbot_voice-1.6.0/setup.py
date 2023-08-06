d0c5cd7fc6f959064c360f680654a0e6ff827063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:34 +0000

    Add package philbot-voice/1.6.0/philbot_voice-1.6.0.tar.gz

---
+++ packages/philbot-voice/philbot_voice-1.6.0.tar.gz/philbot_voice-1.6.0/setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['philbot-voice']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Flask>=2.2.2,<3.0.0',
+ 'PyNaCl>=1.5.0,<2.0.0',
+ 'PyOgg==0.6.14a1',
+ 'opentelemetry-exporter-otlp-proto-http>=1.15.0,<2.0.0',
+ 'opentelemetry-instrumentation-flask>=0.36b0,<0.37',
+ 'opentelemetry-instrumentation>=0.36b0,<0.37',
+ 'opentelemetry-sdk>=1.15.0,<2.0.0',
+ 'websocket-client>=1.4.2,<2.0.0',
+ 'youtube_dl>=2021.12.17,<2022.0.0']
+
+entry_points = \
+{'console_scripts': ['philbot-voice = voice:main']}
+
+setup_kwargs = {
+    'name': 'philbot-voice',
+    'version': '1.6.0',
+    'description': '',
+    'long_description': 'None',
+    'author': 'philipp.lengauer',
+    'author_email': 'p.lengauer@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
