b26e24758ff9f78636b5358b07cc634fc6356173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package huble/0.1.89/huble-0.1.89-py3-none-any.whl

---
+++ packages/huble/huble-0.1.89-py3-none-any.whl/huble/sklearn/process/__init__.py
@@ -0,0 +1,5 @@
+from .functions import remove_nan_values, replace_nan_values, drop_rows_columns, remove_outliers, drop_duplicates, change_data_type
+from .functions import round_data, filter_dataframe, truncate_dataframe, sort_values, transpose
+from .functions import min_max_scale, max_abs_scale, robust_scale, standard_scale, normalize, ordinal_encode, one_hot_encode
+from .templating import *
+from .generate import generate_script as preprocess_generate
