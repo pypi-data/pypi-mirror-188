0cbdade3ae25e72f6d617ae4338b6aba686ebb76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:20 +0000

    Add package cag/1.1.5/cag-1.1.5-py3-none-any.whl

---
+++ packages/cag/cag-1.1.5-py3-none-any.whl/cag/framework/__init__.py
@@ -0,0 +1,4 @@
+from .annotator.annotator_base import GenericAnnotator
+from .creator.base_creator import GraphCreatorBase
+from .analyzer.analyzer_base import AnalyzerBase
+__all__ = [GenericAnnotator, GraphCreatorBase, AnalyzerBase]
