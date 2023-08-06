8ad60856e503f77300084a3dc8639e551a508133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:59 +0000

    Add package bobtail/0.0.12/bobtail-0.0.12-py3-none-any.whl

---
+++ packages/bobtail/bobtail-0.0.12-py3-none-any.whl/bobtail/exceptions.py
@@ -0,0 +1,18 @@
+class InvalidStatusError(Exception):
+    pass
+
+
+class NoRoutesError(Exception):
+    pass
+
+
+class RouteClassError(Exception):
+    pass
+
+
+class FormDataError(Exception):
+    pass
+
+
+class MultipartFormDataError(Exception):
+    pass
