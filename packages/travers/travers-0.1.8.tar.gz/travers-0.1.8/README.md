a75f52095112c531cadfaf978b800621ca695801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:02 +0000

    Add package travers/0.1.8/travers-0.1.8.tar.gz

---
+++ packages/travers/travers-0.1.8.tar.gz/travers-0.1.8/README.md
@@ -0,0 +1,11 @@
+<img src="https://raw.githubusercontent.com/joocer/travers/main/icon.png" height=82px>
+
+### travers
+_Python Graph Library_
+
+----
+
+travers is a limited feature-set, low-memory, traversal optimized Directed Graph structure.
+
+## Documentation
+
---
+++ packages/travers/travers-0.1.8.tar.gz/travers-0.1.8/setup.py
@@ -0,0 +1,27 @@
+# python setup.py build_ext --inplace
+
+from setuptools import setup, find_packages  # type:ignore
+
+with open("travers/version.py", "r") as v:
+    vers = v.read()
+exec(vers)  # nosec
+
+with open("README.md", "r") as rm:
+    long_description = rm.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="travers",
+    version=__version__,
+    description="Python Graph Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    maintainer="Joocer",
+    author="joocer",
+    author_email="justin.joyce@joocer.com",
+    packages=find_packages(include=["travers", "travers.*"]),
+    url="https://github.com/joocer/travers",
+    install_requires=required,
+)
