05f114523329e48b56ae6fbeb592d48b19b15f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:22 +0000

    Add package nectarifere/2.3.1/nectarifere-2.3.1.tar.gz

---
+++ packages/nectarifere/nectarifere-2.3.1.tar.gz/nectarifere-2.3.1/nectarifere/nectar.py
@@ -0,0 +1,30 @@
+'''
+"With great power comes great responsibility." M.S.
+'''
+
+import os
+import random
+from glob import glob
+from .extracteur import ExtracteurDeJus
+from play_sounds import play_file
+from pathlib import Path
+import time
+directory = os.path.dirname(os.path.abspath(__file__))
+
+
+class nectar(ExtracteurDeJus):
+
+    def __init__(self, function):
+        super().__init__(function)
+        self.success_files = glob(os.path.join(directory, 'Sounds', 'Kaamelott', 'Succes', '*.wav'))
+        self.failure_files = glob(os.path.join(directory, 'Sounds', 'Kaamelott', 'Echec', '*.wav'))
+
+    def success(self):
+        time.sleep(0.5)
+        p = Path(random.choice(self.success_files), encoding=None)
+        play_file(p)
+
+    def failure(self):
+        time.sleep(0.5)
+        p = Path(random.choice(self.failure_files), encoding=None)
+        play_file(p)
---
+++ packages/nectarifere/nectarifere-2.3.1.tar.gz/nectarifere-2.3.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='nectarifere',
+    url='https://github.com/XanAstia/Nectarifere.git',
+    author='Xan Astia',
+    author_email='xan.astia@live.fr',
+    # Needed to actually package something
+    packages=['nectarifere', 'tests'],
+    # Needed for dependencies
+    install_requires=['play-sounds', 'playsound==1.2.2', 'pillow'],
+    # *strongly* suggested for sharing
+    version='2.3.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Décorateur pour ambiancer vos codes.',
+    long_description=long_description,
+    long_description_content_type = 'text/markdown',
+#   include_package_data=True, #empeche package_data de fonctionner
+    package_data={'nectarifere': ['Sounds/Kaamelott/Echec/*.wav', 'Sounds/Kaamelott/Succes/*.wav', 'Pictures/Echec/*.jpg', 'Pictures/Succes/*.jpg']},
+)
