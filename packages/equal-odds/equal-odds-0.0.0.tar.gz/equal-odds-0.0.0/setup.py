94dfa9d89266a05ed9d00c70c3af9c1ab5e3a626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package equal-odds/0.0.0/equal-odds-0.0.0.tar.gz

---
+++ packages/equal-odds/equal-odds-0.0.0.tar.gz/equal-odds-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='equal-odds',
+    version='0.0.0',
+    url='https://github.com/AndreFCruz/equal-odds.git',
+    author='AndreFCruz',
+    author_email='andrecruz97@gmail.com',
+    description='TODO: in construction',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
+
