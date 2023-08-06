2146c8f4c1c6ee4308091e5b8d8d6675dd74e5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:38 +0000

    Add package nectarifere/2.2/nectarifere-2.2.tar.gz

---
+++ packages/nectarifere/nectarifere-2.2.tar.gz/nectarifere-2.2/nectarifere/nectar.py
@@ -0,0 +1,27 @@
+'''
+"With great power comes great responsibility." M.S.
+'''
+
+import os
+import random
+from glob import glob
+from .extracteur import ExtracteurDeJus
+from play_sounds import play_file
+from pathlib import Path
+directory = os.path.dirname(os.path.abspath(__file__))
+
+
+class nectar(ExtracteurDeJus):
+
+    def __init__(self, function):
+        super().__init__(function)
+        self.success_files = glob(os.path.join(directory, 'Sounds', 'Kaamelott', 'Succes', '*.wav'))
+        self.failure_files = glob(os.path.join(directory, 'Sounds', 'Kaamelott', 'Echec', '*.wav'))
+
+    def success(self):
+        p = Path(random.choice(self.success_files), encoding=None)
+        play_file(p)
+
+    def failure(self):
+        p = Path(random.choice(self.failure_files), encoding=None)
+        play_file(p)
