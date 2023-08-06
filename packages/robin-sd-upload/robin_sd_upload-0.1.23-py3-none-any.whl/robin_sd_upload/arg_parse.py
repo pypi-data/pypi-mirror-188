be0087ad124cb49217ea61a2292e7e37c0bc923f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:05 +0000

    Add package robin-sd-upload/0.1.23/robin_sd_upload-0.1.23-py3-none-any.whl

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.23-py3-none-any.whl/robin_sd_upload/arg_parse.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+import yaml
+from .validate import validate
+from .create_zip import create_zip
+from .push_software import push_software
+from .remove_zip import remove_zip
+from .parse_config import parse_config
+from .check_upload_file import check_upload_file
+
+def arg_parse(config_file):
+    parser = argparse.ArgumentParser(
+        description = 'Robin Radar Systems Software Uploader',
+        usage       = 'robin-sd-upload [options]', 
+        prog        = 'Robin Radar Systems Software Uploader',
+        epilog      = 'To report any bugs or issues, please visit: https://support.robinradar.systems'
+    )
+
+    parser.add_argument('--check', action='store_true', help='ensure all prerequisites are met')
+    parser.add_argument('--upload', action='store_true', help='upload software: robin-sd-upload --upload --type=radar_type --version=version_name --source=upload_file_absolute_path')
+    # arguments for upload
+    parser.add_argument("--type", type=str, help="radar type")
+    parser.add_argument("--version", type=str, help="version name")
+    parser.add_argument("--source", type=str, help="upload file absolute path")
+
+    args = parser.parse_args()
+    radarType = args.type
+    version_name = args.version
+    upload_file_path = args.source
+
+    if args.check:
+        parse_config(config_file)
+        check_upload_file(upload_file_path)
+        exit(0)
+    elif args.upload:
+        parse_config(config_file)
+        validate(radarType, version_name)
+        check_upload_file(upload_file_path)
+
+        version_dir_to_upload= os.path.join(upload_file_path, version_name)
+        zipped_file_path = os.path.join(upload_file_path, version_name + '.zip')
+
+        print('version_dir_to_upload: ', version_dir_to_upload)
+        print('zipped_file_path: ', zipped_file_path)
+
+        create_zip(zipped_file_path, version_name)
+        push_software(config_file, zipped_file_path, radarType, version_name)
+        remove_zip(zipped_file_path)
+    else:
+        parser.print_help()
+        exit(1)
+
+
+
+
+
