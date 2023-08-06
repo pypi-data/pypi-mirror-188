eb4b5fa2be7f5840d873f4980831a15aae889fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:25 +0000

    Add package onnx2tf/1.5.34/onnx2tf-1.5.34.tar.gz

---
+++ packages/onnx2tf/onnx2tf-1.5.34.tar.gz/onnx2tf-1.5.34/onnx2tf/ops/Gather.py
@@ -0,0 +1,152 @@
+import random
+random.seed(0)
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+import onnx_graphsurgeon as gs
+from onnx2tf.utils.common_functions import (
+    get_replacement_parameter,
+    replace_parameter,
+    get_constant_or_variable,
+    convert_axis,
+    print_node_info,
+    inverted_operation_enable_disable,
+    make_tf_node_info,
+    pre_process_transpose,
+    post_process_transpose,
+)
+from onnx2tf.utils.enums import NUMPY_DTYPES_TO_TF_DTYPES
+
+
+@print_node_info
+@inverted_operation_enable_disable
+@get_replacement_parameter
+def make_node(
+    *,
+    graph_node: gs.Node,
+    tf_layers_dict: dict,
+    **kwargs: dict,
+):
+    """Gather
+
+    Parameters
+    ----------
+    graph_node: gs.Node
+        graph_surgeon Node
+
+    tf_layers_dict: dict
+        optype, shape, dtype, tensorflow graph
+    """
+    before_op_output_shape_trans_1 = \
+        tf_layers_dict.get(graph_node.inputs[0].name, {}).get('before_op_output_shape_trans', True)
+    before_op_output_shape_trans_2 = \
+        tf_layers_dict.get(graph_node.inputs[1].name, {}).get('before_op_output_shape_trans', True)
+    before_op_output_shape_trans = \
+        before_op_output_shape_trans_1 \
+        and before_op_output_shape_trans_2
+
+    graph_node_input_1 = get_constant_or_variable(
+        graph_node.inputs[0],
+        before_op_output_shape_trans,
+    )
+    graph_node_input_2 = get_constant_or_variable(
+        graph_node.inputs[1],
+        before_op_output_shape_trans,
+    )
+    graph_node_output: gs.Variable = graph_node.outputs[0]
+
+    input_tensor = tf_layers_dict[graph_node_input_1.name]['tf_node'] \
+        if isinstance(graph_node_input_1, gs.Variable) else graph_node_input_1
+    indices = tf_layers_dict[graph_node_input_2.name]['tf_node'] \
+        if isinstance(graph_node_input_2, gs.Variable) else graph_node_input_2
+
+    shape = graph_node_output.shape
+    dtype = graph_node_output.dtype
+
+    axis = graph_node.attrs.get("axis", 0)
+    axis = convert_axis(
+        axis=axis,
+        tensor_rank=len(input_tensor.shape),
+        before_op_output_shape_trans=before_op_output_shape_trans,
+    )
+
+    optimization_for_gpu_delegate: bool = \
+        kwargs['optimization_for_gpu_delegate']
+
+    # tensorflow gather supports only positive indices
+    out_dtype = NUMPY_DTYPES_TO_TF_DTYPES[indices.dtype] \
+        if isinstance(indices.dtype, np.dtype) else indices.dtype
+    if not optimization_for_gpu_delegate:
+        cond = tf.cast(indices < 0, dtype=out_dtype)
+        indices = tf.cast(
+            indices + cond * tf.shape(input_tensor, out_type=out_dtype)[axis],
+            dtype=out_dtype,
+        )
+    else:
+        cond = indices < 0
+        indices = indices + tf.cast(
+            tf.where(cond, 1, 0) * tf.shape(input_tensor, out_type=out_dtype)[axis],
+            dtype=out_dtype,
+        )
+
+    # Preserving Graph Structure (Dict)
+    tf_layers_dict[graph_node_output.name] = {
+        'optype': graph_node.op,
+        'shape': shape,
+        'dtype': dtype,
+    }
+
+    # Param replacement
+    input_tensor = replace_parameter(
+        value_before_replacement=input_tensor,
+        param_target='inputs',
+        param_name=graph_node.inputs[0].name,
+        **kwargs,
+    )
+    indices = replace_parameter(
+        value_before_replacement=indices,
+        param_target='inputs',
+        param_name=graph_node.inputs[1].name,
+        **kwargs,
+    )
+
+    # Pre-process transpose
+    input_tensor = pre_process_transpose(
+        value_before_transpose=input_tensor,
+        param_target='inputs',
+        param_name=graph_node.inputs[0].name,
+        **kwargs,
+    )
+
+    # Generation of TF OP
+    tf_layers_dict[graph_node_output.name]['tf_node'] = \
+        tf.gather(
+            params=input_tensor,
+            indices=indices,
+            axis=axis,
+            name=graph_node.name,
+        )
+
+    # Post-process transpose
+    tf_layers_dict[graph_node_output.name]['tf_node'] = post_process_transpose(
+        value_before_transpose=tf_layers_dict[graph_node_output.name]['tf_node'],
+        param_target='outputs',
+        param_name=graph_node.outputs[0].name,
+        **kwargs,
+    )
+
+    # Generation of Debug Info
+    tf_layers_dict[graph_node_output.name]['tf_node_info'] = \
+        make_tf_node_info(
+            node_info={
+                'tf_op_type': tf.gather,
+                'tf_inputs': {
+                    'params': input_tensor,
+                    'indices': indices,
+                    'axis': axis,
+                },
+                'tf_outputs': {
+                    'output': tf_layers_dict[graph_node_output.name]['tf_node'],
+                },
+            }
+        )
