9990ff6f9ba3adaea8fb0c06573db6968fc58e55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package gracy/0.2.0/gracy-0.2.0.tar.gz

---
+++ packages/gracy/gracy-0.2.0.tar.gz/gracy-0.2.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['gracy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.23.3,<0.24.0']
+
+extras_require = \
+{'rich': ['rich']}
+
+setup_kwargs = {
+    'name': 'gracy',
+    'version': '0.2.0',
+    'description': 'Gracefully manage your API interactions',
+    'long_description': '<p align="center">\n    <img src="./img/logo-dark.png#gh-dark-mode-only">\n    <img src="./img/logo-light.png#gh-light-mode-only">\n</p>\n\n<h2 align="center">Gracefully manage your API interactions</h2>\n\n<p align="center">\n  <!-- CI --><a href="https://github.com/guilatrova/gracy/actions"><img alt="Actions Status" src="https://github.com/guilatrova/gracy/workflows/CI/badge.svg"></a>\n  <!-- PyPI --><a href="https://pypi.org/project/gracy/"><img alt="PyPI" src="https://img.shields.io/pypi/v/gracy"/></a>\n  <!-- Supported Python versions --><img src="https://badgen.net/pypi/python/gracy" />\n  <!-- Alternative Python versioning: <img alt="python version" src="https://img.shields.io/badge/python-3.9%20%7C%203.10-blue"> -->\n  <!-- LICENSE --><a href="https://github.com/guilatrova/gracy/blob/main/LICENSE"><img alt="GitHub" src="https://img.shields.io/github/license/guilatrova/gracy"/></a>\n  <!-- PyPI downloads --><a href="https://pepy.tech/project/gracy/"><img alt="Downloads" src="https://static.pepy.tech/personalized-badge/gracy?period=total&units=international_system&left_color=grey&right_color=blue&left_text=%F0%9F%A6%96%20Downloads"/></a>\n  <!-- Formatting --><a href="https://github.com/psf/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"/></a>\n  <!-- Tryceratops --><a href="https://github.com/guilatrova/tryceratops"><img alt="try/except style: tryceratops" src="https://img.shields.io/badge/try%2Fexcept%20style-tryceratops%20%F0%9F%A6%96%E2%9C%A8-black" /></a>\n  <!-- Typing --><a href="https://github.com/python/mypy"><img alt="Types: mypy" src="https://img.shields.io/badge/types-mypy-blue.svg"/></a>\n  <!-- Follow handle --><a href="https://twitter.com/intent/user?screen_name=guilatrova"><img alt="Follow guilatrova" src="https://img.shields.io/twitter/follow/guilatrova?style=social"/></a>\n  <!-- Sponsor --><a href="https://github.com/sponsors/guilatrova"><img alt="Sponsor guilatrova" src="https://img.shields.io/github/sponsors/guilatrova?logo=GitHub%20Sponsors&style=social"/></a>\n</p>\n\nGracy helps you handle failures, logging, retries, and tracking for all your HTTP interactions.\n\n> "Don\'t repeat, simplify"\n\n---\n\n## Get started\n\n### Installation\n\n```\npip install gracy\n```\n\nOR\n\n```\npoetry add gracy\n```\n',
+    'author': 'Guilherme Latrova',
+    'author_email': 'hello@guilatrova.dev',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/guilatrova/gracy',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/gracy/gracy-0.2.0.tar.gz/gracy-0.2.0/src/gracy/reports.py
@@ -0,0 +1,133 @@
+from collections import defaultdict
+from dataclasses import dataclass
+from http import HTTPStatus
+from statistics import mean
+from typing import Dict, List, Literal, Set
+
+import httpx
+from rich.console import Console
+from rich.table import Table
+
+
+@dataclass(frozen=True)
+class GracyRequestResult:
+    __slots__ = ("url", "response")
+
+    url: str
+    response: httpx.Response
+
+
+class GracyReport:
+    def __init__(self) -> None:
+        self._results: List[GracyRequestResult] = []
+
+    def track(self, request_result: GracyRequestResult):
+        self._results.append(request_result)
+
+    def print(self):
+        requests_by_url = defaultdict[str, Set[httpx.Response]](set)
+        requests_sum = defaultdict[str, Dict[HTTPStatus | Literal["total"], int]](
+            lambda: defaultdict[HTTPStatus | Literal["total"], int](int)
+        )
+
+        for result in self._results:
+            requests_by_url[result.url].add(result.response)
+            requests_sum[result.url]["total"] += 1
+            requests_sum[result.url][HTTPStatus(result.response.status_code)] += 1
+
+        requests_sum = dict(sorted(requests_sum.items(), key=lambda item: item[1]["total"], reverse=True))
+
+        console = Console()
+        table = Table(title="HTTP Requests Summary", show_lines=True)
+        table.add_column("URL", overflow="fold")
+        table.add_column("Total Requests (#)", justify="right")
+        table.add_column("Successful Requests (%)", justify="right")
+        table.add_column("Failed Requests (%)", justify="right")
+        table.add_column("Avg Latency (s)", justify="right")
+        table.add_column("Max Latency (s)", justify="right")
+        table.add_column("2xx Responses", justify="right")
+        table.add_column("3xx Responses", justify="right")
+        table.add_column("4xx Responses", justify="right")
+        table.add_column(">5xx Responses", justify="right")
+
+        footer_totals = {
+            "URL": "[b]TOTAL[/b]",
+            "total": 0,
+            "success": 0,
+            "failed": 0,
+            "avg_latency": [],
+            "max_latency": 0.0,
+            "2xx": 0,
+            "3xx": 0,
+            "4xx": 0,
+            "5xx": 0,
+        }
+
+        for url, data in requests_sum.items():
+            all_requests = requests_by_url[url]
+
+            # Latency
+            url_latency = [r.elapsed.total_seconds() for r in all_requests]
+            avg_latency = mean(url_latency)
+            max_latency = max(url_latency)
+
+            # Total and Rates
+            total_requests = data["total"]
+            successful_requests = sum(
+                [count for status, count in data.items() if status != "total" and 200 <= status.value < 300]
+            )
+            failed_requests = total_requests - successful_requests
+            success_rate = (successful_requests / total_requests) * 100
+            failed_rate = (failed_requests / total_requests) * 100
+            failed_color = "red" if failed_requests else "white"
+
+            # Status Ranges
+            # fmt:off
+            responses_2xx = sum(count for status, count in data.items() if status != "total" and 200 <= status.value < 300)  # noqa: E501
+            responses_3xx = sum(count for status, count in data.items() if status != "total" and 300 <= status.value < 400)  # noqa: E501
+            responses_4xx = sum(count for status, count in data.items() if status != "total" and 400 <= status.value < 500)  # noqa: E501
+            responses_5xx = sum(count for status, count in data.items() if status != "total" and 500 <= status.value)
+
+            color_4xx = "red" if responses_4xx else "white"
+            color_5xx = "red" if responses_5xx else "white"
+            # fmt:on
+
+            # Footer
+            footer_totals["total"] += total_requests
+            footer_totals["success"] += successful_requests
+            footer_totals["failed"] += total_requests - successful_requests
+            footer_totals["avg_latency"] += url_latency
+            footer_totals["max_latency"] = max(footer_totals["max_latency"], max_latency)
+            footer_totals["2xx"] += responses_2xx
+            footer_totals["3xx"] += responses_3xx
+            footer_totals["4xx"] += responses_4xx
+            footer_totals["5xx"] += responses_5xx
+
+            # Row
+            table.add_row(
+                url,
+                f"[bold]{total_requests}[/bold]",
+                f"[green]{success_rate:.2f}%[/green]",
+                f"[bold][{failed_color}]{failed_rate:.2f}%[/bold][/{failed_color}]",
+                f"{avg_latency:.2f}",
+                f"{max_latency:.2f}",
+                str(responses_2xx),
+                str(responses_3xx),
+                f"[{color_4xx}]{responses_4xx}[/{color_4xx}]",
+                f"[{color_5xx}]{responses_5xx}[/{color_5xx}]",
+            )
+
+        table.add_row(
+            footer_totals["URL"],
+            str(footer_totals["total"]),
+            f"{((footer_totals['success'] / footer_totals['total']) * 100):.2f}%",
+            f"{((footer_totals['failed'] / footer_totals['total']) * 100):.2f}%",
+            f"{mean(footer_totals['avg_latency']):.2f}",
+            str(footer_totals["max_latency"]),
+            str(footer_totals["2xx"]),
+            str(footer_totals["3xx"]),
+            str(footer_totals["4xx"]),
+            str(footer_totals["5xx"]),
+        )
+
+        console.print(table)
