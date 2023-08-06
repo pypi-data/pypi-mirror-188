d094ab1f2817e3d6c0ce24cbeac499ecbd0a0397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:34 +0000

    Add package vja/0.0.3/vja-0.0.3.tar.gz

---
+++ packages/vja/vja-0.0.3.tar.gz/vja-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["click", "click-aliases", "requests", "parsedatetime", "python-dateutil"]
+
+setup(
+    name="vja",
+    version="0.0.3",
+    author="Christoph Ernst",
+    author_email="christoph.ernst72@googlemail.com",
+    description="A simple CLI for Vikunja task manager",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ce72/vja/",
+    packages=find_packages(),
+    install_requires=requirements,
+    scripts=[
+          'vja/vja',
+      ],
+    # entry_points={'console_scripts': [
+    #     'vja=vja:main'
+    # ]},
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    include_package_data=True
+)
