ad0a0fcca5353a82b8547d5a884693ae6b52a69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:58 +0000

    Add package pantsbuild.pants/2.16.0.dev6/pantsbuild.pants-2.16.0.dev6-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/pantsbuild.pants/pantsbuild.pants-2.16.0.dev6-cp39-cp39-macosx_10_15_x86_64.whl/pants/base/exceptions.py
@@ -0,0 +1,60 @@
+# Copyright 2014 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from pants.engine.internals.native_engine import EngineError as EngineError  # noqa: F401
+from pants.engine.internals.native_engine import (  # noqa: F401
+    IncorrectProductError as IncorrectProductError,
+)
+from pants.engine.internals.native_engine import IntrinsicError as IntrinsicError  # noqa: F401
+
+if TYPE_CHECKING:
+    from pants.engine.internals.native_engine import PyFailure
+
+
+class TargetDefinitionException(Exception):
+    """Indicates an invalid target definition.
+
+    :API: public
+    """
+
+    def __init__(self, target, msg):
+        """
+        :param target: the target in question
+        :param string msg: a description of the target misconfiguration
+        """
+        super().__init__(f"Invalid target {target}: {msg}")
+
+
+class BuildConfigurationError(Exception):
+    """Indicates an error in a pants installation's configuration."""
+
+
+class BackendConfigurationError(BuildConfigurationError):
+    """Indicates a plugin backend with a missing or malformed register module."""
+
+
+class MappingError(Exception):
+    """Indicates an error mapping addressable objects."""
+
+
+class NativeEngineFailure(Exception):
+    """A wrapper around a `Failure` instance.
+
+    The failure instance being wrapped can come from an exception raised in a rule. When this
+    failure is returned to a requesting rule it is first unwrapped so the original exception will be
+    presented in the rule, thus the `NativeEngineFailure` exception will not be seen in rule code.
+
+    This is different from the other `EngineError` based exceptions which doesn't originate from
+    rule code.
+
+    TODO: This type is defined in Python because pyo3 doesn't support declaring Exceptions with
+    additional fields. See https://github.com/PyO3/pyo3/issues/295
+    """
+
+    def __init__(self, msg: str, failure: PyFailure) -> None:
+        super().__init__(msg)
+        self.failure = failure
