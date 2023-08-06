09b5469462b62b90dc031e4e998588c8c93dc40b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:50 +0000

    Add package getlatency/1.1/getlatency-1.1.tar.gz

---
+++ packages/getlatency/getlatency-1.1.tar.gz/getlatency-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='getlatency',
+    version='1.1',
+    description='get latency module for you',
+    author='Skyler Palmer',
+    author_email='contact@skyler.co.uk'
+)
