7c100c719672920ba82dadbade3bd6995f26cb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:26 +0000

    Add package bpf4/1.8.2/bpf4-1.8.2-cp39-cp39-win_amd64.whl

---
+++ packages/bpf4/bpf4-1.8.2-cp39-cp39-win_amd64.whl/bpf4/__init__.py
@@ -0,0 +1,11 @@
+from . import core
+from .core import (
+    BpfInterface,
+    BpfBase,
+    blend
+)
+
+from .api import *
+from .config import CONFIG
+from .version import __version__
+from .util import asbpf
