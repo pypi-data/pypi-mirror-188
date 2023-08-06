23fd648125469c89e2339b476d7c80b4a98c55d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:16:52 +0000

    Add package slackker/1.0.1/slackker-1.0.1.tar.gz

---
+++ packages/slackker/slackker-1.0.1.tar.gz/slackker-1.0.1/slackker/__init__.py
@@ -0,0 +1,20 @@
+from slackker import callbacks
+from slackker.utils import checkker
+from slackker.utils import funckker
+
+__author__ = 'Siddhesh Gunjal'
+__email__ = 'siddhu19@live.com'
+__version__ = '1.0.1'
+
+# module level doc-string
+__doc__ = """
+slackker
+=====================================================================
+Description
+-----------
+slackker is a python package for monitoring your Model training status in real-time on slack channel.
+
+References
+----------
+https://github.com/siddheshgunjal/slackker
+"""
