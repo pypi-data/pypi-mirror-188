2ff15f6c79cbeca9bf31856dc18fdc82e674be9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:27 +0000

    Add package otterop-lang/0.1.0.post1/otterop_lang-0.1.0.post1-py3-none-any.whl

---
+++ packages/otterop-lang/otterop_lang-0.1.0.post1-py3-none-any.whl/otterop/lang/array.py
@@ -0,0 +1,23 @@
+from otterop_lang.string import String
+
+class Array:
+    def __init__(self, list):
+        self._wrapped = list
+
+    def get(self, i):
+        return self._wrapped[i]
+
+    def set(self, i, value):
+        self._wrapped[i] = value
+
+    def size(self):
+        return len(self._wrapped)
+
+    @staticmethod
+    def wrap(list):
+        return Array(list)
+
+    @staticmethod
+    def wrap_string(list):
+        list = [ String.wrap(s) for s in list ]
+        return Array.wrap(list)
