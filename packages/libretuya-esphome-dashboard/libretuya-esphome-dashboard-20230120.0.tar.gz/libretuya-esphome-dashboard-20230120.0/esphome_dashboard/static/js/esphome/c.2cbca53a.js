c4f124c8a87d10e40d52bfcfd2bb9f1b07249df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:20 +0000

    Add package libretuya-esphome-dashboard/20230120.0/libretuya-esphome-dashboard-20230120.0.tar.gz

---
+++ packages/libretuya-esphome-dashboard/libretuya-esphome-dashboard-20230120.0.tar.gz/libretuya-esphome-dashboard-20230120.0/esphome_dashboard/static/js/esphome/c.2cbca53a.js
@@ -0,0 +1,25 @@
+import{_ as o,d as t,a as s,w as e,n as i,y as r}from"./c.998383f4.js";import"./c.79283163.js";import{o as a}from"./c.bc0ca5c5.js";import{o as n}from"./c.fae1e983.js";import"./c.5b9dec44.js";import"./c.abbbe9de.js";import"./c.fa668126.js";import"./c.1a700628.js";let c=class extends i{render(){return r`
+      <esphome-process-dialog
+        always-show-close
+        .heading=${`Logs ${this.configuration}`}
+        .type=${"logs"}
+        .spawnParams=${{configuration:this.configuration,port:this.target}}
+        @closed=${this._handleClose}
+        @process-done=${this._handleProcessDone}
+      >
+        <mwc-button
+          slot="secondaryAction"
+          dialogAction="close"
+          label="Edit"
+          @click=${this._openEdit}
+        ></mwc-button>
+        ${void 0===this._result||0===this._result?"":r`
+              <mwc-button
+                slot="secondaryAction"
+                dialogAction="close"
+                label="Retry"
+                @click=${this._handleRetry}
+              ></mwc-button>
+            `}
+      </esphome-process-dialog>
+    `}_openEdit(){a(this.configuration)}_handleProcessDone(o){this._result=o.detail}_handleRetry(){n(this.configuration,this.target)}_handleClose(){this.parentNode.removeChild(this)}};o([t()],c.prototype,"configuration",void 0),o([t()],c.prototype,"target",void 0),o([s()],c.prototype,"_result",void 0),c=o([e("esphome-logs-dialog")],c);
---
+++ packages/libretuya-esphome-dashboard/libretuya-esphome-dashboard-20230120.0.tar.gz/libretuya-esphome-dashboard-20230120.0/esphome_dashboard/static/js/esphome/c.fa668126.js
@@ -0,0 +1,1 @@
+import{f as r}from"./c.998383f4.js";const t=()=>r("./serial-ports").then((r=>r.filter((r=>"OTA"!==r.port))));export{t as g};
