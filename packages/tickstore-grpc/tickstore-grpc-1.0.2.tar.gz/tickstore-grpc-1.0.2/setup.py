e4194335b877b8933ffd90e2f8dfcfd676cf6d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:47 +0000

    Add package tickstore-grpc/1.0.2/tickstore-grpc-1.0.2.tar.gz

---
+++ packages/tickstore-grpc/tickstore-grpc-1.0.2.tar.gz/tickstore-grpc-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import pathlib
+
+setup(
+    name='tickstore-grpc',
+    version='1.0.2',
+    packages=[''],
+    license='copyright',
+    author='Alphaticks',
+    description='grpc bindings',
+    long_description_content_type="text/markdown",
+    install_requires=[]
+)
