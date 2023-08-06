b5d607dbf90221922e6d8258c34d7b8bf457d527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:41 +0000

    Add package packagesorry/0.1.1/packagesorry-0.1.1.tar.gz

---
+++ packages/packagesorry/packagesorry-0.1.1.tar.gz/packagesorry-0.1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='packagesorry',
+version='0.1.1',
+description="This code with harry",
+long_description='This is very very long description',
+author='Harry',
+instaall_require=[])
