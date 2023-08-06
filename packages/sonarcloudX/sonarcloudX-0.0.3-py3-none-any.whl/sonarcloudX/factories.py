9dc3c95a14b0c55f6887cf480aa73e173575bcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package sonarcloudX/0.0.3/sonarcloudX-0.0.3-py3-none-any.whl

---
+++ packages/sonarcloudX/sonarcloudX-0.0.3-py3-none-any.whl/sonarcloudX/factories.py
@@ -0,0 +1,13 @@
+
+import factory
+from .project import Project
+
+
+class ProjectFactory(factory.Factory):
+    
+    class Meta:
+        model = Project
+        
+    personal_access_token = None
+    sonar_url = None
+
