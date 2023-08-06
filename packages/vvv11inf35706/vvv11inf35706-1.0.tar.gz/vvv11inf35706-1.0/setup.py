62842925c4419bb9efde27ff7ef79a3299baa86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:02 +0000

    Add package vvv11inf35706/1.0/vvv11inf35706-1.0.tar.gz

---
+++ packages/vvv11inf35706/vvv11inf35706-1.0.tar.gz/vvv11inf35706-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='vvv11inf35706',
+	version='1.0',
+	author='vvv06357',
+	author_email='valery.ovechey@gmail.com',
+	description='flskdzhgby',
+	packages=['vvv11inf35706'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
