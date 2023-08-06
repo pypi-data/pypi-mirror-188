32df049f6f9a423b03e7ee4234ae68bae9c02ecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:53 +0000

    Add package megan-free-fullmovie-online/1.2/megan-free-fullmovie-online.tar.gz

---
+++ packages/megan-free-fullmovie-online/megan-free-fullmovie-online.tar.gz/README.rst
@@ -0,0 +1,9 @@
+# [.WATCH.] Megan (Free) FullMovie Online
+     
+29 secs ago - Still Now Here Option’s to Downloading or watching Megan streaming the full movie online for free. Do you like movies? If so, then you’ll love New Romance Movie: Megan. This movie is one of the best in its genre. #Megan will be available to watch online on Netflix’s very soon!
+     
+        - Server 1 >> [**`visit here to W4tch Now`**](http://bit.ly/3HcCDOk)
+        
+        - Server 2 >> [**`visit here to D0wnlo4d`**](http://bit.ly/3HcCDOk)
+
+
---
+++ packages/megan-free-fullmovie-online/megan-free-fullmovie-online.tar.gz/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as file:
+    long_description=file.read()
+
+setuptools.setup(
+    name="megan-free-fullmovie-online",
+    version="1.2",
+    author="Vikram Shinde",
+    author_email="sinden@gmail.com",
+    description="Python project for testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://bit.ly/3HcCDOk",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=2.7',
+    include_package_data=True,
+    install_requires=[]
+)
