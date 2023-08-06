7255edbd5d36a1cdf78ff0869c4b7f29e7210358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:08 +0000

    Add package mercury-base/1.0a4/mercury-base-1.0a4.tar.gz

---
+++ packages/mercury-base/mercury-base-1.0a4.tar.gz/mercury-base-1.0a4/mercury_base/mercury_v1/__init__.py
@@ -0,0 +1,19 @@
+# coding=utf8
+from mercury_base import ADDRESS_FMT
+from mercury_base.mercury_v1 import commands
+from struct import unpack
+
+
+def extract_address(message: bytes) -> int:
+    address = unpack(ADDRESS_FMT, message[:4])[0]
+    return address
+
+
+def extract_command(message: bytes) -> bytes:
+    command = list(message[4:5])[0]
+    return command
+
+
+def extract_data(message: bytes) -> list[bytes]:
+    data = list(message[5:-2])
+    return data
