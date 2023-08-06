f398eed1f0220ec6d2398e5c4bd9c312c34a1cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:38 +0000

    Add package simplepydeepqlearning/1.0/simplepydeepqlearning-1.0.tar.gz

---
+++ packages/simplepydeepqlearning/simplepydeepqlearning-1.0.tar.gz/simplepydeepqlearning-1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='simplepydeepqlearning',
+    version='1.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/AdrienDumontet',
+    license='Let the package like is it',
+    author_email='',
+    author='LeLaboDuGame, https://twitch.tv/LeLaboDuGame',
+    description='A simple python lib to do Deep Q Learning',
+    install_requires=[
+        "numpy",
+        "random",
+        "tqdm",
+        "simplepyai"
+    ]
+)
