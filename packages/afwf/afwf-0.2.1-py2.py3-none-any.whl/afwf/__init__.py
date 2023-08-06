f6253b8aff4b31924a57cec145e06b006352dd26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:04 +0000

    Add package afwf/0.2.1/afwf-0.2.1-py2.py3-none-any.whl

---
+++ packages/afwf/afwf-0.2.1-py2.py3-none-any.whl/afwf/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Alfred Workflow Script Filter power tool.
+"""
+
+from ._version import __version__
+
+__short_description__ = "Alfred Workflow Script Filter power tool.."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+try:
+    from .item import (
+        Icon,
+        Text,
+        Item,
+        VarKeyEnum,
+        VarValueEnum,
+        ModEnum,
+    )
+    from .script_filter import ScriptFilter
+    from .handler import Handler
+    from .workflow import log_debug_info
+    from .workflow import Workflow
+    from .icon import IconFileEnum
+    from .query import Query
+    from .query import QueryParser
+except ImportError as e:
+    print(e)
