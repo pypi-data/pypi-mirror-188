791a53c8743f1b585f916b795b55b8cffc8addee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:04 +0000

    Add package sleapyfaces/1.2.3/sleapyfaces-1.2.3-py3-none-any.whl

---
+++ packages/sleapyfaces/sleapyfaces-1.2.3-py3-none-any.whl/sleapyfaces/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2022-present Annie Ehler <annie.ehler.4@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from sleapyfaces.__about__ import *
+
+from sleapyfaces import base
+from sleapyfaces import files
+from sleapyfaces.utils import structs
+from sleapyfaces.utils import normalize
+from sleapyfaces import utils
+from sleapyfaces import clustering
+from sleapyfaces import docstring
+from sleapyfaces import config
