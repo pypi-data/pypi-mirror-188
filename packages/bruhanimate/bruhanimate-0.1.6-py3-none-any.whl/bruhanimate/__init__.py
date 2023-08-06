fdfc080ef488da4e4f3878feaf53cda9bd7418f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:36 +0000

    Add package bruhanimate/0.1.6/bruhanimate-0.1.6-py3-none-any.whl

---
+++ packages/bruhanimate/bruhanimate-0.1.6-py3-none-any.whl/bruhanimate/__init__.py
@@ -0,0 +1,26 @@
+import sys
+from bruhanimate.bruhscreen import Screen
+from bruhanimate.bruheffects    import BaseEffect, StaticEffect, OffsetEffect, NoiseEffect, StarEffect, PlasmaEffect, GameOfLifeEffect, RainEffect
+from bruhanimate.bruhffer       import Buffer
+from bruhanimate.bruhrenderer   import BaseRenderer, EffectRenderer, CenterRenderer, PanRenderer
+from bruhanimate import images
+from bruhanimate import demo
+
+__all__ = [
+    "Screen",
+    "demo"
+    "BaseEffect",
+    "StaticEffect",
+    "OffsetEffect", 
+    "NoiseEffect", 
+    "StarEffect",
+    "PlasmaEffect",
+    "GameOfLifeEffect",
+    "RainEffect",
+    "Buffer",
+    "BaseRenderer",
+    "EffectRenderer",
+    "CenterRenderer",
+    "PanRenderer",
+    "images"
+]
