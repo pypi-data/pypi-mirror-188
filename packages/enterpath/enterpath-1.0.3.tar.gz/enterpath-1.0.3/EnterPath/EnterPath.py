9f9b295de3d1f60e4b5fd62050ded915f67c72b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:53 +0000

    Add package enterpath/1.0.3/enterpath-1.0.3.tar.gz

---
+++ packages/enterpath/enterpath-1.0.3.tar.gz/enterpath-1.0.3/EnterPath/EnterPath.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+def enter_path():
+    """
+    Enter current python file .py path
+    """
+    path = sys.argv[0]
+    path2 = os.path.split(path)[0]
+    if path2 != '':
+        os.chdir(path2)
+    print("Current Path is {}".format(os.getcwd()))
+    return os.getcwd()
---
+++ packages/enterpath/enterpath-1.0.3.tar.gz/enterpath-1.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='enterpath',
+    version='1.0.3',
+    description="Auto enter path function",
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    author='Lucy Situ',
+    author_email='lucy.situ@byd.com',
+    maintainer='Lucy Situ',
+    maintainer_email='lucy.situ@byd.com',
+    license='MIT License',
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.9',
+    install_requires=[''],
+    entry_points={
+        'console_scripts': [
+            ''
+        ],
+    },
+)
