0278bd4c222cf1feb00939a86fa2773ac3e146a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:33 +0000

    Add package promptlayer/0.1.32/promptlayer-0.1.32.tar.gz

---
+++ packages/promptlayer/promptlayer-0.1.32.tar.gz/promptlayer-0.1.32/promptlayer/promptlayer.py
@@ -0,0 +1,44 @@
+import requests
+import datetime
+
+class PromptLayer(object):
+    __slots__ = ["_obj", "__weakref__", "_function_name"]
+
+    def __init__(self, obj, function_name=""):
+        object.__setattr__(self, "_obj", obj)
+        object.__setattr__(self, "_function_name", function_name)
+
+    def __getattr__(self, name):
+        return PromptLayer(
+            getattr(object.__getattribute__(self, "_obj"), name),
+            function_name=f'{object.__getattribute__(self, "_function_name")}.{name}',
+        )
+
+    def __delattr__(self, name):
+        delattr(object.__getattribute__(self, "_obj"), name)
+
+    def __setattr__(self, name, value):
+        setattr(object.__getattribute__(self, "_obj"), name, value)
+
+    def __call__(self, *args, **kwargs):
+        from promptlayer.utils import get_api_key
+        tag = kwargs.pop("pl_tags", None)
+        request_start_time = datetime.datetime.now().timestamp()
+        response = object.__getattribute__(self, "_obj")(*args, **kwargs)
+        request_end_time = datetime.datetime.now().timestamp()
+        request_response = requests.post(
+            "https://api.promptlayer.com/track-request",
+            json={
+                "function_name": object.__getattribute__(self, "_function_name"),
+                "args": args,
+                "kwargs": kwargs,
+                "tags": tag,
+                "request_response": response,
+                "request_start_time": request_start_time,
+                "request_end_time": request_end_time,
+                "api_key": get_api_key(),
+            },
+        )
+        if request_response.status_code != 200:
+            raise Exception(f"Error while tracking request: {request_response.json().get('message')}")
+        return response
