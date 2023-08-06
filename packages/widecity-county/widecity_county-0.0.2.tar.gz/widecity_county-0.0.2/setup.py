16355d0f9425b19ecd8d1ff25e8d4d5774b982d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:31 +0000

    Add package widecity-county/0.0.2/widecity_county-0.0.2.tar.gz

---
+++ packages/widecity-county/widecity_county-0.0.2.tar.gz/widecity_county-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'a simple counter'
+LONG_DESCRIPTION = 'count from 1 to 10.'
+
+# Setting up
+setup(
+    name="widecity_county",
+    version=VERSION,
+    author="AMAL BENNY",
+    author_email="<amalpullan4@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'counter', 'county', 'counter-county', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
