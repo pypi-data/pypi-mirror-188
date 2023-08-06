b166d91f54241ede88ab7fea0e5f7b2a778a2c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:33 +0000

    Add package Topsis-hasika-102003542/0.1.0/Topsis-hasika-102003542-0.1.0.tar.gz

---
+++ packages/Topsis-hasika-102003542/Topsis-hasika-102003542-0.1.0.tar.gz/Topsis-hasika-102003542-0.1.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+def readme():
+    with open('README.md') as file:
+        README = file.read()
+    return README
+
+setuptools.setup(
+    name="Topsis-hasika-102003542",
+    version="0.1.0",
+    description="A Python package for implementing TOPSIS technique.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author="Hasika Tandon",
+    author_email="htandon_be20@thapar.edu",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=["src"],
+    include_package_data=True,
+    install_requires=['scipy',
+                      'tabulate',
+                      'numpy',
+                      'pandas',
+     ],
+     entry_points={
+        "console_scripts": [
+            "topsis=src.topsis:main",
+        ]
+     },
+)
