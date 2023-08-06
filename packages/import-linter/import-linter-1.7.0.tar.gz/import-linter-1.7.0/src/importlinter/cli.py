076b98eb3c6c9a2b28552c82ac01dc2ccc872ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:27 +0000

    Add package import-linter/1.7.0/import-linter-1.7.0.tar.gz

---
+++ packages/import-linter/import-linter-1.7.0.tar.gz/import-linter-1.7.0/src/importlinter/cli.py
@@ -0,0 +1,93 @@
+import os
+import sys
+from typing import Optional, Tuple
+
+import click
+
+from . import configuration
+from .application import use_cases
+
+configuration.configure()
+
+EXIT_STATUS_SUCCESS = 0
+EXIT_STATUS_ERROR = 1
+
+
+@click.command()
+@click.option("--config", default=None, help="The config file to use.")
+@click.option(
+    "--contract",
+    default=(),
+    multiple=True,
+    help="Limit the check to the supplied contract identifier. May be passed multiple times.",
+)
+@click.option("--debug", is_flag=True, help="Run in debug mode.")
+@click.option(
+    "--show-timings",
+    is_flag=True,
+    help="Show times taken to build the graph and to check each contract.",
+)
+@click.option(
+    "--verbose",
+    is_flag=True,
+    help="Noisily output progress as we go along.",
+)
+def lint_imports_command(
+    config: Optional[str],
+    contract: Tuple[str, ...],
+    debug: bool,
+    show_timings: bool,
+    verbose: bool,
+) -> int:
+    """
+    Check that a project adheres to a set of contracts.
+    """
+    exit_code = lint_imports(
+        config_filename=config,
+        limit_to_contracts=contract,
+        is_debug_mode=debug,
+        show_timings=show_timings,
+        verbose=verbose,
+    )
+    sys.exit(exit_code)
+
+
+def lint_imports(
+    config_filename: Optional[str] = None,
+    limit_to_contracts: Tuple[str, ...] = (),
+    is_debug_mode: bool = False,
+    show_timings: bool = False,
+    verbose: bool = False,
+) -> int:
+    """
+    Check that a project adheres to a set of contracts.
+
+    This is the main function that runs the linter.
+
+    Args:
+        config_filename:    the filename to use to parse user options.
+        limit_to_contracts: if supplied, only lint the contracts with the supplied ids.
+        is_debug_mode:      whether debugging should be turned on. In debug mode, exceptions are
+                            not swallowed at the top level, so the stack trace can be seen.
+        show_timings:       whether to show the times taken to build the graph and to check
+                            each contract.
+        verbose:            if True, noisily output progress as it goes along.
+
+    Returns:
+        EXIT_STATUS_SUCCESS or EXIT_STATUS_ERROR.
+    """
+    # Add current directory to the path, as this doesn't happen automatically.
+    sys.path.insert(0, os.getcwd())
+
+    passed = use_cases.lint_imports(
+        config_filename=config_filename,
+        limit_to_contracts=limit_to_contracts,
+        is_debug_mode=is_debug_mode,
+        show_timings=show_timings,
+        verbose=verbose,
+    )
+
+    if passed:
+        return EXIT_STATUS_SUCCESS
+    else:
+        return EXIT_STATUS_ERROR
