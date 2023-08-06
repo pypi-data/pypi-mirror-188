dfeb644ce4b2a5f42ec9caa10930d852f4fa062c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:00 +0000

    Add package markdown-pytest/0.2.0/markdown_pytest-0.2.0.tar.gz

---
+++ packages/markdown-pytest/markdown_pytest-0.2.0.tar.gz/markdown_pytest-0.2.0/markdown_pytest.py
@@ -0,0 +1,141 @@
+from itertools import groupby
+from pathlib import Path
+from types import CodeType
+from typing import Iterable, Iterator, Mapping, NamedTuple, Optional, Tuple
+
+import pytest
+
+
+class CodeBlock(NamedTuple):
+    start_line: int
+    lines: Tuple[str, ...]
+    arguments: Tuple[Tuple[str, str], ...]
+    path: str
+    name: str
+
+    @property
+    def end_line(self) -> int:
+        return self.start_line + len(self.lines)
+
+
+def parse_code_blocks(fspath) -> Iterator[CodeBlock]:
+    with open(fspath, "r") as fp:
+        lines = list(enumerate(fp.read().splitlines()))
+        index = -1
+
+        def parse_arguments(lines: Iterable[str]) -> Mapping[str, str]:
+            result = {}
+            if not lines:
+                return result
+
+            args = "".join(
+                "".join(lines).strip()[5:-3].splitlines(),
+            ).split(";")
+
+            for arg in args:
+                if ":" not in arg:
+                    continue
+
+                key, value = arg.split(":", 1)
+                result[key.strip()] = value.strip()
+
+            return result
+
+        while index < len(lines):
+            index += 1
+
+            if index > (len(lines) - 1):
+                return
+
+            lineno, line = lines[index]
+
+            if not line.rstrip().endswith("```python"):
+                continue
+
+            if index > 0 and lines[index - 1][1].endswith("-->"):
+                arguments_lines = []
+                for i in range(index - 1, -1, -1):
+                    arguments_lines.append(lines[i][1])
+                    if lines[i][1].startswith("<!---"):
+                        break
+
+                arguments = parse_arguments(arguments_lines[::-1])
+            else:
+                arguments = {}
+
+            # the next line after ```python
+            start_lineno = lineno + 1
+            code_lines = []
+
+            while True:
+                index += 1
+                lineno, line = lines[index]
+
+                if line.rstrip().startswith("```"):
+                    break
+
+                code_lines.append(line)
+
+            if not arguments or "name" not in arguments:
+                continue
+
+            yield CodeBlock(
+                start_line=start_lineno,
+                lines=tuple(code_lines),
+                arguments=tuple(arguments.items()),
+                path=str(fspath),
+                name=arguments.pop("name"),
+            )
+
+
+class MDTestItem(pytest.Item):
+    def __init__(self, name: str, parent: "MDModule", code: CodeType):
+        super().__init__(name=name, parent=parent)
+        self.module = code
+
+    def runtest(self) -> None:
+        exec(self.module, {"__name__": "markdown-pytest"})
+
+
+def compile_code_blocks(*blocks: CodeBlock) -> Optional[CodeType]:
+    blocks = sorted(blocks, key=lambda x: x.start_line)
+    if not blocks:
+        return None
+    lines = [""] * blocks[-1].end_line
+    path = blocks[0].path
+    for block in blocks:
+        lines[block.start_line:block.end_line] = block.lines
+    return compile(source="\n".join(lines), mode="exec", filename=path)
+
+
+class MDModule(pytest.Module):
+    def collect(self) -> Iterable["MDTestItem"]:
+        test_prefix = self.config.getoption("--md-prefix")
+
+        for test_name, blocks in groupby(
+            parse_code_blocks(self.fspath),
+            key=lambda x: x.name,
+        ):
+            if not test_name.startswith(test_prefix):
+                continue
+
+            blocks = list(blocks)
+            code = compile_code_blocks(*blocks)
+            if code is None:
+                continue
+
+            yield MDTestItem.from_parent(name=test_name, parent=self, code=code)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--md-prefix", default="test",
+        help="Markdown test code-block prefix from comment",
+    )
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_collect_file(path, parent: pytest.Collector) -> Optional[MDModule]:
+    if path.ext.lower() not in (".md", ".markdown"):
+        return None
+    return MDModule.from_parent(parent=parent, path=Path(path))
---
+++ packages/markdown-pytest/markdown_pytest-0.2.0.tar.gz/markdown_pytest-0.2.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['markdown_pytest']
+entry_points = \
+{'pytest11': ['markdown-pytest = markdown_pytest']}
+
+setup_kwargs = {
+    'name': 'markdown-pytest',
+    'version': '0.2.0',
+    'description': 'Pytest plugin for runs tests directly from Markdown files',
+    'long_description': 'markdown-pytest\n===============\n\nPytest plugin for running tests directly from Markdown files.\n\nMarkdown:\n\n````markdown\n<!--- name: test_assert_true -->\n```python\nassert True\n```\n````\n\n<details>\n<summary>Will be shown as</summary>\n\n<!--- name: test_assert_true -->\n```python\nassert True\n```\n\n</details>\n\nCode split\n----------\n\nYou can split test to the multiple blocks with the same test name:\n\nMarkdown:\n\n````markdown\nThis block performs import:\n\n<!--- name: test_example -->\n```python\nfrom itertools import chain\n```\n\n`chain` usage example:\n\n<!--- name: test_example -->\n```python\nassert list(chain(range(2), range(2))) == [0, 1, 0, 1]\n```\n````\n\n<details>\n<summary>Will be shown as</summary>\n\nThis block performs import:\n\n<!--- name: test_example -->\n```python\nfrom itertools import chain\n```\n\n`chain` usage example:\n\n<!--- name: test_example -->\n```python\nassert list(chain(range(2), range(2))) == [0, 1, 0, 1]\n```\n\n</details>\n\nFictional Code Examples\n-----------------------\n\nCode without `&#60;!--- name: test_name --&#62;` comment will not be executed.\n\n````markdown\n```python\nfrom universe import antigravity, WrongPlanet\n\ntry:\n    antigravity()\nexcept WrongPlanet:\n    print("You are on the wrong planet.")\n    exit(1)\n```\n````\n\n<details>\n<summary>Will be shown as</summary>\n\n```python\nfrom universe import antigravity, WrongPlanet\n\ntry:\n    antigravity()\nexcept WrongPlanet:\n    print("You are on the wrong planet.")\n    exit(1)\n```\n</details>\n\nUsage example\n-------------\n\nThis README.md file might be tested like this:\n\n```bash\n$ pytest -v README.md\n======================= test session starts =======================\nplatform darwin -- Python 3.10.2, pytest-7.2.0, pluggy-1.0.0\nplugins: markdown-pytest-0.1.0\ncollected 2 items\n\nREADME.md::test_assert_true PASSED                                                                                                                                                                                                     [ 50%]\nREADME.md::test_example PASSED\n```\n',
+    'author': 'Dmitry Orlov',
+    'author_email': 'me@mosquito.su',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/mosquito/markdown-pytest',
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
