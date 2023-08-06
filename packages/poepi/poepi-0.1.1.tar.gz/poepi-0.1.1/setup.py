aa19ab15d49ab03bf82dd0877ef8199990cad83e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:43 +0000

    Add package poepi/0.1.1/poepi-0.1.1.tar.gz

---
+++ packages/poepi/poepi-0.1.1.tar.gz/poepi-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poepi']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'poepi',
+    'version': '0.1.1',
+    'description': 'Poepi package',
+    'long_description': '',
+    'author': 'vodovi',
+    'author_email': 'vodovi4524@crtsec.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
