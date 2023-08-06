dd24d3095d828a12e900084e63fe867c96bdff7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:32 +0000

    Add package fast-ensemble/0.0.5/fast-ensemble-0.0.5.tar.gz

---
+++ packages/fast-ensemble/fast-ensemble-0.0.5.tar.gz/fast-ensemble-0.0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="fast-ensemble",
+    version="0.0.5",
+    author="leffff",
+    author_email="levnovitskiy@gmail.com",
+    description="Library for high level model ensembling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leffff/fast-ensemble",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "catboost",
+        "xgboost",
+        "lightgbm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
