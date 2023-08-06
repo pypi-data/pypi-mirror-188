877f72870098f13a634a8a1d9898377040219ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:16 +0000

    Add package webviz-subsurface-components/0.4.15rc2/webviz_subsurface_components-0.4.15rc2.tar.gz

---
+++ packages/webviz-subsurface-components/webviz_subsurface_components-0.4.15rc2.tar.gz/webviz_subsurface_components-0.4.15rc2/react/src/lib/components/ColorLegends/WebVizContinuousLegend.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ContinuousLegend } from "@emerson-eps/color-tables";
+import { colorTablesArray } from "@emerson-eps/color-tables/";
+
+interface LegendProps {
+    title?: string;
+    min: number;
+    max: number;
+    cssLegendStyles?: Record<string, unknown>;
+    colorName: string;
+    horizontal?: boolean | null;
+    colorTables: colorTablesArray | string | undefined;
+}
+
+const ContinuousLegendWrapper: React.FC<LegendProps> = ({
+    title,
+    min,
+    max,
+    cssLegendStyles,
+    colorName,
+    horizontal,
+    colorTables,
+}) => {
+    return (
+        <ContinuousLegend
+            min={min}
+            max={max}
+            dataObjectName={title}
+            cssLegendStyles={cssLegendStyles}
+            colorName={colorName}
+            horizontal={horizontal}
+            colorTables={colorTables}
+        />
+    );
+};
+
+ContinuousLegendWrapper.propTypes = {
+    title: PropTypes.string,
+    min: PropTypes.number.isRequired,
+    max: PropTypes.number.isRequired,
+    cssLegendStyles: PropTypes.objectOf(PropTypes.string),
+    colorName: PropTypes.string.isRequired,
+    horizontal: PropTypes.bool,
+    colorTables: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+};
+
+export default ContinuousLegendWrapper;
