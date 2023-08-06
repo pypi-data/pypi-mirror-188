d3143d2555a393456a2cdf4127d1328a3c383681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package zeno-etl-libs-v3/1.0.15/zeno_etl_libs_v3-1.0.15-py3-none-any.whl

---
+++ packages/zeno-etl-libs-v3/zeno_etl_libs_v3-1.0.15-py3-none-any.whl/zeno_etl_libs/helper/aws/dms.py
@@ -0,0 +1,25 @@
+import boto3
+
+from zeno_etl_libs.config.common import Config
+
+
+class DMS:
+    """
+    Helper class to integrate with AWS DMS
+    """
+
+    def __init__(self):
+        configobj = Config.get_instance()
+        self.secrets = configobj.get_secrets()
+        self.client = boto3.client('dms')
+
+    def start(self, task_id):
+        arn = f"arn:aws:dms:{self.secrets['AWS_REGION']}:{self.secrets['AWS_ACCOUNT_ID']}:task:{task_id}"
+        return self.client.start_replication_task(
+            ReplicationTaskArn=arn,
+            StartReplicationTaskType='resume-processing'
+        )
+
+    def stop(self, task_id):
+        arn = f"arn:aws:dms:{self.secrets['AWS_REGION']}:{self.secrets['AWS_ACCOUNT_ID']}:task:{task_id}"
+        return self.client.stop_replication_task(ReplicationTaskArn=arn)
