53384687485366dac8b8b48f09202c229d106970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:01 +0000

    Add package genr/0.1.1/Genr-0.1.1-py3-none-any.whl

---
+++ packages/genr/Genr-0.1.1-py3-none-any.whl/Genr/test.py
@@ -0,0 +1,18 @@
+from genr import OpenAI
+
+
+#With default parameters
+
+api_key = "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"
+openai = OpenAI(api_key)
+text = "OpenAI is an artificial intelligence research laboratory consisting of the for-profit OpenAI LP and its parent company, the non-profit OpenAI Inc. The company is dedicated to promoting and developing friendly AI in a way that benefits humanity as a whole."
+summary = openai.generate_questions(text, model="text-davinci-003", temperature=1)
+print(summary)
+
+#With additional parameters
+
+# api_key = "<API-KEY>"
+# openai = OpenAIWrapper(api_key)
+# text = "OpenAI is an artificial intelligence research laboratory consisting of the for-profit OpenAI LP and its parent company, the non-profit OpenAI Inc. The company is dedicated to promoting and developing friendly AI in a way that benefits humanity as a whole."
+# summary = openai.get_summary(text, temperature=0.7, max_tokens=120, model="text-davinci-003")
+# print(text)
