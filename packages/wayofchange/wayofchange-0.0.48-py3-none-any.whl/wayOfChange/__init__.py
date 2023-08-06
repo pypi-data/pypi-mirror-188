5c6e9db8e53e64259f84fc11b22edac468e89063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:24 +0000

    Add package wayofchange/0.0.48/wayofchange-0.0.48-py3-none-any.whl

---
+++ packages/wayofchange/wayofchange-0.0.48-py3-none-any.whl/wayOfChange/__init__.py
@@ -0,0 +1,11 @@
+#__init__.py
+import sys, os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from .Key import Key
+from .JazzNote import JazzNote
+from .Book import Book
+from .Change import Change
+from .Utility import Utility
+from .Graphics import Graphics
+from .FCircle import FCircle
+__all__ = ["Key","JazzNote", "Utility","Change",'Book', 'Graphics','FCircle']
