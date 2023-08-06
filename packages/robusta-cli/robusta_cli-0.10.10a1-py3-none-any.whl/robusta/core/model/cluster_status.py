3643fc25fc9b8c9d1dff6145d13d82b9a591c6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:02 +0000

    Add package robusta-cli/0.10.10a1/robusta_cli-0.10.10a1-py3-none-any.whl

---
+++ packages/robusta-cli/robusta_cli-0.10.10a1-py3-none-any.whl/robusta/core/model/cluster_status.py
@@ -0,0 +1,9 @@
+from pydantic.main import BaseModel, List, Optional
+
+
+class ClusterStatus(BaseModel):
+    account_id: str
+    cluster_id: str
+    version: str
+    last_alert_at: Optional[str]  # ts
+    light_actions: List[str]
