d1f4ae2ab7679a637131be3891e896d9d584a7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:25 +0000

    Add package woche/0.1/woche-0.1.tar.gz

---
+++ packages/woche/woche-0.1.tar.gz/woche-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='woche',
+    version='0.1',
+    author='HEEDEGIT',
+    author_email='follynamylus@gmail.com',
+    packages=['Woche'],
+    install_requires=["Pandas","Streamlit","Python"]
+)
