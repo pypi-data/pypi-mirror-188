623c9df6a1d40c26cd16da73d5e055c1a2948d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:12 +0000

    Add package xeye/0.35/xeye-0.35.tar.gz

---
+++ packages/xeye/xeye-0.35.tar.gz/xeye-0.35/src/xeye/__init__.py
@@ -0,0 +1,7 @@
+from .dataset import Dataset
+from .fast_dataset import FastDataset
+from .manual_dataset import ManualDataset
+from .build_dataset import BuildDataset
+
+#version
+__version__ = "0.35"
