03df723c7f4c01206057afed6c47e199032f2f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:49 +0000

    Add package jtokengen/0.0.2/jtokengen-0.0.2-py2.py3-none-any.whl

---
+++ packages/jtokengen/jtokengen-0.0.2-py2.py3-none-any.whl/jtokengen/__init__.py
@@ -0,0 +1,10 @@
+"""
+tokengen
+
+Easier command token generator.
+"""
+
+__version__ = "0.0.1"
+__author__ = 'Edwin Ariza'
+__credits__ = 'Jumíchica Community'
+
---
+++ packages/jtokengen/jtokengen-0.0.2-py2.py3-none-any.whl/jtokengen/main.py
@@ -0,0 +1,17 @@
+from uuid import uuid4
+import sys
+
+class tokengen:
+    def start(self):
+        # GENERAMOS EL TOKEN DINÁMICAMENTE
+        rand_token = uuid4()
+        print(rand_token)
+
+        # OBTENEMOS EL PATH PARA EL ARCHIVO DE DONDE SE ALMACENA EL TOKEN
+        path = sys.argv[1]
+        print(path)
+
+        # GENERAMOS EL ARCHIVO DE TOKEN EN LA RUTA ESTABLECIDA
+        oftoken = open(path, "w+")
+        oftoken.write(str(rand_token))
+
