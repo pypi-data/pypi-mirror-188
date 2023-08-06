a833fbbb780c8332d224ed0ef153cfc4e2da712c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:46 +0000

    Add package PyQt-Fluent-Widgets/0.2.1/PyQt-Fluent-Widgets-0.2.1.tar.gz

---
+++ packages/PyQt-Fluent-Widgets/PyQt-Fluent-Widgets-0.2.1.tar.gz/PyQt-Fluent-Widgets-0.2.1/qfluentwidgets/components/widgets/__init__.py
@@ -0,0 +1,7 @@
+from .menu import DWMMenu, LineEditMenu
+from .slider import Slider, HollowHandleStyle
+from .stacked_widget import PopUpAniStackedWidget, OpacityAniStackedWidget
+from .switch_button import SwitchButton, IndicatorPosition
+from .state_tool_tip import StateToolTip, ToastToolTip
+from .scroll_area import ScrollArea, SmoothMode, SmoothScrollArea, SmoothScrollBar
+from .tool_tip import ToolTip
