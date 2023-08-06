accabd78935f13721cb3b457417a177c0516a99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:48 +0000

    Add package sapiadapter/0.38.394/sapiadapter-0.38.394.tar.gz

---
+++ packages/sapiadapter/sapiadapter-0.38.394.tar.gz/sapiadapter-0.38.394/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sapiadapter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'sapiadapter',
+    'version': '0.38.394',
+    'description': '',
+    'long_description': '',
+    'author': 'Zafar Iqbal',
+    'author_email': 'zaf@sparc.space',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.12',
+}
+
+
+setup(**setup_kwargs)
