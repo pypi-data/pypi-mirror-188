9c39f67d9eb5b66a5b9d8902a88097aee6889102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:49 +0000

    Add package pychromvar/0.0.2/pychromvar-0.0.2.tar.gz

---
+++ packages/pychromvar/pychromvar-0.0.2.tar.gz/pychromvar-0.0.2/pychromvar/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.2"
+__version_info__ = tuple([int(num) for num in __version__.split('.')])  # noqa: F401
+
+from .preprocessing import *
+from .match_motif import match_motif
+from .compute_deviations import compute_deviations
+from .get_genome import get_genome
+
