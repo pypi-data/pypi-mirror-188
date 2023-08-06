a6c774b018f10ab274e0f8b22b90df5260dfff19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package zpy-db-core/1.2.2/zpy-db-core-1.2.2.tar.gz

---
+++ packages/zpy-db-core/zpy-db-core-1.2.2.tar.gz/zpy-db-core-1.2.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zpy-db-core",
+    version="1.2.2",
+    author="Noé Cruz | linkedin.com/in/zurckz/",
+    author_email="zurckz.services@gmail.com",
+    description="Helper module for manage database connection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NoeCruzMW",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "zpy-api-core",
+        "mysql-connector-python",
+        "cx-Oracle",
+        "psycopg2"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
