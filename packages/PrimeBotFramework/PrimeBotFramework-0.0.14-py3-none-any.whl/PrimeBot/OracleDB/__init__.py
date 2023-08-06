f5ab2793acaa191f6f999432bd6d829b1f825d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:12 +0000

    Add package PrimeBotFramework/0.0.14/PrimeBotFramework-0.0.14-py3-none-any.whl

---
+++ packages/PrimeBotFramework/PrimeBotFramework-0.0.14-py3-none-any.whl/PrimeBot/OracleDB/__init__.py
@@ -0,0 +1,16 @@
+import cx_Oracle
+
+
+
+class OracleDB:
+    def __init__(self, dir_instant_client):
+        cx_Oracle.init_oracle_client(lib_dir=dir_instant_client)
+
+    def execute_query(self, user, password, dsn, encoding, query):
+        with cx_Oracle.connect(user=user, password=password,
+                               dsn=dsn,
+                               encoding=encoding) as connection:
+            cur = connection.cursor()
+            cur.execute(query)
+            res = cur.fetchall()
+        return res
