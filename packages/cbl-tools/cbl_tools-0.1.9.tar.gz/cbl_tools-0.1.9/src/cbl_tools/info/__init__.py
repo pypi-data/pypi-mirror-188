c91d8c9bd7bda894e9af322b94e079d1891ed758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package cbl-tools/0.1.9/cbl_tools-0.1.9.tar.gz

---
+++ packages/cbl-tools/cbl_tools-0.1.9.tar.gz/cbl_tools-0.1.9/src/cbl_tools/info/__init__.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))  # enables relative local imports
+
+import dataclasses
+
+#import info
+from info_impl import PlatformInfo, platform_info
+
+
+@dataclasses.dataclass
+class PackageInfo:
+    full_name: str
+    version: str
+
+
+def package_info2() -> PackageInfo:
+    return PackageInfo("cbl_test_package.info", "2.0.2")  # "1.0.1"
+
