28177087915d7426fb69f6b200798852af9e792b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:30 +0000

    Add package phoenixtesting/0.1.5/phoenixtesting-0.1.5.tar.gz

---
+++ packages/phoenixtesting/phoenixtesting-0.1.5.tar.gz/phoenixtesting-0.1.5/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='phoenixtesting',
+    version='0.1.5',
+    author='Prateek Joshi',
+    author_email='prateekjoshi2013@gmail.com',
+    description='Testing Package',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    )
