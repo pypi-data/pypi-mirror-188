150461ee679819fad2cb44d4138658f98437f00d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:42 +0000

    Add package winstools/0.0.12/winstools-0.0.12.tar.gz

---
+++ packages/winstools/winstools-0.0.12.tar.gz/winstools-0.0.12/$.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='winstools',
+	version='0.0.12',
+	packages=['winstools'],
+	package_dir={'winstools': '$'},
+	url='',
+	license='',
+	author='Winstang',
+	author_email='',
+	description='HoHo'
+)
---
+++ packages/winstools/winstools-0.0.12.tar.gz/winstools-0.0.12/$/aaa.py
@@ -0,0 +1,12 @@
+import zhconv
+
+from pysubs2 import SSAEvent
+
+
+class BBB:
+	@classmethod
+	def ccc(cls):
+		print('I am Winstang.')
+		pass
+
+	pass
