82b67d10c272802f5a5371c747ce07a27349bf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:19 +0000

    Add package id-extractor/0.0.5/id_extractor-0.0.5.tar.gz

---
+++ packages/id-extractor/id_extractor-0.0.5.tar.gz/id_extractor-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+requirements =[
+    'Pillow',
+    'numpy',
+    'tqdm',
+    'gdown',
+]
+
+setup(
+    name="id_extractor",
+    version="0.0.5",
+    author="Innerverz-by.JJY",
+    author_email="pensee0.0a@innerverz.com",
+    description="innerverz package - id extractor",
+    python_requires=">=3.6",
+    install_requires=requirements,
+    packages=find_packages()
+    
+    
+)
