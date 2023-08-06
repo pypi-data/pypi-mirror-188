d97508d913f662dfdce678f20a489cf373623658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:37 +0000

    Add package ckanext-transmute/1.5.8/ckanext-transmute-1.5.8.tar.gz

---
+++ packages/ckanext-transmute/ckanext-transmute-1.5.8.tar.gz/ckanext-transmute-1.5.8/ckanext/transmute/types.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import Any
+from typing_extensions import TypedDict
+
+from recordclass import RecordClass
+
+
+class TransmuteData(TypedDict):
+    data: dict[str, Any]
+    schema: dict[str, Any]
+
+
+class Field(RecordClass):
+    field_name: str
+    value: Any
+    type: str
+    data: dict[str, Any]
+
+
+MODE_COMBINE = "combine"
+MODE_FIRST_FILLED = "first-filled"
