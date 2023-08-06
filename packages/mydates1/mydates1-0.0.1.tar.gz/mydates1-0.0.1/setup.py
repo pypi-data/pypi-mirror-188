d165e72180406b38f0cf7bc613913e424629ab84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:00 +0000

    Add package mydates1/0.0.1/mydates1-0.0.1.tar.gz

---
+++ packages/mydates1/mydates1-0.0.1.tar.gz/mydates1-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='mydates1',
+    version='0.0.1',
+    author='jiangqingliu',
+    author_email='jiangqingliu822@gmail.com',
+    # url='www.baidu.com',
+    license='LICENSE',
+    packages=['mydates1'],
+    description='An example of building Python package.',
+    install_requires=[
+        # 'python>=3.6.0',
+        # 'pandas>=0.10.0'
+    ]
+)
