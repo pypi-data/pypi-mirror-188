25b5c5c69bfa7c46d0be7e8e329deb55d3344b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:11 +0000

    Add package PyGameAutoAndroid/0.0.15/PyGameAutoAndroid-0.0.15-py3-none-any.whl

---
+++ packages/PyGameAutoAndroid/PyGameAutoAndroid-0.0.15-py3-none-any.whl/PyGameAutoAndroid/__init__.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py
+# Time       ：2022.8.31 0:04
+# Author     ：Lex
+# HomePage   : lexgeeker.com
+# Email      : 2983997560@qq.com
+# Description：
+"""
+from .PyGameAutoAndroid import *
