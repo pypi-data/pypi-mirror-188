fb19676971b8ec0a5beae2b7f6067602313a0788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:15 +0000

    Add package vlpaket/0.1.0/vlpaket-0.1.0.tar.gz

---
+++ packages/vlpaket/vlpaket-0.1.0.tar.gz/vlpaket-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vlpaket',
+    version='0.1.0',
+    author='vadlap',
+    author_email='vadlaped@gmail.com',
+    description='Best package',
+    long_description=open('README.md').read(),
+    packages=find_packages()
+)
