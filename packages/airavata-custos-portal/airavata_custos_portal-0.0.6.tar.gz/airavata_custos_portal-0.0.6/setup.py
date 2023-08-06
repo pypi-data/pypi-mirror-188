fad071a317e69951423b32c8f816934dd713b1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:08 +0000

    Add package airavata-custos-portal/0.0.6/airavata_custos_portal-0.0.6.tar.gz

---
+++ packages/airavata-custos-portal/airavata_custos_portal-0.0.6.tar.gz/airavata_custos_portal-0.0.6/setup.py
@@ -0,0 +1,46 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name="airavata_custos_portal",
+    version="0.0.6",
+    url="https://github.com/apache/airavata-custos-portal",
+    author="Apache Software Foundation",
+    author_email="dev@airavata.apache.org",
+    description=(
+        "The Airavata Custos Portal SDK is a library that makes "
+        "it easier to develop Airavata Custos Portal customizations."
+    ),
+    long_description=read("README.md"),
+    long_description_content_type='text/markdown',
+    license="Apache License 2.0",
+    packages=['airavata_custos_portal', 'airavata_custos_portal.apps', 'airavata_custos_portal.apps.frontend',
+              'airavata_custos_portal.apps.api'],
+    package_data={'airavata_custos_portal.apps.frontend': ['static/**/*', 'templates/**/*']},
+    install_requires=[
+        "Django==3.2.16",
+        "django-webpack-loader==0.6.0",
+        "djangorestframework==3.14.0",
+        "requests==2.28.2",
+        "PyJWT==0.4.3",
+        "django-environ"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ]
+)
