f53c647af935906df1de3134350d2f03cb1126fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:01 +0000

    Add package zenimport/0.1.2/zenimport-0.1.2.tar.gz

---
+++ packages/zenimport/zenimport-0.1.2.tar.gz/zenimport-0.1.2/setup.py
@@ -0,0 +1,8 @@
+#https://www.freecodecamp.org/news/build-your-first-python-package/
+from setuptools import setup, find_packages
+
+setup(
+    name='zenimport',
+    version='0.1.2',
+    packages=find_packages()
+)
