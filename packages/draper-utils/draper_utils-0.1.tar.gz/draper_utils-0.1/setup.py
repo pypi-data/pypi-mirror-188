f3e450b4a6170cdd1cdf5cc11c20b58e0527ce84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:42 +0000

    Add package draper-utils/0.1/draper_utils-0.1.tar.gz

---
+++ packages/draper-utils/draper_utils-0.1.tar.gz/draper_utils-0.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+REQUIREMENTS = [
+    'django',
+]
+
+# https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS = []
+
+setup(
+    name='draper_utils',
+    version='0.1',
+    description='Project Management Module for Django - Utils',
+    author='siteshell.net',
+    author_email='pdbethke@siteshell.net',
+    url='https://github.com/pdbethke/project_center',
+    packages=find_packages(),
+    license='LICENSE.txt',
+    platforms=['OS Independent'],
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False,
+    # test_suite="test_settings.run",
+)
