cee77bfe95eefa8da5ee72bfc86e13295a6aa7e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:00 +0000

    Add package orm.base/1.0.1/orm.base-1.0.1-py3.7.egg

---
+++ packages/orm.base/orm.base-1.0.1-py3.7.egg/orm/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+    Module :mod:``
+
+
+    LICENSE: The End User license agreement is located at the entry level.
+
+"""
+
+__import__('pkg_resources').declare_namespace(__name__)
+
+
+# ----------- START: Native Imports ---------- #
+# ----------- END: Native Imports ---------- #
+
+# ----------- START: Third Party Imports ---------- #
+# ----------- END: Third Party Imports ---------- #
+
+# ----------- START: In-App Imports ---------- #
+# ----------- END: In-App Imports ---------- #
+
+__all__ = [
+    # All public symbols go here.
+]
+
