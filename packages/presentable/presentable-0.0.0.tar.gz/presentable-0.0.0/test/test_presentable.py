f79301442e9922cf9e8a6c4cd563de57d16b2744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:45 +0000

    Add package presentable/0.0.0/presentable-0.0.0.tar.gz

---
+++ packages/presentable/presentable-0.0.0.tar.gz/presentable-0.0.0/test/test_presentable.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import unittest
+
+# from presentable import presentable
+
+
+class Test_presentable(unittest.TestCase):
+    """
+    Test the various functionalities of presentable.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        return
+
+    def test_placeholder(self):
+        """
+        """
+        self.assertTrue(1+1 == 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
