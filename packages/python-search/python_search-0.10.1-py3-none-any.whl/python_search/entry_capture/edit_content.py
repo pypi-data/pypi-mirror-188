4833bb19736c7040c50de8d2693f62db18ff7196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:07 +0000

    Add package python-search/0.10.1/python_search-0.10.1-py3-none-any.whl

---
+++ packages/python-search/python_search-0.10.1-py3-none-any.whl/python_search/entry_capture/edit_content.py
@@ -0,0 +1,88 @@
+""" Module responsible for the logic of editing entry files """
+import logging
+import subprocess
+from typing import Optional
+
+from python_search.apps.terminal import Terminal
+from python_search.interpreter.cmd import CmdInterpreter
+
+
+class EditKey:
+    """
+    Set of commands to edit the _entries
+    """
+
+    def __init__(self, configuration):
+        self.configuration = configuration
+
+    def edit_key(self, key: str, dry_run=False):
+        """
+        Edits the _configuration files by searching the text
+        """
+        print(f"Editing key {key}")
+        if not key:
+            self.edit_default()
+            return
+
+        if not len(key):
+            print('Editing default')
+            self.edit_default()
+            return
+
+        # needs to be case insensitive search
+        cmd = f"ack -i '{key}' {self.configuration.get_project_root()} --py || true"
+        logging.info(f"Command: {cmd}")
+        result_shell = subprocess.check_output(cmd, shell=True, text=True)
+
+        if not result_shell:
+            logging.info("Could not find match edit main file")
+            self._edit_config(self.configuration.get_source_file(), dry_run)
+            return
+
+        file, line, *_ = result_shell.split(":")
+        print(f"Editng file and line {file}, {line}")
+
+        self._edit_config(file, line)
+
+    def search_entries_directory(self, key=None):
+        entry = {
+            "cli_cmd": f"fzf_directory.sh {self.configuration.get_project_root()}",
+            "directory": self.configuration.get_project_root(),
+        }
+
+        CmdInterpreter(entry).interpret_default()
+
+    def edit_default(self):
+        self._edit_config(self.configuration.get_project_root() + "/entries_main.py")
+
+    def _edit_config(self, file_name: str, line: Optional[int] = 30, dry_run=False):
+        """
+        edit a configuration file given the name and line
+        """
+
+        # @ todo make this editor generic
+
+        terminal  = Terminal()
+        cmd: str = (
+            f"MY_TITLE='GrimorieSearchRun' kitty {terminal.GENERIC_TERMINAL_PARAMS} {terminal.get_background_color()} bash -c 'cd"
+            f" {self.configuration.get_project_root()} "
+            f"; {self._get_open_text_editor_command(file_name, line)} "
+        )
+        print(cmd)
+
+        if dry_run:
+            logging.info(f"Command to edit file: {cmd}")
+            return
+
+        import os
+
+        os.system(cmd)
+
+    def _get_open_text_editor_command(self, file, line):
+        if self.configuration.get_text_editor() == 'vim':
+            return f"{self.configuration.get_text_editor()} {file} +{line}'"
+        elif self.configuration.get_text_editor() == 'docker_nvim':
+            return f"{self.configuration.get_text_editor()} {file} --line={line}'"
+        else:
+            # if is not a known editor just open the file
+            return f"{self.configuration.get_text_editor()} {file}"
