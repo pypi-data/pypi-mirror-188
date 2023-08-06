c56aaa1aecfd870209cab37502b254a1cd359b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:13 +0000

    Add package scipp/23.1.1/scipp-23.1.1.tar.gz

---
+++ packages/scipp/scipp-23.1.1.tar.gz/scipp-23.1.1/src/scipp/core/cpp_classes.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+# flake8: noqa: F401
+
+from .._scipp.core import BinEdgeError, BinnedDataError, CoordError, Coords, \
+    DataArray, DataArrayError, Dataset, DatasetError, DimensionError, DType, \
+    DTypeError, GroupByDataArray, GroupByDataset, Masks, Unit, UnitError, Variable, \
+    VariableError, VariancesError  # NOQA
