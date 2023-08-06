561d8dcd026f69fae6c39646e3b987d7755766af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package linked-roles/1.2.0/linked-roles-1.2.0.tar.gz

---
+++ packages/linked-roles/linked-roles-1.2.0.tar.gz/linked-roles-1.2.0/linked_roles/role.py
@@ -0,0 +1,400 @@
+# Copyright (c) 2023-present staciax
+# Licensed under the MIT license. Refer to the LICENSE file in the project root for more information.
+
+from __future__ import annotations
+
+import re
+from datetime import datetime
+from typing import TYPE_CHECKING, Any, Dict, Generic, List, Mapping, Optional, Tuple, Type, TypeVar, Union
+
+from .enums import AppRoleConnectionMetadataRecordType as RoleMetadataType
+
+if TYPE_CHECKING:
+    from typing_extensions import Self
+
+    from .http import AppRoleConnectionMetadataRecord as RoleMetadataRecordPayload
+
+    MetadataDataType = Union[str, int, bool, datetime]
+
+__all__: Tuple[str, ...] = (
+    'RolePlatform',
+    'RoleMetadata',
+    'RoleMetadataRecord',
+)
+
+PlatformT = TypeVar('PlatformT', bound='RolePlatform')
+
+VALID_ROLE_METADATA_KEY = r'^[A-Za-z0-9_]{0,50}$'
+
+
+def validate_metadata_key(key: str) -> str:
+    """Validate a metadata key."""
+    match = re.match(VALID_ROLE_METADATA_KEY, key)
+    if not match:
+        raise ValueError(f'{key!r} must be between 1-50 and only contain letters, numbers, and underscores')
+
+    if key.lower() != key:
+        raise ValueError(f'{key!r} must be lowercase')
+
+    return key
+
+
+class RolePlatform:
+    """
+    Represents a platform that a role is connected to.
+
+    Parameters
+    ----------
+    name : Optional[:class:`str`]
+        The name of the platform.
+    username : Optional[:class:`str`]
+        The username of the platform.
+    Attributes
+    ----------
+    name : :class:`str`
+        The name of the platform.
+    username : :class:`str`
+        The username of the platform.
+    """
+
+    def __init__(self, *, name: Optional[str] = None, username: Optional[str] = None):
+        if name is None:
+            name = ''
+        if len(name) > 50:
+            raise ValueError('Platform name must be less than 50 characters')
+        self.name: str = name
+        if username is None:
+            username = ''
+        if len(username) > 100:
+            raise ValueError('Platform username must be less than 100 characters')
+        self.username: str = username
+        self._metadata: Dict[str, RoleMetadata] = {}
+
+    def __repr__(self) -> str:
+        return f'<RolePlatform name={self.name!r} username={self.username!r}>'
+
+    def get_all_metadata(self) -> List[RoleMetadata]:
+        """Get all metadata for this platform.
+        Returns
+        -------
+        List[:class:`RoleMetadata`]
+            A list of metadata.
+        """
+        return list(self._metadata.values())
+
+    def get_metadata(self, key: str) -> Optional[RoleMetadata]:
+        """Get a metadata value for this platform.
+        Parameters
+        ----------
+        key : :class:`str`
+            The key of the metadata.
+        Returns
+        -------
+        Optional[:class:`RoleMetadata`]
+            The metadata value.
+        """
+        return self._metadata.get(key)
+
+    def add_metadata(self, key: str, value: MetadataDataType) -> Self:
+        """
+        Add a metadata value to this platform.
+        Parameters
+        ----------
+        key : :class:`str`
+            The key of the metadata.
+        value : Union[:class:`str`, :class:`int`, :class:`bool`, :class:`datetime.datetime`]
+            The value of the metadata.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The platform.
+        Raises
+        ------
+        ValueError
+            You can only have 5 metadata values per platform or the key does not exist.
+        """
+        if len(self._metadata) >= 5:
+            raise ValueError('You can only have 5 metadata values per platform')
+        metadata = self.get_metadata(key)
+        if metadata is not None:
+            raise ValueError(f'{key!r} already exists')
+        self._metadata[key] = RoleMetadata(key=key, value=value)
+        return self
+
+    def edit_metadata(self, key: str, value: MetadataDataType) -> Self:
+        """
+        Edit a metadata value for this platform.
+        Parameters
+        ----------
+        key : :class:`str`
+            The key of the metadata.
+        value : Union[:class:`str`, :class:`int`, :class:`bool`, :class:`datetime.datetime`]
+            The value of the metadata.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The platform.
+        Raises
+        ------
+        ValueError
+            The key does not exist.
+        """
+        metadata = self.get_metadata(key)
+        if metadata is not None:
+            metadata.value = value
+        return self
+
+    def add_or_edit_metadata(self, key: str, value: MetadataDataType) -> Self:
+        """
+        Add or edit a metadata value for this platform.
+        Parameters
+        ----------
+        key : :class:`str`
+            The key of the metadata.
+        value : Union[:class:`str`, :class:`int`, :class:`bool`, :class:`datetime.datetime`]
+            The value of the metadata.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The platform.
+        """
+        metadata = self.get_metadata(key)
+        if metadata is None:
+            self.add_metadata(key, value)
+        else:
+            self.edit_metadata(key, value)
+        return self
+
+    def remove_metadata(self, key: str) -> Self:
+        """
+        Remove a metadata value from this platform.
+        Parameters
+        ----------
+        key : :class:`str`
+            The key of the metadata.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The platform.
+        Raises
+        ------
+        ValueError
+            The key does not exist.
+        """
+        try:
+            del self._metadata[key]
+        except KeyError:
+            pass
+        return self
+
+    def clear_metadata(self) -> Self:
+        """Clear all metadata from this platform.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The platform.
+        """
+
+        try:
+            self._metadata.clear()
+        except AttributeError:
+            self._metadata = {}
+
+        return self
+
+    def copy(self) -> Self:
+        """Copy the platform.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The copied platform.
+        """
+        return self.__class__.from_dict(self.to_dict())
+
+    def to_dict(self) -> Mapping[str, Any]:
+        """Convert the platform to a dictionary.
+        Returns
+        -------
+        Dict[:class:`str`, Any]
+            The platform as a dictionary.
+        """
+        payload = {
+            'platform_name': self.name,
+            'platform_username': self.username,
+            'metadata': {},
+        }
+        if self._metadata:
+            meta_payload = {}
+            for key, metadata in self._metadata.items():
+                if isinstance(metadata.value, datetime):
+                    meta_payload[key] = metadata.value.isoformat()
+                elif isinstance(metadata.value, bool):
+                    meta_payload[key] = int(metadata.value)
+                else:
+                    meta_payload[key] = metadata.value
+            payload['metadata'] = meta_payload
+        return payload
+
+    @classmethod
+    def from_dict(cls: Type[Self], data: Mapping[str, Any]) -> Self:
+        """
+        Create a platform from a dictionary.
+        Parameters
+        ----------
+        data: Mapping[:class:`str`, Any]
+            The dictionary to create the platform from.
+        Returns
+        -------
+        :class:`RolePlatform`
+            The platform.
+        """
+        self = cls.__new__(cls)
+        self.name = data.get('platform_name', '')
+        self.username = data.get('platform_username', '')
+        self._metadata = {}
+        metadata = data.get('metadata')
+        if metadata is not None:
+            for key, value in metadata.items():
+                # TODO: check value type
+                self.add_metadata(key=key, value=value)
+        return self
+
+
+class RoleMetadata:
+    """
+    Represents a metadata value for a role.
+    Parameters
+    ----------
+    key : :class:`str`
+        The key of the metadata.
+    value : Union[:class:`str`, :class:`int`, :class:`bool`, :class:`datetime.datetime`]
+        The value of the metadata.
+    """
+
+    def __init__(self, key: str, value: MetadataDataType):
+        self.key: str = validate_metadata_key(key)
+        self.value: MetadataDataType = value
+
+    def __repr__(self) -> str:
+        return f'<RoleMetadata key={self.key!r} value={self.value!r}>'
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Convert the metadata to a dictionary.
+        Returns
+        -------
+        Dict[:class:`str`, Any]
+            The metadata as a dictionary.
+        """
+        return {
+            'key': self.key,
+            'value': self.value,
+        }
+
+
+class RoleMetadataRecord(Generic[PlatformT]):
+    """
+    Represents a metadata record for a role.
+    Parameters
+    ----------
+    key : :class:`str`
+        The key of the metadata.
+    name : :class:`str`
+        The name of the metadata.
+    type : Union[:class:`RoleMetadataType`, :class:`int`]
+        The type of the metadata.
+    description : :class:`str`
+        The description of the metadata.
+    name_localizations : Optional[Dict[:class:`str`, :class:`str`]]
+        The name localizations of the metadata.
+    description_localizations : Optional[Dict[:class:`str`, :class:`str`]]
+        The description localizations of the metadata.
+    """
+
+    def __init__(
+        self,
+        *,
+        key: str,
+        name: str,
+        type: Union[RoleMetadataType, int],
+        description: Optional[str] = None,
+        name_localizations: Optional[Dict[str, str]] = None,
+        description_localizations: Optional[Dict[str, str]] = None,
+    ) -> None:
+        self.key: str = validate_metadata_key(key)
+        if len(name) > 100:
+            raise ValueError('Metadata name must be 100 characters or less')
+        self.name: str = name
+        if description is None:
+            description = '...'
+        if len(description) > 200:
+            raise ValueError('Metadata description must be 200 characters or less')
+        self.description: str = description
+        if not isinstance(type, RoleMetadataType):
+            try:
+                type = RoleMetadataType(type)
+            except ValueError:
+                raise ValueError(f'{type!r} is not a valid RoleMetadataType')
+        self._type: RoleMetadataType = type
+        self.name_localizations = name_localizations
+        self.description_localizations: Optional[Dict[str, Any]] = description_localizations
+        self._parent: Optional[PlatformT] = None  # or self._platform?
+
+    def __repr__(self) -> str:
+        return f'<RoleMetadata key={self.key!r} name={self.name!r} type={self._type!r}>'
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, RoleMetadataRecord) and self.key == other.key
+
+    def __ne__(self, other) -> bool:
+        return not self.__eq__(other)
+
+    @property
+    def parent(self) -> Optional[PlatformT]:
+        """Optional[:class:`RolePlatform`]: The parent platform of the metadata record."""
+        return self._parent
+
+    @property
+    def data_type(self) -> Optional[Type[Union[int, datetime, bool]]]:
+        """Optional[Type[Union[:class:`int`, :class:`datetime.datetime`, :class:`bool`]]]: The data type of the metadata record."""
+        return self._type.data_type
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Convert the metadata record to a dictionary.
+        Returns
+        -------
+        Dict[:class:`str`, Any]
+            The metadata record as a dictionary.
+        """
+        payload = {
+            'key': self.key,
+            'name': self.name,
+            'description': self.description,
+            'type': self._type.value,
+        }
+        if self.name_localizations is not None:
+            payload['name_localizations'] = self.name_localizations
+        if self.description_localizations is not None:
+            payload['description_localizations'] = self.description_localizations
+        return payload
+
+    @classmethod
+    def from_dict(cls, data: RoleMetadataRecordPayload) -> Self:
+        """
+        Create a metadata record from a dictionary.
+        Parameters
+        ----------
+        data: RoleMetadataRecordPayload
+            The dictionary to create the metadata record from.
+        Returns
+        -------
+        :class:`RoleMetadataRecord`
+            The metadata record.
+        """
+        return cls(
+            key=data['key'],
+            name=data['name'],
+            description=data['description'],
+            type=RoleMetadataType(data['type']),
+            name_localizations=data.get('name_localizations'),
+            description_localizations=data.get('description_localizations'),
+        )
---
+++ packages/linked-roles/linked-roles-1.2.0.tar.gz/linked-roles-1.2.0/linked_roles/user.py
@@ -0,0 +1,178 @@
+# Copyright (c) 2023-present staciax
+# Licensed under the MIT license. Refer to the LICENSE file in the project root for more information.
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, Union
+
+if TYPE_CHECKING:
+    from .client import LinkedRolesOAuth2
+    from .http import User as UserPayload
+    from .oauth2 import OAuth2Token
+    from .role import RolePlatform
+
+    Snowflake = Union[str, int]
+
+
+class BaseUser:
+    """
+    Represents a base user.
+
+    Parameters
+    ----------
+    data : :class:`UserPayload`
+        The data of the user.
+    Attributes
+    id : :class:`Snowflake`
+        The ID of the user.
+    username : :class:`str`
+        The username of the user.
+    discriminator : :class:`str`
+        The discriminator of the user.
+    _avatar : Optional[:class:`str`]
+        The avatar of the user."""
+
+    if TYPE_CHECKING:
+        id: Snowflake
+        username: str
+        discriminator: str
+        _avatar: Optional[str]
+        _banner: Optional[str]
+        bot: bool
+        system: bool
+        accent_color: Optional[int]
+
+    def __init__(self, data: UserPayload):
+        self._update(data)
+
+    def _update(self, data: UserPayload) -> None:
+        self.id: Snowflake = data.get('id')
+        self.username: str = data.get('username')
+        self.discriminator: str = data.get('discriminator')
+        self._avatar: Optional[str] = data.get('avatar')
+        self._banner: Optional[str] = data.get('banner')
+        self.bot: bool = data.get('bot') or False
+        self.system: bool = data.get('system') or False
+        self.accent_color: Optional[int] = data.get('accent_color')
+
+    def __repr__(self) -> str:
+        return f'<User id={self.id!r} username={self.username!r} discriminator={self.discriminator!r}>'
+
+    def __str__(self) -> str:
+        return f'{self.username}#{self.discriminator}'
+
+    @property
+    def avatar_url(self) -> Optional[str]:
+        """:class:`Optional[:class:`str`] The avatar URL of the user."""
+        if self._avatar is None:
+            return None
+        return f'https://cdn.discordapp.com/avatars/{self.id}/{self._avatar}.png?size=1024'
+
+    @property
+    def banner_url(self) -> Optional[str]:
+        """:class:`Optional[:class:`str`] The banner URL of the user."""
+        if self._banner is None:
+            return None
+        animated = self._banner.startswith('a_')
+        format = 'gif' if animated else 'png'
+        return f'https://cdn.discordapp.com/banners/{self.id}/{self._banner}.{format}?size=1024'
+
+
+class User(BaseUser):
+    """
+    Represents a user.
+
+    Parameters
+    ----------
+    client : :class:`LinkedRolesOAuth2`
+        The client of the user.
+    data : :class:`UserPayload`
+        The data of the user.
+    tokens : Optional[:class:`OAuth2Token`]
+        The tokens of the user.
+    """
+
+    def __init__(self, client: LinkedRolesOAuth2, data: UserPayload, *, tokens: Optional[OAuth2Token] = None):
+        super().__init__(data)
+        self.client = client
+        self._role_platform: Optional[RolePlatform] = None
+        self._tokens: Optional[OAuth2Token] = tokens
+        self.__orginal_role_platform__: Optional[RolePlatform] = None
+
+    def _update(self, data: UserPayload, tokens: Optional[OAuth2Token] = None) -> None:
+        super()._update(data)
+        if tokens is not None:
+            self._tokens = tokens
+
+    def get_role_platform(self) -> Optional[RolePlatform]:
+        """ " Returns the role platform of the user.
+        Returns
+        -------
+        Optional[:class:`RolePlatform`]
+            The role platform of the user.
+        """
+        return self._role_platform or self.__orginal_role_platform__
+
+    def get_tokens(self) -> Optional[OAuth2Token]:
+        """ " Returns the tokens of the user.
+        Returns
+        -------
+        Optional[:class:`OAuth2Token`]
+            The tokens of the user.
+        """
+        if self._tokens is not None:
+            if self._tokens.is_expired():
+                self.client.loop.create_task(self._tokens.refresh())
+        return self._tokens
+
+    def set_tokens(self, value: OAuth2Token) -> None:
+        """ " Sets the tokens of the user.
+        Parameters
+        ----------
+        value : :class:`OAuth2Token`
+            The tokens of the user.
+        """
+        self._tokens = value
+
+    async def edit_role_metadata(self, platform: Optional[RolePlatform] = None) -> Optional[RolePlatform]:
+        """Edits the role metadata of the user.
+        Parameters
+        ----------
+        platform : Optional[:class:`RolePlatform`]
+            The role platform of the user.
+        Returns
+        -------
+        Optional[:class:`RolePlatform`]
+            The role platform of the user.
+        Raises
+        ------
+        ValueError
+            The role metadata is not found.
+        TypeError
+            The role metadata value must be the same type.
+        """
+
+        if self.__orginal_role_platform__ is None and self._role_platform is not None:
+            self.__orginal_role_platform__ = self._role_platform
+
+        platform = platform or self.get_role_platform()
+
+        if platform is not None:
+
+            if self.client.is_role_metadata_fetched():
+                for metadata in platform.get_all_metadata():
+
+                    # verify metadata
+                    get_metadata = self.client.get_role_metadata(metadata.key)
+
+                    if get_metadata is None:
+                        raise ValueError(f'Role metadata {metadata.key!r} is not found')
+
+                    if get_metadata.data_type is not None:
+                        if not isinstance(metadata.value, get_metadata.data_type):
+                            raise TypeError(f'Role metadata {metadata.key!r} value must be {get_metadata.data_type!r}')
+
+            platform = await self.client.edit_user_role_connection(self, platform)
+            if platform is not None:
+                self._role_platform = platform
+        return self.get_role_platform()
