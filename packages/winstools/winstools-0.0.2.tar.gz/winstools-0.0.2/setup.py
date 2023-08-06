9c7578e8ed2ce998cbf1bb8c86c7ecbc4332935b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:57 +0000

    Add package winstools/0.0.2/winstools-0.0.2.tar.gz

---
+++ packages/winstools/winstools-0.0.2.tar.gz/winstools-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+	name="winstools",
+	version="0.0.2",
+	author="Winstang",
+	author_email="Winstang2020@gmail.com",
+	description="測試測試",
+	long_description="測試測試測試測試測試測試測試測試",
+	packages=setuptools.find_packages(),
+)
