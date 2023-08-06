7397c1c5c1cb3e0d6d43f71aae4b24b0c1742af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:10 +0000

    Add package correos-preregistro/0.0.4/correos-preregistro-0.0.4.tar.gz

---
+++ packages/correos-preregistro/correos-preregistro-0.0.4.tar.gz/correos-preregistro-0.0.4/correos_preregistro/resources/receiver.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Receiver:
+    name: str
+    address: str
+    city: str
+    state: str
+    zip: str
+    phone: str
+    email: str
+    lang: str
+    surname: str = None
+    nif: str = None
