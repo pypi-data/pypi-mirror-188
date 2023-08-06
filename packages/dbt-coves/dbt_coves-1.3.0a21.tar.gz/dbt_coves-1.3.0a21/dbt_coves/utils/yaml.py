bab03598305886dc4898fed83711f2df2d548ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:54 +0000

    Add package dbt-coves/1.3.0a21/dbt_coves-1.3.0a21.tar.gz

---
+++ packages/dbt-coves/dbt_coves-1.3.0a21.tar.gz/dbt_coves-1.3.0a21/dbt_coves/utils/yaml.py
@@ -0,0 +1,36 @@
+"""Contains yaml related utils which might get used in places."""
+from pathlib import Path
+from typing import Any, Dict
+
+import yaml
+
+
+def open_yaml(path: Path) -> Dict[str, Any]:
+    """Opens a yaml file... Nothing too exciting there.
+
+    Args:
+        path (Path): Full filename path pointing to the yaml file we want to open.
+
+    Returns:
+        Dict[str, Any]: A python dict containing the content from the yaml file.
+    """
+    if path.is_file():
+        with open(path, "r") as stream:
+            return yaml.load(stream, Loader=yaml.Loader)
+    raise FileNotFoundError(f"File {path.resolve()} was not found.")
+
+
+class DbtCovesDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, *args, **kwargs):
+        return super().increase_indent(flow=flow, indentless=False)
+
+
+def save_yaml(path: Path, data: Dict[str, Any]) -> None:
+    """Saves a YAML content.
+
+    Args:
+        path (Path): Full filename path pointing to the yaml file we want to save.
+        data (dict[str, Any]): Data to save in the file.
+    """
+    with open(path, "w") as outfile:
+        yaml.dump(data, outfile, sort_keys=False, Dumper=DbtCovesDumper)
