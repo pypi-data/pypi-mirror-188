e150b22ad6faab22aa22228078757643a4592f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:41 +0000

    Add package pytorch-metric-learning/2.0.0.dev1/pytorch-metric-learning-2.0.0.dev1.tar.gz

---
+++ packages/pytorch-metric-learning/pytorch-metric-learning-2.0.0.dev1.tar.gz/pytorch-metric-learning-2.0.0.dev1/src/pytorch_metric_learning/wrappers/__init__.py
@@ -0,0 +1,4 @@
+from .base_loss_wrapper import BaseLossWrapper
+from .cross_batch_memory import CrossBatchMemory
+from .multiple_losses import MultipleLosses
+from .self_supervised_loss import SelfSupervisedLoss
