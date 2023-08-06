54cbb7890facc55315a9301d51f37a3eb9c4aafa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:18 +0000

    Add package opensignals/0.0.5.post1/opensignals-0.0.5.post1-py3-none-any.whl

---
+++ packages/opensignals/opensignals-0.0.5.post1-py3-none-any.whl/opensignals/__about__.py
@@ -0,0 +1,10 @@
+"""Contain information about the project."""
+__package_name__ = 'opensignals'
+__version__ = '0.0.5-1'
+
+__description__ = 'Open Signals'
+
+__author__ = 'Jordi Villar'
+__author_email__ = 'jrdi.villar@gmail.com'
+
+__license__ = 'Apache License'
