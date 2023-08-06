6d8bd594a0d8f7539ac3cbb8f453ac14aec4395d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:26 +0000

    Add package mypy-boto3-kafka/1.26.60/mypy_boto3_kafka-1.26.60-py3-none-any.whl

---
+++ packages/mypy-boto3-kafka/mypy_boto3_kafka-1.26.60-py3-none-any.whl/mypy_boto3_kafka/__init__.py
@@ -0,0 +1,60 @@
+"""
+Main interface for kafka service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_kafka import (
+        Client,
+        KafkaClient,
+        ListClusterOperationsPaginator,
+        ListClustersPaginator,
+        ListClustersV2Paginator,
+        ListConfigurationRevisionsPaginator,
+        ListConfigurationsPaginator,
+        ListKafkaVersionsPaginator,
+        ListNodesPaginator,
+        ListScramSecretsPaginator,
+    )
+
+    session = Session()
+    client: KafkaClient = session.client("kafka")
+
+    list_cluster_operations_paginator: ListClusterOperationsPaginator = client.get_paginator("list_cluster_operations")
+    list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+    list_clusters_v2_paginator: ListClustersV2Paginator = client.get_paginator("list_clusters_v2")
+    list_configuration_revisions_paginator: ListConfigurationRevisionsPaginator = client.get_paginator("list_configuration_revisions")
+    list_configurations_paginator: ListConfigurationsPaginator = client.get_paginator("list_configurations")
+    list_kafka_versions_paginator: ListKafkaVersionsPaginator = client.get_paginator("list_kafka_versions")
+    list_nodes_paginator: ListNodesPaginator = client.get_paginator("list_nodes")
+    list_scram_secrets_paginator: ListScramSecretsPaginator = client.get_paginator("list_scram_secrets")
+    ```
+"""
+from .client import KafkaClient
+from .paginator import (
+    ListClusterOperationsPaginator,
+    ListClustersPaginator,
+    ListClustersV2Paginator,
+    ListConfigurationRevisionsPaginator,
+    ListConfigurationsPaginator,
+    ListKafkaVersionsPaginator,
+    ListNodesPaginator,
+    ListScramSecretsPaginator,
+)
+
+Client = KafkaClient
+
+
+__all__ = (
+    "Client",
+    "KafkaClient",
+    "ListClusterOperationsPaginator",
+    "ListClustersPaginator",
+    "ListClustersV2Paginator",
+    "ListConfigurationRevisionsPaginator",
+    "ListConfigurationsPaginator",
+    "ListKafkaVersionsPaginator",
+    "ListNodesPaginator",
+    "ListScramSecretsPaginator",
+)
