9e272902321882a9b832ae118b5ba0c912561561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package openmetadata-ingestion/0.13.1.9/openmetadata-ingestion-0.13.1.9.tar.gz

---
+++ packages/openmetadata-ingestion/openmetadata-ingestion-0.13.1.9.tar.gz/openmetadata-ingestion-0.13.1.9/setup.py
@@ -0,0 +1,252 @@
+#  Copyright 2021 Collate
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#  http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+from typing import Dict, Set
+
+from setuptools import find_namespace_packages, setup
+
+
+def get_long_description():
+    root = os.path.dirname(__file__)
+    with open(os.path.join(root, "README.md")) as f:
+        description = f.read()
+    return description
+
+
+base_requirements = {
+    "commonregex",
+    "idna<3,>=2.5",
+    "mypy_extensions>=0.4.3",
+    "typing-inspect",
+    "pydantic~=1.9.0",
+    "email-validator>=1.0.3",
+    "google>=3.0.0",
+    "google-auth>=1.33.0",
+    "python-dateutil>=2.8.1",
+    "wheel~=0.36.2",
+    "python-jose==3.3.0",
+    "sqlalchemy>=1.4.0,<2",
+    "requests>=2.23",
+    "cryptography",
+    "Jinja2>=2.11.3",
+    "PyYAML",
+    "jsonschema",
+    "sqllineage==1.3.7",
+    "antlr4-python3-runtime==4.9.2",
+    "boto3~=1.19.12",
+    "botocore==1.22.12",
+    "avro-python3==1.10.2",
+    "grpcio-tools==1.48.2",
+    # compatibility requirements for 3.7
+    "typing-compat~=0.1.0",
+    "importlib-metadata~=4.12.0",  # From airflow constraints
+    "croniter~=1.3.0",
+    "requests-aws4auth==1.1.2",
+    "pymysql>=1.0.2",
+    "cached-property==1.5.2",
+    "pandas==1.3.5",
+    "chardet==4.0.0",
+}
+
+
+datalake_common = {
+    "pandas==1.3.5",
+    "pyarrow==6.0.1",
+}
+
+plugins: Dict[str, Set[str]] = {
+    "airflow": {
+        "apache-airflow==2.3.3"
+    },  # Same as ingestion container. For development.
+    "airflow-container-1.10.15": {"markupsafe==2.0.1 ", "requests==2.23.0"},
+    "amundsen": {"neo4j~=4.4.0"},
+    "athena": {"PyAthena[SQLAlchemy]"},
+    "atlas": {},
+    "azuresql": {"pyodbc"},
+    "bigquery": {
+        "grpcio==1.50.0",
+        "sqlalchemy-bigquery>=1.2.2",
+        "pyarrow~=6.0.1",
+        "google-cloud-datacatalog==3.6.2",
+    },
+    "bigquery-usage": {"google-cloud-logging", "cachetools"},
+    "docker": {"python_on_whales==0.34.0"},
+    "backup": {"boto3~=1.19.12", "azure-identity", "azure-storage-blob"},
+    "dagster": {"pymysql>=1.0.2", "psycopg2-binary", "GeoAlchemy2", "dagster_graphql"},
+    "datalake-s3": {
+        "s3fs==0.4.2",
+        "boto3~=1.19.12",
+        *datalake_common,
+    },
+    "datalake-gcs": {
+        "google-cloud-storage==1.43.0",
+        "gcsfs==2022.5.0",
+        *datalake_common,
+    },
+    "dbt": {"google-cloud", "boto3", "google-cloud-storage==1.43.0"},
+    "druid": {"pydruid>=0.6.2"},
+    "elasticsearch": {"elasticsearch==7.13.1", "requests-aws4auth==1.1.2"},
+    "glue": {"boto3~=1.19.12"},
+    "dynamodb": {"boto3~=1.19.12"},
+    "sagemaker": {"boto3~=1.19.12"},
+    "hive": {
+        "pyhive~=0.6.5",
+        "thrift~=0.13.0",
+        "sasl==0.3.1",
+        "thrift-sasl==0.4.3",
+        "presto-types-parser==0.0.2",
+    },
+    "kafka": {
+        "confluent_kafka==1.8.2",
+        "fastavro>=1.2.0",
+        "avro-python3==1.10.2",
+        "avro==1.11.1",
+        "grpcio-tools",
+        "protobuf",
+    },
+    "kinesis": {"boto3~=1.19.12"},
+    "redpanda": {
+        "confluent_kafka==1.8.2",
+        "fastavro>=1.2.0",
+        "avro-python3==1.10.2",
+        "avro==1.11.1",
+        "grpcio-tools",
+        "protobuf",
+    },
+    "ldap-users": {"ldap3==2.9.1"},
+    "looker": {"looker-sdk>=22.20.0"},
+    "mssql": {"sqlalchemy-pytds>=0.3"},
+    "pymssql": {"pymssql==2.2.5"},
+    "mssql-odbc": {"pyodbc"},
+    "mysql": {"pymysql>=1.0.2"},
+    "oracle": {"cx_Oracle", "oracledb==1.0.3"},
+    "powerbi": {"msal==1.17.0"},
+    "presto": {"pyhive~=0.6.3"},
+    "trino": {"trino[sqlalchemy]"},
+    "postgres": {"pymysql>=1.0.2", "psycopg2-binary", "GeoAlchemy2"},
+    "redash": {"redash-toolbelt==0.1.8"},
+    "redshift": {"sqlalchemy-redshift==0.8.9", "psycopg2-binary", "GeoAlchemy2"},
+    "redshift-usage": {
+        "sqlalchemy-redshift==0.8.9",
+        "psycopg2-binary",
+        "GeoAlchemy2",
+    },
+    "snowflake": {"snowflake-sqlalchemy~=1.4.3"},
+    "snowflake-usage": {"snowflake-sqlalchemy~=1.4.3"},
+    "superset": {},
+    "tableau": {"tableau-api-lib==0.1.50"},
+    "vertica": {"sqlalchemy-vertica[vertica-python]>=0.0.5"},
+    "webhook-server": {},
+    "salesforce": {"simple_salesforce~=1.11.4"},
+    "okta": {"okta~=2.3.0"},
+    "mlflow": {"mlflow-skinny~=1.26.1"},
+    "sklearn": {"scikit-learn==1.0.2"},
+    "db2": {"ibm-db-sa==0.3.8"},
+    "clickhouse": {"clickhouse-driver==0.2.5", "clickhouse-sqlalchemy==0.2.3"},
+    "databricks": {"sqlalchemy-databricks==0.1.0"},
+    "singlestore": {"pymysql>=1.0.2"},
+    "azure-sso": {"msal~=1.17.0"},
+    "deltalake": {"delta-spark~=2.2.0"},
+    "great-expectations": {"great-expectations~=0.15.0"},
+    "pinotdb": {"pinotdb~=0.3.11"},
+    "nifi": {},
+    "domo": {"pydomo~=0.3.0.5"},
+    "datalake-azure": {
+        "azure-storage-blob~=12.14.1",
+        "azure-identity~=1.12.0",
+        "adlfs==2022.2.0",
+        *datalake_common,
+    },
+}
+dev = {
+    "datamodel-code-generator==0.13.4",
+    "black==22.3.0",
+    "pycln==1.3.2",
+    "docker",
+    "google-cloud-storage==1.43.0",
+    "twine",
+}
+test = {
+    "isort==5.10.1",
+    "pre-commit",
+    "pylint",
+    "pytest==7.0.0",
+    "pytest-cov",
+    "pytest-order",
+    "coverage",
+    # sklearn integration
+    "scikit-learn==1.0.2",
+    "pandas==1.3.5",
+    # great_expectations tests
+    "great-expectations~=0.15.0",
+    # Airflow tests
+    "apache-airflow==2.3.3",
+    # Domo test
+    "pydomo~=0.3.0.5",
+    # mock boto3 functions
+    "moto==4.0.8",
+    # amundsen
+    "neo4j~=4.4.0",
+}
+
+build_options = {"includes": ["_cffi_backend"]}
+setup(
+    name="openmetadata-ingestion",
+    version="0.13.1.9",
+    url="https://open-metadata.org/",
+    author="OpenMetadata Committers",
+    license="Apache License 2.0",
+    description="Ingestion Framework for OpenMetadata",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    options={"build_exe": build_options},
+    package_dir={"": "src"},
+    package_data={"metadata.examples": ["workflows/*.yaml"]},
+    zip_safe=False,
+    dependency_links=[],
+    project_urls={
+        "Documentation": "https://docs.open-metadata.org/",
+        "Source": "https://github.com/open-metadata/OpenMetadata",
+    },
+    packages=find_namespace_packages(where="./src", exclude=["tests*"]),
+    namespace_package=["metadata"],
+    entry_points={
+        "console_scripts": ["metadata = metadata.cmd:metadata"],
+        "apache_airflow_provider": [
+            "provider_info = airflow_provider_openmetadata:get_provider_config"
+        ],
+    },
+    install_requires=list(base_requirements),
+    extras_require={
+        "base": list(base_requirements),
+        "dev": list(dev),
+        "test": list(test),
+        "data-insight": list(plugins["elasticsearch"]),
+        **{plugin: list(dependencies) for (plugin, dependencies) in plugins.items()},
+        "all": list(
+            base_requirements.union(
+                *[
+                    requirements
+                    for plugin, requirements in plugins.items()
+                    if plugin
+                    not in {
+                        "airflow",
+                        "airflow-container-1.10.15",
+                        "db2",
+                        "great-expectations",
+                    }
+                ]
+            )
+        ),
+    },
+)
---
+++ packages/openmetadata-ingestion/openmetadata-ingestion-0.13.1.9.tar.gz/openmetadata-ingestion-0.13.1.9/src/metadata/generated/antlr/EntityLinkLexer.py
@@ -0,0 +1,138 @@
+# Generated from /home/runner/work/OpenMetadata/OpenMetadata/openmetadata-spec/src/main/antlr4/org/openmetadata/schema/EntityLink.g4 by ANTLR 4.7.2
+from antlr4 import *
+from io import StringIO
+from typing.io import TextIO
+import sys
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\t")
+        buf.write("\u00e3\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\3\2\3\2\3\2\3\2\3\3\3\3\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\5\4\u00b8\n\4\3\5\3\5\3\5")
+        buf.write("\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3")
+        buf.write("\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\5\5\u00d5\n")
+        buf.write("\5\3\6\3\6\3\6\3\7\6\7\u00db\n\7\r\7\16\7\u00dc\3\b\6")
+        buf.write("\b\u00e0\n\b\r\b\16\b\u00e1\2\2\t\3\3\5\4\7\5\t\6\13\7")
+        buf.write("\r\b\17\t\3\2\4\3\2c|\n\2$$))..\60\60\62;C\\aac|\2\u00fb")
+        buf.write("\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13")
+        buf.write("\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\3\21\3\2\2\2\5\25\3")
+        buf.write("\2\2\2\7\u00b7\3\2\2\2\t\u00d4\3\2\2\2\13\u00d6\3\2\2")
+        buf.write("\2\r\u00da\3\2\2\2\17\u00df\3\2\2\2\21\22\7>\2\2\22\23")
+        buf.write("\7%\2\2\23\24\7G\2\2\24\4\3\2\2\2\25\26\7@\2\2\26\6\3")
+        buf.write("\2\2\2\27\30\7v\2\2\30\31\7c\2\2\31\32\7d\2\2\32\33\7")
+        buf.write("n\2\2\33\u00b8\7g\2\2\34\35\7f\2\2\35\36\7c\2\2\36\37")
+        buf.write("\7v\2\2\37 \7c\2\2 !\7d\2\2!\"\7c\2\2\"#\7u\2\2#\u00b8")
+        buf.write("\7g\2\2$%\7f\2\2%&\7c\2\2&\'\7v\2\2\'(\7c\2\2()\7d\2\2")
+        buf.write(")*\7c\2\2*+\7u\2\2+,\7g\2\2,-\7U\2\2-.\7e\2\2./\7j\2\2")
+        buf.write("/\60\7g\2\2\60\61\7o\2\2\61\u00b8\7c\2\2\62\63\7o\2\2")
+        buf.write("\63\64\7g\2\2\64\65\7v\2\2\65\66\7t\2\2\66\67\7k\2\2\67")
+        buf.write("8\7e\2\28\u00b8\7u\2\29:\7f\2\2:;\7c\2\2;<\7u\2\2<=\7")
+        buf.write("j\2\2=>\7d\2\2>?\7q\2\2?@\7c\2\2@A\7t\2\2A\u00b8\7f\2")
+        buf.write("\2BC\7r\2\2CD\7k\2\2DE\7r\2\2EF\7g\2\2FG\7n\2\2GH\7k\2")
+        buf.write("\2HI\7p\2\2I\u00b8\7g\2\2JK\7e\2\2KL\7j\2\2LM\7c\2\2M")
+        buf.write("N\7t\2\2N\u00b8\7v\2\2OP\7t\2\2PQ\7g\2\2QR\7r\2\2RS\7")
+        buf.write("q\2\2ST\7t\2\2T\u00b8\7v\2\2UV\7v\2\2VW\7q\2\2WX\7r\2")
+        buf.write("\2XY\7k\2\2Y\u00b8\7e\2\2Z[\7o\2\2[\\\7n\2\2\\]\7o\2\2")
+        buf.write("]^\7q\2\2^_\7f\2\2_`\7g\2\2`\u00b8\7n\2\2ab\7d\2\2bc\7")
+        buf.write("q\2\2c\u00b8\7v\2\2de\7V\2\2ef\7J\2\2fg\7T\2\2gh\7G\2")
+        buf.write("\2hi\7C\2\2i\u00b8\7F\2\2jk\7n\2\2kl\7q\2\2lm\7e\2\2m")
+        buf.write("n\7c\2\2no\7v\2\2op\7k\2\2pq\7q\2\2q\u00b8\7p\2\2rs\7")
+        buf.write("i\2\2st\7n\2\2tu\7q\2\2uv\7u\2\2vw\7u\2\2wx\7c\2\2xy\7")
+        buf.write("t\2\2y\u00b8\7{\2\2z{\7i\2\2{|\7n\2\2|}\7q\2\2}~\7u\2")
+        buf.write("\2~\177\7u\2\2\177\u0080\7c\2\2\u0080\u0081\7t\2\2\u0081")
+        buf.write("\u0082\7{\2\2\u0082\u0083\7V\2\2\u0083\u0084\7g\2\2\u0084")
+        buf.write("\u0085\7t\2\2\u0085\u00b8\7o\2\2\u0086\u0087\7v\2\2\u0087")
+        buf.write("\u0088\7c\2\2\u0088\u00b8\7i\2\2\u0089\u008a\7v\2\2\u008a")
+        buf.write("\u008b\7c\2\2\u008b\u008c\7i\2\2\u008c\u008d\7E\2\2\u008d")
+        buf.write("\u008e\7c\2\2\u008e\u008f\7v\2\2\u008f\u0090\7g\2\2\u0090")
+        buf.write("\u0091\7i\2\2\u0091\u0092\7q\2\2\u0092\u0093\7t\2\2\u0093")
+        buf.write("\u00b8\7{\2\2\u0094\u0095\7v\2\2\u0095\u0096\7{\2\2\u0096")
+        buf.write("\u0097\7r\2\2\u0097\u00b8\7g\2\2\u0098\u0099\7v\2\2\u0099")
+        buf.write("\u009a\7g\2\2\u009a\u009b\7u\2\2\u009b\u009c\7v\2\2\u009c")
+        buf.write("\u009d\7F\2\2\u009d\u009e\7g\2\2\u009e\u009f\7h\2\2\u009f")
+        buf.write("\u00a0\7k\2\2\u00a0\u00a1\7p\2\2\u00a1\u00a2\7k\2\2\u00a2")
+        buf.write("\u00a3\7v\2\2\u00a3\u00a4\7k\2\2\u00a4\u00a5\7q\2\2\u00a5")
+        buf.write("\u00b8\7p\2\2\u00a6\u00a7\7v\2\2\u00a7\u00a8\7g\2\2\u00a8")
+        buf.write("\u00a9\7u\2\2\u00a9\u00aa\7v\2\2\u00aa\u00ab\7U\2\2\u00ab")
+        buf.write("\u00ac\7w\2\2\u00ac\u00ad\7k\2\2\u00ad\u00ae\7v\2\2\u00ae")
+        buf.write("\u00b8\7g\2\2\u00af\u00b0\7v\2\2\u00b0\u00b1\7g\2\2\u00b1")
+        buf.write("\u00b2\7u\2\2\u00b2\u00b3\7v\2\2\u00b3\u00b4\7E\2\2\u00b4")
+        buf.write("\u00b5\7c\2\2\u00b5\u00b6\7u\2\2\u00b6\u00b8\7g\2\2\u00b7")
+        buf.write("\27\3\2\2\2\u00b7\34\3\2\2\2\u00b7$\3\2\2\2\u00b7\62\3")
+        buf.write("\2\2\2\u00b79\3\2\2\2\u00b7B\3\2\2\2\u00b7J\3\2\2\2\u00b7")
+        buf.write("O\3\2\2\2\u00b7U\3\2\2\2\u00b7Z\3\2\2\2\u00b7a\3\2\2\2")
+        buf.write("\u00b7d\3\2\2\2\u00b7j\3\2\2\2\u00b7r\3\2\2\2\u00b7z\3")
+        buf.write("\2\2\2\u00b7\u0086\3\2\2\2\u00b7\u0089\3\2\2\2\u00b7\u0094")
+        buf.write("\3\2\2\2\u00b7\u0098\3\2\2\2\u00b7\u00a6\3\2\2\2\u00b7")
+        buf.write("\u00af\3\2\2\2\u00b8\b\3\2\2\2\u00b9\u00ba\7e\2\2\u00ba")
+        buf.write("\u00bb\7q\2\2\u00bb\u00bc\7n\2\2\u00bc\u00bd\7w\2\2\u00bd")
+        buf.write("\u00be\7o\2\2\u00be\u00bf\7p\2\2\u00bf\u00d5\7u\2\2\u00c0")
+        buf.write("\u00c1\7f\2\2\u00c1\u00c2\7g\2\2\u00c2\u00c3\7u\2\2\u00c3")
+        buf.write("\u00c4\7e\2\2\u00c4\u00c5\7t\2\2\u00c5\u00c6\7k\2\2\u00c6")
+        buf.write("\u00c7\7r\2\2\u00c7\u00c8\7v\2\2\u00c8\u00c9\7k\2\2\u00c9")
+        buf.write("\u00ca\7q\2\2\u00ca\u00d5\7p\2\2\u00cb\u00cc\7v\2\2\u00cc")
+        buf.write("\u00cd\7c\2\2\u00cd\u00ce\7i\2\2\u00ce\u00d5\7u\2\2\u00cf")
+        buf.write("\u00d0\7v\2\2\u00d0\u00d1\7c\2\2\u00d1\u00d2\7u\2\2\u00d2")
+        buf.write("\u00d3\7m\2\2\u00d3\u00d5\7u\2\2\u00d4\u00b9\3\2\2\2\u00d4")
+        buf.write("\u00c0\3\2\2\2\u00d4\u00cb\3\2\2\2\u00d4\u00cf\3\2\2\2")
+        buf.write("\u00d5\n\3\2\2\2\u00d6\u00d7\7<\2\2\u00d7\u00d8\7<\2\2")
+        buf.write("\u00d8\f\3\2\2\2\u00d9\u00db\t\2\2\2\u00da\u00d9\3\2\2")
+        buf.write("\2\u00db\u00dc\3\2\2\2\u00dc\u00da\3\2\2\2\u00dc\u00dd")
+        buf.write("\3\2\2\2\u00dd\16\3\2\2\2\u00de\u00e0\t\3\2\2\u00df\u00de")
+        buf.write("\3\2\2\2\u00e0\u00e1\3\2\2\2\u00e1\u00df\3\2\2\2\u00e1")
+        buf.write("\u00e2\3\2\2\2\u00e2\20\3\2\2\2\7\2\u00b7\u00d4\u00dc")
+        buf.write("\u00e1\2")
+        return buf.getvalue()
+
+
+class EntityLinkLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    ENTITY_TYPE = 3
+    ENTITY_FIELD = 4
+    RESERVED = 5
+    ENTITY_ATTRIBUTE = 6
+    ENTITY_FQN = 7
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'<#E'", "'>'", "'::'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "ENTITY_TYPE", "ENTITY_FIELD", "RESERVED", "ENTITY_ATTRIBUTE", 
+            "ENTITY_FQN" ]
+
+    ruleNames = [ "T__0", "T__1", "ENTITY_TYPE", "ENTITY_FIELD", "RESERVED", 
+                  "ENTITY_ATTRIBUTE", "ENTITY_FQN" ]
+
+    grammarFileName = "EntityLink.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.7.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
