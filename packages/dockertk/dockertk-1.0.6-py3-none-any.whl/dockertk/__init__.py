ed167e19a65d6933e968e332e13ad61e4dd2c69e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:03 +0000

    Add package dockertk/1.0.6/dockertk-1.0.6-py3-none-any.whl

---
+++ packages/dockertk/dockertk-1.0.6-py3-none-any.whl/dockertk/__init__.py
@@ -0,0 +1,38 @@
+from . import core as core
+from . import util
+
+
+def docker_compose_dict(path: str):
+    return core.env_dict(path=path, env=core.EnvType.DOCKER_COMPOSE)
+
+
+def dotenv_dict(path: str):
+    return core.env_dict(path=path, env=core.EnvType.DOTENV)
+
+
+def dockerfile_dict(path: str):
+    return core.env_dict(path=path, env=core.EnvType.DOCKERFILE)
+
+
+def refresh_envs(
+    docker_compose_path: str = "docker-compose.yml",
+    dotenv_path: str = ".env",
+    dockerfile_path: str = "Dockerfile",
+    python_env_path: str = "env.py",
+):
+    docker_env = {}
+    if core.path_exists(docker_compose_path):
+        docker_compose_env = docker_compose_dict(path=docker_compose_path)
+        docker_env = {**docker_env, **docker_compose_env}
+    if core.path_exists(dockerfile_path):
+        dockerfile_env = dockerfile_dict(dockerfile_path)
+        docker_env = {**docker_env, **dockerfile_env}
+
+    if docker_env:
+        core.write_text(dotenv_path, core.form_dotenv_src(docker_env))
+        core.write_text(
+            python_env_path, core.form_python_env_src(docker_env, dotenv_path)
+        )
+        core.write_text(
+            dockerfile_path, core.form_dockerfile_env_src(docker_env, dockerfile_path)
+        )
