8ce24057e3e8148976f543e878741d67f6a94175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:55 +0000

    Add package robin-sd-upload/0.1.39/robin_sd_upload-0.1.39.tar.gz

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.39.tar.gz/robin_sd_upload-0.1.39/robin_sd_upload/logger.py
@@ -0,0 +1,21 @@
+import logging
+
+def setup_logger(log_file):
+    """
+    Function to setup a logger for the script
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # create console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.ERROR)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
