0267c0a2031312333a421fe7b2aef927fe1f02b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:42 +0000

    Add package abba-model-f41/0.0.5/abba_model_f41-0.0.5.tar.gz

---
+++ packages/abba-model-f41/abba_model_f41-0.0.5.tar.gz/abba_model_f41-0.0.5/README.md
@@ -0,0 +1,4 @@
+# ABBA MODEL Packages
+
+F4_1 Usage:
+    - Descript usage of f4_1 module here
---
+++ packages/abba-model-f41/abba_model_f41-0.0.5.tar.gz/abba_model_f41-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    install_requires=open('requirements.txt').read().splitlines(),
+    name="abba_model_f41",
+    version="0.0.5",
+    license="MIT",
+    author="abba",
+    author_email="jaebin9274@cogniterm.com",
+    description="sample",
+    long_description=open("README.md").read(),
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]    
+)
