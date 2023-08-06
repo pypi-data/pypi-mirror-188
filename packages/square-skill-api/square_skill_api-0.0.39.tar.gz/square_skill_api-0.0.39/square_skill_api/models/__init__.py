d6c63e021dc2e951fe9205340fc7ee1c0abff10c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:06 +0000

    Add package square-skill-api/0.0.39/square_skill_api-0.0.39.tar.gz

---
+++ packages/square-skill-api/square_skill_api-0.0.39.tar.gz/square_skill_api-0.0.39/square_skill_api/models/__init__.py
@@ -0,0 +1,9 @@
+from square_skill_api.models.heartbeat import HeartbeatResult
+from square_skill_api.models.prediction import (
+    QueryOutput,
+    Prediction,
+    PredictionDocument,
+    PredictionOutput,
+    QueryOutput,
+)
+from square_skill_api.models.request import QueryRequest
