2061e73dcd4bd61a4b978e260e3d9f7943a3a6b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:09 +0000

    Add package erutils/0.6.2/erutils-0.6.2.tar.gz

---
+++ packages/erutils/erutils-0.6.2.tar.gz/erutils-0.6.2/erutils/__init__.py
@@ -0,0 +1,12 @@
+"""
+This Section is left empty to give user better access to which ever part
+that user want to access
+: command_line_interface
+: dll
+: lightning
+: neurons
+: nlp
+: utils
+"""
+
+from .utils import *
