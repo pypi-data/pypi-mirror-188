a9644286c2c907463387d5345fe46164b3a252eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:56 +0000

    Add package aio-aerospike-python/0.0.4/aio_aerospike_python-0.0.4.tar.gz

---
+++ packages/aio-aerospike-python/aio_aerospike_python-0.0.4.tar.gz/aio_aerospike_python-0.0.4/src/basic_example.py
@@ -0,0 +1,64 @@
+from aio_aerospike_python import AioAerospikeClient
+from aio_aerospike_python import exception
+from aerospike_helpers import expressions as exp
+import aerospike
+
+
+import asyncio
+config = {
+    'hosts': [('0.0.0.0', 3000)]
+}
+
+client = AioAerospikeClient(config)
+print(client.is_connected())
+
+
+async def put_some_data(limit: int):
+    for i in range(limit):
+        key = ("test", "test", i)
+        data = {"a": i}
+        await client.put(key, data)
+
+
+async def read_data(limit: int):
+    for i in range(limit):
+        key = ("test", "test", i)
+        r = await client.get(key)
+        print(r)
+
+
+async def use_query(mina: int, maxa: int):
+    query = client.query("test", "test")
+    expr = expr = exp.And(
+        exp.LT(exp.IntBin("a"), maxa),
+        exp.GT(exp.IntBin("a"), mina)
+    ).compile()
+    scan_policy = {"expressions": expr}
+    results = await query.results(scan_policy)
+    print("query results ===")
+    for r in results:
+        print(r)
+
+
+async def use_scan(mina: int, maxa: int):
+    scan = client.query("test", "test")
+    expr = exp.And(
+        exp.LT(exp.IntBin("a"), maxa),
+        exp.GT(exp.IntBin("a"), mina)
+    ).compile()
+    scan_policy = {"expressions": expr}
+    results = await scan.results(scan_policy)
+    print("scan results ===")
+    for r in results:
+        print(r)
+
+
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(put_some_data(5000))
+loop.run_until_complete(read_data(50))
+loop.run_until_complete(use_query(10,20))
+loop.run_until_complete(use_scan(40, 45))
+
+# client.close()
