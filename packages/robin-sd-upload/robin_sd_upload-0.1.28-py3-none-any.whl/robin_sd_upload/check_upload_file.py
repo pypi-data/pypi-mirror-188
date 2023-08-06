da79c3f3d3bfa9147b1412d1af61982308219ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:16 +0000

    Add package robin-sd-upload/0.1.28/robin_sd_upload-0.1.28-py3-none-any.whl

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.28-py3-none-any.whl/robin_sd_upload/check_upload_file.py
@@ -0,0 +1,17 @@
+import os
+
+def check_upload_file(upload_file):
+    print ("Checking upload file...")
+    if os.path.isdir(upload_file):
+        print("Folder exists: " + upload_file)
+        for root, dirs, files in os.walk(upload_file):
+            for file in files:
+                if file == "Packages.gz" or file == "Packages":
+                    print("Packages file found: " + file + "assuming this is a valid folder for upload")
+                    return True
+                else:
+                    print("Packages file not found: " + file)
+                    return False
+    else:
+        print ("File not valid or not exist: " + upload_file)
+        return False
