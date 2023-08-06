99dd727bffce17473429e76560b94ce8e60a89bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:12 +0000

    Add package pyasic/0.25.0/pyasic-0.25.0-py3-none-any.whl

---
+++ packages/pyasic/pyasic-0.25.0-py3-none-any.whl/pyasic/miners/whatsminer/btminer/M3X/M30S.py
@@ -0,0 +1,47 @@
+#  Copyright 2022 Upstream Data Inc
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from pyasic.miners._backends import BTMiner  # noqa - Ignore access to _module
+from pyasic.miners._types import (  # noqa - Ignore access to _module
+    M30S,
+    M30SV50,
+    M30SVE10,
+    M30SVE20,
+    M30SVG20,
+    M30SVG10,
+)
+
+
+class BTMinerM30S(BTMiner, M30S):
+    pass
+
+
+class BTMinerM30SVE10(BTMiner, M30SVE10):
+    pass
+
+
+class BTMinerM30SVG10(BTMiner, M30SVG10):
+    pass
+
+
+class BTMinerM30SVG20(BTMiner, M30SVG20):
+    pass
+
+
+class BTMinerM30SVE20(BTMiner, M30SVE20):
+    pass
+
+
+class BTMinerM30SV50(BTMiner, M30SV50):
+    pass
