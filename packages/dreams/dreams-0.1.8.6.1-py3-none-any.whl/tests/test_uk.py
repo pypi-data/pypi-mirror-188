3d641b2bbf317b5c199d4a9283ae9d6c1bc10886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:29 +0000

    Add package dreams/0.1.8.6.1/dreams-0.1.8.6.1-py3-none-any.whl

---
+++ packages/dreams/dreams-0.1.8.6.1-py3-none-any.whl/tests/test_uk.py
@@ -0,0 +1,12 @@
+from dreams import ukdelviz as uk, __version__
+print(__version__)
+
+
+def test_search_porn():
+    vds = uk.search_porn('lorena aquino')
+    print(vds,'\n')
+    vds = vds.videos
+
+
+
+test_search_porn()
