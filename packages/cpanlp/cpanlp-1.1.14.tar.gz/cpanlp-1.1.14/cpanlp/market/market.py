f172eb7639827e3110aec866f94b0e21e47cf310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:10 +0000

    Add package cpanlp/1.1.14/cpanlp-1.1.14.tar.gz

---
+++ packages/cpanlp/cpanlp-1.1.14.tar.gz/cpanlp-1.1.14/cpanlp/market/market.py
@@ -0,0 +1,53 @@
+from cpanlp.account.income.revenue import *
+from cpanlp.market.commodity import *
+import math
+
+class Market:
+    def __init__(self,commodity,participants):
+        self.commodity = commodity
+        self.participants = participants
+        self.transaction_costs = None
+        self.free_entry=None
+        self.number_of_participants = None 
+class PerfectlyCompetitiveMarket(Market):
+    def __init__(self, commodity, participants):
+        #price takers
+        super().__init__(commodity, participants)
+        self.equilibrium_price = None
+        self.equilibrium_quantity = None
+        self.barriers = False
+        self.free_entry = True
+        self.number_of_participants = math.inf
+    def calculate_equilibrium(self):
+        # Code to calculate equilibrium price and quantity
+        pass
+class MonopolyMarket(Market):
+    def __init__(self, commodity, businessentity):
+        super().__init__(commodity, [])
+        self.businessentity=businessentity
+        self.profit_maximizing_price = None
+        self.profit_maximizing_quantity = None
+        self.market_demand = None
+        self.total_cost = None
+        self.barriers = True
+        self.free_entry = False
+class OligopolyMarket(Market):
+    def __init__(self, commodity, participants):
+        super().__init__(commodity, participants)
+        self.barriers = True
+        self.free_entry = False
+
+    def price_setting(self):
+        """Price setting behavior of firms in oligopoly market"""
+        for firm in self.participants:
+            if firm.market_leader:
+                # Firm sets price based on competitors' reactions
+                pass
+            else:
+                # Firm sets price based on market leader's price
+                pass
+            
+def main():
+    print("hello")
+if __name__ == '__main__':
+    main()
