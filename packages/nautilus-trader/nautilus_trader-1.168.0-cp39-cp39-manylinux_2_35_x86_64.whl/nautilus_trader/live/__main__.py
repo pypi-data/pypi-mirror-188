7d92d4bacaebebbcc8ca4357241d0678a1d1c282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:34 +0000

    Add package nautilus-trader/1.168.0/nautilus_trader-1.168.0-cp39-cp39-manylinux_2_35_x86_64.whl

---
+++ packages/nautilus-trader/nautilus_trader-1.168.0-cp39-cp39-manylinux_2_35_x86_64.whl/nautilus_trader/live/__main__.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+import click
+import fsspec
+import msgspec.json
+
+from nautilus_trader.config import TradingNodeConfig
+from nautilus_trader.live.node import TradingNode
+
+
+@click.command()
+@click.option("--raw", help="A raw string config")
+@click.option("--fsspec-url", help="A fsspec url to read config from")
+@click.option("--start", default=True, help="Start the live node")
+def main(
+    raw: Optional[str] = None,
+    fsspec_url: Optional[str] = None,
+    start: bool = True,
+):
+    assert raw is not None or fsspec_url is not None, "Must pass one of `raw` or `fsspec_url`"
+    if fsspec_url and raw is None:
+        with fsspec.open(fsspec_url, "rb") as f:
+            raw = f.read().decode()
+    config: TradingNodeConfig = msgspec.json.decode(raw, type=TradingNodeConfig)
+    node = TradingNode(config=config)
+    node.build()
+    if start:
+        node.start()
+
+
+if __name__ == "__main__":
+    main()
