9ec7246eb0ed87070268c368e4f209d1756fa45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:53 +0000

    Add package river/0.15.0/river-0.15.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/river/river-0.15.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/river/drift/__init__.py
@@ -0,0 +1,25 @@
+"""
+Concept Drift Detection.
+
+This module contains concept drift detection methods. The purpose of a drift detector is to raise
+an alarm if the data distribution changes. A good drift detector method is the one that maximizes
+the true positives while keeping the number of false positives to a minimum.
+
+"""
+
+from . import binary
+from .adwin import ADWIN
+from .dummy import DummyDriftDetector
+from .kswin import KSWIN
+from .page_hinkley import PageHinkley
+from .retrain import DriftRetrainingClassifier
+
+__all__ = [
+    "binary",
+    "ADWIN",
+    "DriftRetrainingClassifier",
+    "DummyDriftDetector",
+    "KSWIN",
+    "PageHinkley",
+    "PeriodicTrigger",
+]
