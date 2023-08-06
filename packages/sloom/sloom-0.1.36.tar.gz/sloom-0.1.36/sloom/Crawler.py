5fdc49bcf7c7e5eef27b5a2147bfff51ec256f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:19 +0000

    Add package sloom/0.1.36/sloom-0.1.36.tar.gz

---
+++ packages/sloom/sloom-0.1.36.tar.gz/sloom-0.1.36/sloom/Crawler.py
@@ -0,0 +1,24 @@
+from .Fetcher import Fetcher
+from .Scraper import Scraper
+from .Response import Response
+from .RequestError import RequestError
+from .Core.Terminal import Terminal
+
+class Crawler:
+    def __init__(self, root) -> None:
+        self.root = root
+
+    def crawl(self):
+        fetch = Fetcher()
+        response = fetch.get_page(self.root)
+        if self.isError(response): 
+            Terminal.error("Error at root")
+            return
+
+        scraper = Scraper(response)
+
+        href = scraper.gethref()
+        print(href.contents)
+    
+    def isError(self, response): return isinstance(response, RequestError)
+
