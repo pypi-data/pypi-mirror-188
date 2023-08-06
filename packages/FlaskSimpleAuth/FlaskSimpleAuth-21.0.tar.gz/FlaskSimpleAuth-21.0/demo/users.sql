06b8c986fea37e491383e90e12172ecb61f820dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:43 +0000

    Add package FlaskSimpleAuth/21.0/FlaskSimpleAuth-21.0.tar.gz

---
+++ packages/FlaskSimpleAuth/FlaskSimpleAuth-21.0.tar.gz/FlaskSimpleAuth-21.0/demo/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO Auth(login, email, upass, admin) VALUES
+  ('foo', 'foo@school.org', '$2y$04$wkqfOFhHjOojiR.t4aZKtu/4XLfDP3FmQWkZOtP3xqAxAiCpLCNd2', TRUE)
+, ('bla', 'bla@school.org', '$2y$04$.MOcKMaunZHrs/xgaLTW2uNEydV0fB70Hf9O5CRro5NOcsQCD0Cim', TRUE)
+;
