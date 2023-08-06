9568a1648b64274ccc79fa88fa83766ef459fb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:58 +0000

    Add package jhdata/1.0.1/jhdata-1.0.1.tar.gz

---
+++ packages/jhdata/jhdata-1.0.1.tar.gz/jhdata-1.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+NAME = "jhdata"
+DESCRIPTION = "Abstractions around cloud file interfaces (WIP)"
+URL = "https://github.com/jothapunkt/jhdata"
+EMAIL = "jakob-hoefner@web.de"
+AUTHOR = "Jothapunkt"
+VERSION = "1.0.1"
+REQUIRED = [
+    "boto3",
+    "s3fs",
+    "pandas",
+    "pyarrow",
+    "sqlalchemy"
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    install_requires=REQUIRED,
+    url=URL,
+    packages=["jhdata"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8"
+)
