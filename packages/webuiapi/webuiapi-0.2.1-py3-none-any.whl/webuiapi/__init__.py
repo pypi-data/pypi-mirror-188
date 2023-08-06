a267383e143030e98e1163a479c1f6cedc2decd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:23 +0000

    Add package webuiapi/0.2.1/webuiapi-0.2.1-py3-none-any.whl

---
+++ packages/webuiapi/webuiapi-0.2.1-py3-none-any.whl/webuiapi/__init__.py
@@ -0,0 +1,12 @@
+from .webuiapi import WebUIApi, WebUIApiResult, Upscaler, HiResUpscaler
+
+__version__ = "0.2.1"
+
+__all__ = [
+    "__version__",
+    "WebUIApi",
+    "WebUIApiResult",
+    "Upscaler",
+    "HiResUpscaler",
+]
+
