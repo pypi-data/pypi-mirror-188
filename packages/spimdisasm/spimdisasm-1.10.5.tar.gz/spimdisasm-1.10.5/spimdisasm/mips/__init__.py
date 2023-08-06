5d006b863c3ff31a2b7b656bdc6b4916733a0033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:55 +0000

    Add package spimdisasm/1.10.5/spimdisasm-1.10.5.tar.gz

---
+++ packages/spimdisasm/spimdisasm-1.10.5.tar.gz/spimdisasm-1.10.5/spimdisasm/mips/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from . import sections
+from . import symbols
+
+from . import FilesHandlers
+
+from .InstructionConfig import InstructionConfig
+from .MipsFileBase import FileBase, createEmptyFile
+from .MipsFileSplits import FileSplits
