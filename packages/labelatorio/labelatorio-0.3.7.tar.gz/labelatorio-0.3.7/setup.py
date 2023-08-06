2e6cd18bd69dc0c995eb45341e072091a0296185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:56 +0000

    Add package labelatorio/0.3.7/labelatorio-0.3.7.tar.gz

---
+++ packages/labelatorio/labelatorio-0.3.7.tar.gz/labelatorio-0.3.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from src.labelatorio import __version__
+
+
+setuptools.setup(name='labelatorio',
+                version=__version__,
+                description='Labelator.io python client',
+                long_description=open('README.md').read(),
+                long_description_content_type='text/markdown',
+                author='Juraj Bezdek',
+                author_email='juraj.bezdek@blip.solutions',
+                url='https://github.com/blip-solutions/labelatorio-pyclient',
+                package_dir={"": "src"},
+                packages=setuptools.find_packages(where="src"),
+                license='MIT License',
+                zip_safe=False,
+                keywords='client labelator-io',
+
+                classifiers=[
+                ],
+                python_requires='>=3.8',
+                install_requires=[
+                    "pandas",
+                    "requests",
+                    "dataclasses-json",
+                    "marshmallow",
+                    "tqdm",
+                    "pydantic"
+                ]
+                )
---
+++ packages/labelatorio/labelatorio-0.3.7.tar.gz/labelatorio-0.3.7/src/labelatorio/__init__.py
@@ -0,0 +1,5 @@
+from .client import Client
+from .serving import *
+from .query_model import DocumentQueryFilter
+
+__version__ = '0.3.7'
