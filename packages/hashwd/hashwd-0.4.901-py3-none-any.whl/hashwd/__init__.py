8b5ba50da8d0e55695b3425e44108fcb3b412d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:24 +0000

    Add package hashwd/0.4.901/hashwd-0.4.901-py3-none-any.whl

---
+++ packages/hashwd/hashwd-0.4.901-py3-none-any.whl/hashwd/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+import hashwd
+from hashwd import generate_password, clear_clipboard
+import hashwd.pass_defaults
+from hashwd.pass_defaults import WORDS_DEFAULT, NUMBERS_DEFAULT, SYMBOLS_DEFAULT, SYMBOLS
+import hashwd.dictionary
+import hashwd.__main__
+from hashwd.__main__ import main
