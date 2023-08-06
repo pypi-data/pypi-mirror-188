e818e1aa8f6a8f8403905000a55e6684c90f9adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:48 +0000

    Add package devind-yearfrac/0.5.0/devind_yearfrac-0.5.0.tar.gz

---
+++ packages/devind-yearfrac/devind_yearfrac-0.5.0.tar.gz/devind_yearfrac-0.5.0/README.md
@@ -0,0 +1,12 @@
+[![PyPI version](https://badge.fury.io/py/yearfrac.svg)](https://badge.fury.io/py/yearfrac)
+
+# devind_yearfrac
+
+This is a fork of [`yearfrac`](https://github.com/kmedian/yearfrac) for use in
+[`xlsx_evaluate`](https://github.com/devind-team/xlsx_evaluate).
+This fork mainly supports `poetry` and newer versions of Python.
+
+## Installation
+```
+poetry add devind_yearfrac
+```
---
+++ packages/devind-yearfrac/devind_yearfrac-0.5.0.tar.gz/devind_yearfrac-0.5.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['devind_yearfrac']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.24.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'devind-yearfrac',
+    'version': '0.5.0',
+    'description': 'Daycount methods to compute date differences in year units',
+    'long_description': '[![PyPI version](https://badge.fury.io/py/yearfrac.svg)](https://badge.fury.io/py/yearfrac)\n\n# devind_yearfrac\n\nThis is a fork of [`yearfrac`](https://github.com/kmedian/yearfrac) for use in\n[`xlsx_evaluate`](https://github.com/devind-team/xlsx_evaluate).\nThis fork mainly supports `poetry` and newer versions of Python.\n\n## Installation\n```\npoetry add devind_yearfrac\n```\n',
+    'author': 'Ulf Hamster',
+    'author_email': '554c46@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/devind-team/devind_yearfrac',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
