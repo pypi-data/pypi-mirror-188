ff3a65d9ee7eee00c60c128a23aaa38dd445f59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:25 +0000

    Add package gdshoplib/1.16.4/gdshoplib-1.16.4-py3-none-any.whl

---
+++ packages/gdshoplib/gdshoplib-1.16.4-py3-none-any.whl/gdshoplib/apps/platforms/__init__.py
@@ -0,0 +1,19 @@
+from .avito import AvitoManager
+from .base import Platform
+from .instagram import InstagramManager
+from .ok import OkManager
+from .tg import TgManager
+from .ula import UlaManager
+from .vk import VKManager
+from .yam import YandexMarketManager
+
+__all__ = (
+    "AvitoManager",
+    "InstagramManager",
+    "OkManager",
+    "TgManager",
+    "UlaManager",
+    "VKManager",
+    "YandexMarketManager",
+    "Platform",
+)
