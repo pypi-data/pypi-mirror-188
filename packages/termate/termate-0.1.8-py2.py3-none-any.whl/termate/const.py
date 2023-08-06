edca213f2865a03082e18b7e086a73e1ff275e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:26 +0000

    Add package termate/0.1.8/termate-0.1.8-py2.py3-none-any.whl

---
+++ packages/termate/termate-0.1.8-py2.py3-none-any.whl/termate/const.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+This module contains constants from termate
+
+"""
+
+from lxml import etree
+
+
+NAMESPACES = {
+    None: "urn:iso:std:iso:30042:ed-2",
+}
+XML_LANG = "{http://www.w3.org/XML/1998/namespace}lang"
+TBX_RELAXNG = "relaxng_tbx"
+TBX_SCHEMA = "schema_tbx"
+TBX_DIALECT = "dialect"
+TBX_STYLE = "style"
+TBX_HEADER = "tbxHeader"
+FILEDESC = "fileDesc"
+TITLESTMT = "titleStmt"
+PUBLICATIONSTMT = "publicationStmt"
+SOURCEDESC = "sourceDesc"
+TEXT = "text"
+BODY = "body"
+TITLE = "title"
+PUBLICATION = "publication"
+LANGSEC = "langSec"
+
+
+def QName(prefix: str = None, name: str = None):
+    """ """
+    if prefix is None:
+        qname = etree.QName("{urn:iso:std:iso:30042:ed-2}" + name, name)
+    else:
+        qname = etree.QName("{" + namespaces[prefix] + "}" + name, name)
+    return qname
