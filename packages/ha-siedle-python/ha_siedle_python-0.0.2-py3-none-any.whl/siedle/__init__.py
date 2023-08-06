210bb3c7a2c1922b3518f6c5b17d03e8248fcd50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:12 +0000

    Add package ha-siedle-python/0.0.2/ha_siedle_python-0.0.2-py3-none-any.whl

---
+++ packages/ha-siedle-python/ha_siedle_python-0.0.2-py3-none-any.whl/siedle/__init__.py
@@ -0,0 +1,21 @@
+from asyncio import *
+
+class Siedle:
+    def __init__():
+        
+        pass
+
+
+    def readQrCode():
+        pass
+    
+    def registerNewDevice():
+        pass
+    
+    def openDoor():
+        pass
+    
+    def establishSipConnection():
+        pass
+    
+    
