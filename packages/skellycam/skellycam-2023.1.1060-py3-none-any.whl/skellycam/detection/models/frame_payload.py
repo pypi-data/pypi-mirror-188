a2b806cbf44081e7be98d8ac71263c35b7b11cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:32 +0000

    Add package skellycam/2023.1.1060/skellycam-2023.1.1060-py3-none-any.whl

---
+++ packages/skellycam/skellycam-2023.1.1060-py3-none-any.whl/skellycam/detection/models/frame_payload.py
@@ -0,0 +1,12 @@
+import dataclasses
+
+import numpy as np
+
+
+@dataclasses.dataclass()
+class FramePayload:
+    success: bool = False
+    image: np.ndarray = None
+    timestamp_ns: float = None
+    frame_number: int = None
+    camera_id: str = None
