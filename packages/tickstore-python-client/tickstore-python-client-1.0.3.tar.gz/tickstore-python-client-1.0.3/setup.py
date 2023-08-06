eb02a29493c4933927465e3ed86a3390f13f3052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:25 +0000

    Add package tickstore-python-client/1.0.3/tickstore-python-client-1.0.3.tar.gz

---
+++ packages/tickstore-python-client/tickstore-python-client-1.0.3.tar.gz/tickstore-python-client-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='tickstore-python-client',
+    version='1.0.3',
+    packages=['tickstore', 'tickstore/db', 'tickstore/query', 'tickstore/writers'],
+    url='https://gitlab.com/alphaticks/tickstore-python-client',
+    license='copyright',
+    author='Alphaticks',
+    description='client to communicate with tickstore',
+    install_requires=[
+        'protobuf==4.21.12',
+        'tickstore-grpc==1.0.1',
+    ]
+)
---
+++ packages/tickstore-python-client/tickstore-python-client-1.0.3.tar.gz/tickstore-python-client-1.0.3/tickstore/query/__init__.py
@@ -0,0 +1,4 @@
+from .query import Query, QueryFunctor
+from .query_group import QueryGroup
+from .query_settings import QuerySettings
+from .sampler import TickSampler
