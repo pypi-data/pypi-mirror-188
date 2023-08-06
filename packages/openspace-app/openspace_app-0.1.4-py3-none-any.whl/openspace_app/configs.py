b78cea8beaee42573ce9cd2bfb6b6a63d7609af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:15 +0000

    Add package openspace-app/0.1.4/openspace_app-0.1.4-py3-none-any.whl

---
+++ packages/openspace-app/openspace_app-0.1.4-py3-none-any.whl/openspace_app/configs.py
@@ -0,0 +1,18 @@
+NAV_STYLE = {
+    "background-color": "#010332",
+    "borderRadius": "15px",
+    "margin-left": "5%",
+    "margin-right": "1%",
+    "margin-bottom": "5%",
+    "box-shadow": "2px 2px 2px #fc01fe",
+    "height": "95",
+}
+LAYOUT_BG_STYLE = {"background-color": "black", "height": "100vh"}
+CONTENT_STYLE = {
+    "background-color": "#010332",
+    "borderRadius": "15px",
+    "margin-right": "5%",
+    "margin-bottom": "5%",
+    "box-shadow": "2px 2px 2px #fc01fe",
+    "height": "95%",
+}
