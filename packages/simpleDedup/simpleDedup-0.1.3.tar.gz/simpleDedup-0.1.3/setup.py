8b86733023933fe659b18b4968f10c4b96370d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:40 +0000

    Add package simpleDedup/0.1.3/simpleDedup-0.1.3.tar.gz

---
+++ packages/simpleDedup/simpleDedup-0.1.3.tar.gz/simpleDedup-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+print(find_packages())
+
+requires = [
+    'redis == 4.4.2'
+]
+
+
+setup(
+    name="simpleDedup",
+    version="0.1.3",
+    packages=find_packages(),
+    description="提供多种简单爬虫url去重",
+    author="phimes",
+    author_email="phimes@163.com",
+)
