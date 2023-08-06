677fbd71342997fc497edd703603773d63330060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:52 +0000

    Add package django-sqs-extended-client/2.5.2/django_sqs_extended_client-2.5.2-py3-none-any.whl

---
+++ packages/django-sqs-extended-client/django_sqs_extended_client-2.5.2-py3-none-any.whl/django_sqs_extended_client/event/event_dispatcher.py
@@ -0,0 +1,8 @@
+from .event_base_aws import EventBaseAws
+from django.conf import settings
+
+
+class EventDispatcher(EventBaseAws):
+
+    AWS_SNS_TOPIC = settings.AWS_SNS_TOPIC
+    IS_FIFO = False
