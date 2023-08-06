89ea5d453a0e396d0095c5cbcb7b96e11fa1545b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:23 +0000

    Add package pydsdl/1.18.0/pydsdl-1.18.0.tar.gz

---
+++ packages/pydsdl/pydsdl-1.18.0.tar.gz/pydsdl-1.18.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020 OpenCyphal
+# This software is distributed under the terms of the MIT License.
+# Author: Pavel Kirienko <pavel@opencyphal.org>
+# type: ignore
+
+import sys
+import setuptools
+
+if int(setuptools.__version__.split(".")[0]) < 30:
+    print(
+        "A newer version of setuptools is required. The current version does not support declarative config.",
+        file=sys.stderr,
+    )
+    sys.exit(1)
+
+setuptools.setup()
