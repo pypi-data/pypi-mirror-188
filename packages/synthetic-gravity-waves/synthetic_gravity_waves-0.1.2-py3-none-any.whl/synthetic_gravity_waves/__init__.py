d22d519129d63d4c68fc6b510f1c543d68af89de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:10 +0000

    Add package synthetic-gravity-waves/0.1.2/synthetic_gravity_waves-0.1.2-py3-none-any.whl

---
+++ packages/synthetic-gravity-waves/synthetic_gravity_waves-0.1.2-py3-none-any.whl/synthetic_gravity_waves/__init__.py
@@ -0,0 +1,5 @@
+from .components import make_2d_wavepacket, make_carrier_wave_2d, make_gaussian_2d
+from .composite import make_synthetic_gravity_wave_composite
+from .point_sampling import modified_poisson_disk_sampling
+
+__version__ = "0.1.2"
