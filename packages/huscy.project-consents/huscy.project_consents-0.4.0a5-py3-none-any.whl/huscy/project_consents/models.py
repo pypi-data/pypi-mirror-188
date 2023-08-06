24a2c670d346a7bee7d2ffeee3525421a17d2ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:50 +0000

    Add package huscy.project-consents/0.4.0a5/huscy.project_consents-0.4.0a5-py3-none-any.whl

---
+++ packages/huscy.project-consents/huscy.project_consents-0.4.0a5-py3-none-any.whl/huscy/project_consents/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+from huscy.consents.models import Consent, ConsentCategory, ConsentFile
+from huscy.projects.models import Project
+from huscy.subjects.models import Subject
+
+
+class ProjectConsentCategory(models.Model):
+    consent_category = models.OneToOneField(ConsentCategory, on_delete=models.PROTECT)
+
+    @property
+    def name(self):
+        return self.consent_category.name
+
+    @property
+    def template_text_fragments(self):
+        return self.consent_category.template_text_fragments
+
+
+class ProjectConsent(models.Model):
+    project = models.OneToOneField(Project, on_delete=models.CASCADE)
+    consent = models.ForeignKey(Consent, on_delete=models.PROTECT)
+
+    @property
+    def name(self):
+        return self.consent.name
+
+    @property
+    def text_fragments(self):
+        return self.consent.text_fragments
+
+
+class ProjectConsentFile(models.Model):
+    project_consent = models.ForeignKey(ProjectConsent, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    consent_file = models.OneToOneField(ConsentFile, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = 'project_consent', 'subject'
