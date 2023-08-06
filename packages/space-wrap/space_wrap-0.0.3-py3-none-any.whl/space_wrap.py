ff24ca9e109b04e092d22a9397449562d805dc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:57 +0000

    Add package space-wrap/0.0.3/space_wrap-0.0.3-py3-none-any.whl

---
+++ packages/space-wrap/space_wrap-0.0.3-py3-none-any.whl/space_wrap.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import spacy
+import pandas as pd
+
+
+# In[5]:
+
+
+class space_wrap:
+    def __init__(self, data, column, model):
+        self.data = data
+        self.column = column
+        self.model = model
+    
+    def create_df(self):
+        df = pd.DataFrame(self.data)
+        df = pd.DataFrame(df[self.column])
+        df[self.column] = df[self.column].astype(str).apply(lambda x: x.lower())
+        rows = []
+        nlp = spacy.load(self.model)
+
+        # iterate over the rows of the dataframe
+        for i, row in df.iterrows():
+            doc = nlp(row[self.column])
+            # iterate over the tokens
+            for token in doc:
+                # add a new row to the list with the word, the original sentence, the start index, the end index and the entity
+                rows.append([token.text, row[self.column], token.i, token.i+1, token.ent_type_])
+
+        # create a new dataframe with the desired columns
+        df_new = pd.DataFrame(rows, columns=['token', 'text', 'start','end','entity'])
+        return df_new
+
+
+
+
