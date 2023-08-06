0a54e15ffe0a8e27db9e5fb67cc402f9162f749b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:16 +0000

    Add package pollai/0.1.1/pollai-0.1.1.tar.gz

---
+++ packages/pollai/pollai-0.1.1.tar.gz/pollai-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pollai',
+    version='0.1.1',
+    author='ByteVolx',
+    author_email='onions193@gmail.com',
+    description='AI models simplified and implemented',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/PollAI/pollai',
+    packages=find_packages(),
+    install_requires=[
+        "scikit-learn",
+        "numpy"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    package_dir={'pollai': 'src/pollai'},
+    include_package_data=True
+)
