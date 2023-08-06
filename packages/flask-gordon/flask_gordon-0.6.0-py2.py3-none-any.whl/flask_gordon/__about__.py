a1c10af8104b8cd66d112b4e7bae3317bb3522ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:55 +0000

    Add package flask-gordon/0.6.0/flask_gordon-0.6.0-py2.py3-none-any.whl

---
+++ packages/flask-gordon/flask_gordon-0.6.0-py2.py3-none-any.whl/flask_gordon/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "flask-gordon"
+__summary__ = "Flask blueprint, route, and middleware extensions"
+__uri__ = "https://gitlab.com/jlecomte/projects/flask-gordon"
+
+__version__ = "0.3.2"
+
+__author__ = "Julien Lecomte"
+__email__ = "julien@lecomte.at"
+
+__license__ = "MIT"
+__copyright__ = f"2022-2023 {__author__}"
