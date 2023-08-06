7718ba086e3692f74931b5e4d3c4eb7211093487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:33 +0000

    Add package mobotix-finder/0.0/mobotix_finder-0.0-py3-none-any.whl

---
+++ packages/mobotix-finder/mobotix_finder-0.0-py3-none-any.whl/mobotix_finder/__about__.py
@@ -0,0 +1,6 @@
+__author__ = "Hannah Einstein"
+__author_email__ = "hannah.a.einstein@gmail.com"
+__copyright__ = "Copyright (c) 2023 Hannah Einstein"
+__description__ = "Mobotix camera finder using ZoomEye"
+__license__ = "MIT"
+__version__ = "0.0"
