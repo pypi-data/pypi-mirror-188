43cadbb2f9cb972ebaae9ee7b65f1103148ec222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:52 +0000

    Add package sleapyfaces/1.2.1/sleapyfaces-1.2.1.tar.gz

---
+++ packages/sleapyfaces/sleapyfaces-1.2.1.tar.gz/sleapyfaces-1.2.1/sleapyfaces/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2022-present Annie Ehler <annie.ehler.4@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+from __about__ import *
+
+import base
+import files
+import utils.structs
+import utils.normalize
+import utils
+import clustering
+import docstring
+import config
