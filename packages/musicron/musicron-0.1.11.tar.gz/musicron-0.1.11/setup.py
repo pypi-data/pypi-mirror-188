31bba75138e07117061b2a1453d7feca4f7d0ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:38 +0000

    Add package musicron/0.1.11/musicron-0.1.11.tar.gz

---
+++ packages/musicron/musicron-0.1.11.tar.gz/musicron-0.1.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages,find_namespace_packages
+
+setup(
+    name="musicron",
+    version="0.1.11",
+    author="Wesley Fisher",
+    author_email="wfisher@praetor.tel",
+    description="A cron for music!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gwfisher/musicron",
+    packages=["musicron", "musicron.core", "musicron.plugins"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "pyyaml",
+        "dbus-python",
+        "pathlib",
+        "importlib2",
+        "psutil",
+        "python-dateutil"
+    ],
+)
