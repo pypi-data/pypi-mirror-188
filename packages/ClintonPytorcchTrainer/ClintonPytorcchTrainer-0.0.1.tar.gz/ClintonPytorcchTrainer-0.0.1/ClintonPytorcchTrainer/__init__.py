a0eedd25953b388a57f1a7fd2a61c70598d80d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:22 +0000

    Add package ClintonPytorcchTrainer/0.0.1/ClintonPytorcchTrainer-0.0.1.tar.gz

---
+++ packages/ClintonPytorcchTrainer/ClintonPytorcchTrainer-0.0.1.tar.gz/ClintonPytorcchTrainer-0.0.1/ClintonPytorcchTrainer/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is a PyTorch Model Trainer Class. It trains, validates input data.
+It also saves the model after training to avoid retraining.
+"""
+
+from .core import TrainTestSave
+
+__version__ = '0.0.1'
