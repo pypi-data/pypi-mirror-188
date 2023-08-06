290dabe9a82df425b312ff6b3790dfe418f0cbe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:29 +0000

    Add package supOdoo/1.0.0/supOdoo-1.0.0.tar.gz

---
+++ packages/supOdoo/supOdoo-1.0.0.tar.gz/supOdoo-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+lib_name = "odoo"
+
+setup(
+    name="supOdoo",
+    version="1.0.0",
+    description="Simple cli tool for Suplyd devs to run local Odoo setup without hassel",
+    url="https://github.com/saurabhjainwal-suplyd/suplyd-odoo-cli-runner",
+    author="saurabhjainwal",
+    author_email="saurabh@suplyd.app",
+    license="MIT",
+    scripts=[
+        "./sup-odoo/__init__.py",
+        "./sup-odoo/__main__.py",
+        "./sup-odoo/docker-compose.yaml",
+    ],
+    packages=find_packages(),
+    package_dir={"%s" % lib_name: "odoo"},
+    include_package_data=True,
+    install_requires=["Command", "typer"],
+    python_requires=">=3.7",
+)
