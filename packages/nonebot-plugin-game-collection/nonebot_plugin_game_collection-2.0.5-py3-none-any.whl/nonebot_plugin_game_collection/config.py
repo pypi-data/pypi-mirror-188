f86c62fcf9ca134558d17d17b635a9e77492155c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:54 +0000

    Add package nonebot-plugin-game-collection/2.0.5/nonebot_plugin_game_collection-2.0.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-game-collection/nonebot_plugin_game_collection-2.0.5-py3-none-any.whl/nonebot_plugin_game_collection/config.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+from pydantic import BaseModel, Extra
+from pathlib import Path
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    russian_path: Path = Path()
+    max_bet_gold: int = 1000
+    race_bet_gold: int = 100
+    gacha_gold: int = 50
+    sign_gold: Tuple[int, int] = (100, 300)
+    revolt_sign_gold:Tuple[int, int] = (800, 1200)
+    security_gold:Tuple[int, int] = (50, 200)
+
+    lucky_clover= "• ＬＵＣＫＹ  ＣＬＯＶＥＲ •"
