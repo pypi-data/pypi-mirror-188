1bb7e7e75c33ab63b14148c45357f8d514bbf6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:04 +0000

    Add package pysp2/1.2.0/pysp2-1.2.0.tar.gz

---
+++ packages/pysp2/pysp2-1.2.0.tar.gz/pysp2-1.2.0/pysp2/vis/__init__.py
@@ -0,0 +1,12 @@
+"""
+pysp2.vis
+---------
+
+This module contains visualization subroutines for SP2 data.
+
+.. autosummary::
+    :toctree: generated/
+
+    plot_wave
+"""
+from .plot_wave import plot_wave
