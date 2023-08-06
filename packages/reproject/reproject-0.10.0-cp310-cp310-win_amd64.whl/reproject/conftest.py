cdd36dc81868819cc68495667d38ad89e4d2e642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:53 +0000

    Add package reproject/0.10.0/reproject-0.10.0-cp310-cp310-win_amd64.whl

---
+++ packages/reproject/reproject-0.10.0-cp310-cp310-win_amd64.whl/reproject/conftest.py
@@ -0,0 +1,33 @@
+# This file is used to configure the behavior of pytest when using the Astropy
+# test infrastructure. It needs to live inside the package in order for it to
+# get picked up when running the tests inside an interpreter using
+# packagename.test
+
+import os
+
+try:
+    from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
+
+    ASTROPY_HEADER = True
+except ImportError:
+    ASTROPY_HEADER = False
+
+os.environ["MPLBACKEND"] = "Agg"
+
+
+def pytest_configure(config):
+
+    if ASTROPY_HEADER:
+
+        config.option.astropy_header = True
+
+        PYTEST_HEADER_MODULES.pop("Pandas", None)
+        PYTEST_HEADER_MODULES.pop("h5py", None)
+        PYTEST_HEADER_MODULES.pop("Matplotlib", None)
+        PYTEST_HEADER_MODULES["Astropy"] = "astropy"
+        PYTEST_HEADER_MODULES["astropy-healpix"] = "astropy_healpix"
+        PYTEST_HEADER_MODULES["Cython"] = "cython"
+
+        from reproject import __version__
+
+        TESTED_VERSIONS["reproject"] = __version__
