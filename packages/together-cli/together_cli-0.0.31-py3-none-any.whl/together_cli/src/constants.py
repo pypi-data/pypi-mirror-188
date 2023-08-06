47e13a0384ef55a84391f0513706b435aff46bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:05 +0000

    Add package together-cli/0.0.31/together_cli-0.0.31-py3-none-any.whl

---
+++ packages/together-cli/together_cli-0.0.31-py3-none-any.whl/together_cli/src/constants.py
@@ -0,0 +1,43 @@
+MODEL_CONFIG = {
+    'opt-6.7b': {
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
+        "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-6.7b-tp1.tar.gz",
+        "docker_id": "xzyaoi/faster_transformer:ampere",
+        "sif_name": "ft_base_ampere.sif",
+        "worker_model":"opt-6.7b-tp1",
+        "model_type": "gpt",
+        "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
+    },
+    'opt-1.3b': {
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/ft_base_ampere.sif",
+        "weights_url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/opt-1.3b-tp1.tar.gz",
+        "worker_model":"opt-1.3b-tp1",
+        "docker_id": "xzyaoi/faster_transformer:ampere",
+        "sif_name": "ft_base_ampere.sif",
+        "model_type": "gpt",
+        "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
+    },
+    'stable-diffusion': {
+        "singularity_url": "https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/toma_models/stablediffusion_general.sif",
+        "worker_model":"StableDiffusion",
+        "docker_id": "xzyaoi/stablediffusion:general",
+        "sif_name": "stablediffusion_general.sif",
+        "startup_command":"/home/user/serve.sh"
+    },
+    'gpt-jt': {
+        "singularity_url": "https://together-singularity.s3.us-west-2.amazonaws.com/ft_base_ampere.sif",
+        "weights_url":"https://together-singularity.s3.us-west-2.amazonaws.com/gpt-jt-6b-v1.tar.gz",
+        "worker_model":"GPT-JT-6B-v1-tp1",
+        "docker_id": "xzyaoi/faster_transformer:ampere",
+        "sif_name": "ft_base_ampere.sif",
+        "model_type": "gptj",
+        "startup_command":"/workspace/Port_FasterTransformer/serve.sh"
+    }
+}
+
+BINARY_CONFIG = {
+    "GO-TOGETHER": {
+        "url":"https://filedn.eu/lougUsdPvd1uJK2jfOYWogH/together",
+        "config_url":"",
+    }
+}
