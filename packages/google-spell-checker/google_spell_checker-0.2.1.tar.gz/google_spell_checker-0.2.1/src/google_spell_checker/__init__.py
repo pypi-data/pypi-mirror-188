d97edc1a0ed0733157678781c0a420ac27774289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:46 +0000

    Add package google-spell-checker/0.2.1/google_spell_checker-0.2.1.tar.gz

---
+++ packages/google-spell-checker/google_spell_checker-0.2.1.tar.gz/google_spell_checker-0.2.1/src/google_spell_checker/__init__.py
@@ -0,0 +1,9 @@
+"""
+Google Spell Checker.
+A simple multilingual Spell Checker using Google Text Completion.
+"""
+from .google_spell_checker import GoogleSpellChecker
+
+__version__ = "0.2.1"
+__author__ = 'Cahya Wirawan'
+__credits__ = ''
