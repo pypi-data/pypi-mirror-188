9dc5648610eebb2db948d6fadbac94c01291deec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:23 +0000

    Add package csvimporteract/0.0.2/csvimporteract-0.0.2.tar.gz

---
+++ packages/csvimporteract/csvimporteract-0.0.2.tar.gz/csvimporteract-0.0.2/csvimporteract/csvimporter.py
@@ -0,0 +1,64 @@
+#-*- coding: utf-8 -*-
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+import csv
+
+
+def stackTodb(dataFrame, dbTableName):
+    print(dataFrame)
+    db_connection_str = 'mysql+pymysql://root:12345@127.0.0.1:3307/stackline'
+    db_connection = create_engine(db_connection_str, encoding='utf-8')
+    conn = db_connection.connect()
+
+    dataFrame.to_sql(name=dbTableName, con=db_connection, if_exists='append', index=False)
+    print("finished")
+
+
+def csv_cleaning(csvFile, csv_folder_cleaned):
+    file=open(csvFile,'r', encoding='latin1')
+    targetFile = csv_folder_cleaned + "\\CLEANED_" + os.path.basename(csvFile)
+    target=open(targetFile,'w', encoding='latin1')
+    
+    for line in file:
+        target.write(line[:-1].rstrip(',') + "\n")
+
+    file.close()
+    target.close()
+
+def csvCleaner(csv_folder, csv_folder_cleaned):
+    fileList = os.listdir(csv_folder)
+
+    for i in range(len(fileList)):
+        csv_cleaning(csv_folder + "\\" + fileList[i], csv_folder_cleaned)
+
+
+def csvImporter(csv_folder, csv_folder_cleaned, db_table_name):
+    column_name = ["retailer_id", "retailer_name", "retailer_sku", "upc", "model_number", "title", "brand", "category", "subcategory", "week_id", "week_ending", "units_sold", "retail_sales", "retail_price", "conversion_rate", "total_traffic", "organic_traffic"]
+    csvCleaner(csv_folder, csv_folder_cleaned)
+
+    fileList = os.listdir(csv_folder_cleaned)
+    for i in range(len(fileList)):
+        if "Laptops" in fileList[i]:
+            db_table_name = "tb_stackline_laptop"
+        elif "Headphones" in fileList[i]:
+            db_table_name = "tb_stackline_headphone"
+        elif "Watches" in fileList[i]:
+            db_table_name = "tb_stackline_watch"
+        elif "Tablets" in fileList[i]:
+            db_table_name = "tb_stackline_tablet"            
+        elif "Phones" in fileList[i]:
+            db_table_name = "tb_stackline_cell_phone"
+
+        dataframe = pd.read_csv(csv_folder_cleaned + "\\" + fileList[i], sep=',', engine='c', encoding='latin1')
+        dataframe.columns = column_name
+        dataframe.insert(0, "file_name", fileList[i][:-4], True)
+
+        stackTodb(dataframe, db_table_name)
+
+# if __name__ == "__main__":
+#     db_table_name = 'tb_stackline_dataset'
+#     csv_folder = "csvFolder"
+#     csv_folder_cleaned = "csvFolder_cleaned"
+     
+#     csvImporter(csv_folder, csv_folder_cleaned, db_table_name)
