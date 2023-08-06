c1d1e8e2c7a8c741b36ca62455405cf255913184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:23 +0000

    Add package melid/1.0.3/melid-1.0.3.tar.gz

---
+++ packages/melid/melid-1.0.3.tar.gz/melid-1.0.3/README.md
@@ -0,0 +1,26 @@
+# Melid
+
+Melid is a PyQt5 Library for Desktop Applications containing commonly used utils and advanced widget implementations in very simple forms.
+
+# Features
+
+- [x] router -> done
+- [ ] store (state management) -> in progress
+- [ ] hot-reload -> in progress
+- [x] CSS Processor (TailwindCSS Syntax) -> done
+
+# Install
+
+```sh
+$ pip install melid
+```
+
+# Usage
+
+Check the examples folder for usage examples.
+
+```sh
+$ git clone https://github.com/dev-47/melid.git
+$ cd examples/basic
+$ python main.py
+```
---
+++ packages/melid/melid-1.0.3.tar.gz/melid-1.0.3/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['melid', 'melid.base', 'melid.router']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'melid',
+    'version': '1.0.3',
+    'description': 'Melid',
+    'long_description': '# Melid\n\nMelid is a PyQt5 Library for Desktop Applications containing commonly used utils and advanced widget implementations in very simple forms.\n\n# Features\n\n- [x] router -> done\n- [ ] store (state management) -> in progress\n- [ ] hot-reload -> in progress\n- [x] CSS Processor (TailwindCSS Syntax) -> done\n\n# Install\n\n```sh\n$ pip install melid\n```\n\n# Usage\n\nCheck the examples folder for usage examples.\n\n```sh\n$ git clone https://github.com/dev-47/melid.git\n$ cd examples/basic\n$ python main.py\n```\n',
+    'author': 'Mohammed Al Ameen',
+    'author_email': 'ameenmohammed2311@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+}
+
+
+setup(**setup_kwargs)
