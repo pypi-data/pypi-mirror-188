eb5f8dd7c4cee52f028e31ca54d8e5c3780995f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:10 +0000

    Add package aio.api.github/0.1.5/aio.api.github-0.1.5-py3-none-any.whl

---
+++ packages/aio.api.github/aio.api.github-0.1.5-py3-none-any.whl/aio/api/github/abstract/release.py
@@ -0,0 +1,32 @@
+
+from functools import cached_property
+from typing import Callable, Dict, Union
+
+from packaging import version
+
+import abstracts
+
+from aio.api.github import interface, utils
+from .base import GithubRepoEntity
+
+
+@abstracts.implementer(interface.IGithubRelease)
+class AGithubRelease(GithubRepoEntity, metaclass=abstracts.Abstraction):
+    """A Github release."""
+
+    def __str__(self):
+        return f"<{self.__class__.__name__} {self.repo.name}@{self.tag_name}>"
+
+    @cached_property
+    def __data__(self) -> Dict[str, Callable]:
+        return dict(
+            created_at=utils.dt_from_js_isoformat,
+            published_at=utils.dt_from_js_isoformat)
+
+    @cached_property
+    def version(self) -> Union[version.Version]:
+        return version.parse(self.tag_name)
+
+    @property
+    def tag_name(self):
+        return self.data["tag_name"]
