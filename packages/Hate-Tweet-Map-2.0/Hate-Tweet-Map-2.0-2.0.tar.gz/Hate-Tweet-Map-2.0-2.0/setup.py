10807cc288dce0265a4af7ed578e07eac723ac6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:33 +0000

    Add package Hate-Tweet-Map-2.0/2.0/Hate-Tweet-Map-2.0-2.0.tar.gz

---
+++ packages/Hate-Tweet-Map-2.0/Hate-Tweet-Map-2.0-2.0.tar.gz/Hate-Tweet-Map-2.0-2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='Hate-Tweet-Map-2.0',
+    version='2.0',
+    description='sentiment analyzer for tweets written in italian',
+    url='',
+    author='sdvolpicella',
+    author_email='davidevolpicella@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='hate tweet map',
+    packages=find_packages(),
+    install_requires=['']
+)
