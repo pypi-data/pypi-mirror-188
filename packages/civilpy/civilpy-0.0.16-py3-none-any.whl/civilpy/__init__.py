90a757abbc8badba96dc70bdf561a865941a9b44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:54 +0000

    Add package civilpy/0.0.16/civilpy-0.0.16-py3-none-any.whl

---
+++ packages/civilpy/civilpy-0.0.16-py3-none-any.whl/civilpy/__init__.py
@@ -0,0 +1,9 @@
+"""
+CivilPy main function, running `python civilpy` from the commandline executes the code
+contained here
+
+"""
+import civilpy.CLI
+
+if __name__ == '__main__':
+    CLI.civilpy_cli()
