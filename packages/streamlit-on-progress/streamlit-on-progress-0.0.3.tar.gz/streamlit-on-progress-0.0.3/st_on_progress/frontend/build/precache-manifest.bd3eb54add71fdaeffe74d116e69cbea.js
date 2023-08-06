28f873629094bce5fef9cee0e0e8f4f5210e59e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:18 +0000

    Add package streamlit-on-progress/0.0.3/streamlit-on-progress-0.0.3.tar.gz

---
+++ packages/streamlit-on-progress/streamlit-on-progress-0.0.3.tar.gz/streamlit-on-progress-0.0.3/st_on_progress/frontend/build/precache-manifest.bd3eb54add71fdaeffe74d116e69cbea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40ca31e0cf71f4fa8727a22fa0d721d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f5b4d78d28915d12779",
+    "url": "./static/css/main.239f2e9a.chunk.css"
+  },
+  {
+    "revision": "fc84354c83f0ccf156d6",
+    "url": "./static/js/2.15acec80.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.15acec80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5b4d78d28915d12779",
+    "url": "./static/js/main.0ee0b23e.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
