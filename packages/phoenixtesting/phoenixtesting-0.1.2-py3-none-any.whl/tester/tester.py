0c58447afdb9d122f41454ef6abafc7939b85900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:12 +0000

    Add package phoenixtesting/0.1.2/phoenixtesting-0.1.2-py3-none-any.whl

---
+++ packages/phoenixtesting/phoenixtesting-0.1.2-py3-none-any.whl/tester/tester.py
@@ -0,0 +1,14 @@
+import unittest
+
+class Test(unittest.TestCase):
+
+    def test_addition(self):
+        self.assertEquals(self.test_obj.addition(1,2),3)
+
+    def test_subtraction(self):
+        self.assertEquals(self.test_obj.subtraction(1,2),-1)
+
+    def test_multiplication(self):
+        self.assertEquals(self.test_obj.multiplication(1,2),-1)
+
+
