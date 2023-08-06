ff629fb0a1d04d3201aca1ffc1c9f88d4a440454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:57 +0000

    Add package streamlit-on-machine-data/7.0.3/streamlit_on_machine_data-7.0.3-py3-none-any.whl

---
+++ packages/streamlit-on-machine-data/streamlit_on_machine_data-7.0.3-py3-none-any.whl/st_on_machine_data/frontend/build/precache-manifest.9b3b757575d6c36130719bca3f49ccaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e25cb8ae452b0e5035eeffa33de5ce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0c5aa83bac540b7614d",
+    "url": "./static/css/main.6b42c827.chunk.css"
+  },
+  {
+    "revision": "3dd013eb24346f810442",
+    "url": "./static/js/2.2bb533d1.chunk.js"
+  },
+  {
+    "revision": "abb38f03b4873ee4a8daa94530086add",
+    "url": "./static/js/2.2bb533d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c5aa83bac540b7614d",
+    "url": "./static/js/main.630d81e3.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
