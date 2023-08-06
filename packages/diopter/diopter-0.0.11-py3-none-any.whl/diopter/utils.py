c252b0b99f9d5d9baa34617b095cb5c3463f16ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:30 +0000

    Add package diopter/0.0.11/diopter-0.0.11-py3-none-any.whl

---
+++ packages/diopter/diopter-0.0.11-py3-none-any.whl/diopter/utils.py
@@ -0,0 +1,104 @@
+import os
+import shlex
+import subprocess
+import tempfile
+from dataclasses import dataclass
+from pathlib import Path
+from types import TracebackType
+from typing import IO, Any, TextIO, Union
+
+
+@dataclass(frozen=True, kw_only=True)
+class CommandOutput:
+    stdout: str
+    stderr: str
+
+
+def run_cmd(
+    cmd: Union[str, list[str]],
+    working_dir: Path | None = None,
+    additional_env: dict[str, str] = {},
+    **kwargs: Any,  # https://github.com/python/mypy/issues/8772
+) -> CommandOutput:
+
+    if working_dir is None:
+        working_dir = Path(os.getcwd())
+    env = os.environ.copy()
+    env.update(additional_env)
+
+    if isinstance(cmd, list):
+        cmd = " ".join(cmd)
+    output = subprocess.run(
+        shlex.split(cmd.replace('"', '\\"')),
+        cwd=str(working_dir),
+        check=True,
+        env=env,
+        capture_output=True,
+        **kwargs,
+    )
+
+    return CommandOutput(
+        stdout=output.stdout.decode("utf-8").strip(),
+        stderr=output.stderr.decode("utf-8").strip(),
+    )
+
+
+def run_cmd_to_logfile(
+    cmd: Union[str, list[str]],
+    log_file: TextIO | None = None,
+    working_dir: Path | None = None,
+    additional_env: dict[str, str] = {},
+) -> None:
+
+    if working_dir is None:
+        working_dir = Path(os.getcwd())
+    env = os.environ.copy()
+    env.update(additional_env)
+
+    if isinstance(cmd, list):
+        cmd = " ".join(cmd)
+
+    subprocess.run(
+        shlex.split(cmd),
+        cwd=working_dir,
+        check=True,
+        stdout=log_file,
+        stderr=subprocess.STDOUT,
+        env=env,
+        capture_output=False,
+    )
+
+
+class TempDirEnv:
+    def __init__(self, change_dir: bool = False) -> None:
+        self.td: tempfile.TemporaryDirectory[str]
+        self.old_dir: Path
+
+        self.chdir = change_dir
+
+    def __enter__(self) -> Path:
+        self.td = tempfile.TemporaryDirectory()
+        tempfile.tempdir = self.td.name
+        tmpdir_path = Path(self.td.name)
+        if self.chdir:
+            self.old_dir = Path(os.getcwd()).absolute()
+            os.chdir(tmpdir_path)
+        return tmpdir_path
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_value: BaseException | None,
+        exc_traceback: TracebackType | None,
+    ) -> None:
+        if self.chdir:
+            os.chdir(self.old_dir)
+        tempfile.tempdir = None
+
+
+def save_to_tmp_file(content: str, suffix: str | None = None) -> IO[bytes]:
+    ntf = tempfile.NamedTemporaryFile(suffix=suffix)
+    with open(ntf.name, "w") as f:
+        f.write(content)
+
+    return ntf
