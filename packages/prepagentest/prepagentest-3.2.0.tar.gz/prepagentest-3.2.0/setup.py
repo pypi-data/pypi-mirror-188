f5231dc0b952b6800547ed8c5d8052355f28982d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:41 +0000

    Add package prepagentest/3.2.0/prepagentest-3.2.0.tar.gz

---
+++ packages/prepagentest/prepagentest-3.2.0.tar.gz/prepagentest-3.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md','r') as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'prepagentest',
+    version='3.2.0',
+    description='some package',
+    py_modules=["ragents"],
+    install_requires = [
+        'pandas',
+        'numpy',
+        'datetime',
+        'tqdm',
+        'helperagent==4.0.0',
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    extras_require={
+        "dev":[
+            "pytest",
+        ],
+    },
+)
