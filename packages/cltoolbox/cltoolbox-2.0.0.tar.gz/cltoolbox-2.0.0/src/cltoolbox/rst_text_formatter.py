5ad73b6f4b2610975da502f0d115b781f519038f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:12 +0000

    Add package cltoolbox/2.0.0/cltoolbox-2.0.0.tar.gz

---
+++ packages/cltoolbox/cltoolbox-2.0.0.tar.gz/cltoolbox-2.0.0/src/cltoolbox/rst_text_formatter.py
@@ -0,0 +1,13 @@
+"""A simple class that wraps argparse help with rst2ansi."""
+import argparse
+import sys
+
+from .rst2ansi.rst2ansi import rst2ansi  # type: ignore
+
+
+class RSTHelpFormatter(argparse.RawTextHelpFormatter):
+    """Custom formatter class that is capable of interpreting ReST."""
+
+    def format_help(self):
+        ret = rst2ansi(bytes(super().format_help() + "\n", "utf-8"))
+        return ret.encode(sys.stdout.encoding, "replace").decode(sys.stdout.encoding)
