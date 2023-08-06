1007575a8ddd2a61b2d392b6f395cd930e0cdecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:52 +0000

    Add package mayatk/0.5.3/mayatk-0.5.3-py3-none-any.whl

---
+++ packages/mayatk/mayatk-0.5.3-py3-none-any.whl/mayatk/__init__.py
@@ -0,0 +1,32 @@
+# !/usr/bin/python
+# coding=utf-8
+from mayatk.core import *
+from mayatk.Mash import *
+from mayatk.Node import Node
+from mayatk.Cmpt import Cmpt
+from mayatk.Edit import Edit
+from mayatk.Macro import Macro
+from mayatk.Script import Script
+from mayatk.Rig import Rig
+from mayatk.Xform import Xform
+
+
+name = 'mayatk'
+__version__ = '0.5.3'
+
+# --------------------------------------------------------------------------------------------
+
+
+
+
+
+
+# print (__package__, __file__)
+# --------------------------------------------------------------------------------------------
+# Notes
+# --------------------------------------------------------------------------------------------
+
+
+# --------------------------------------------------------------------------------------------
+# deprecated:
+# --------------------------------------------------------------------------------------------
