50e531ec9c5a3af629a1c38165fbee43c714fd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:52 +0000

    Add package TDBRecord/1.1.3/TDBRecord-1.1.3-py3-none-any.whl

---
+++ packages/TDBRecord/TDBRecord-1.1.3-py3-none-any.whl/TDBRecord/__main__.py
@@ -0,0 +1,80 @@
+import TDBRecord as tdbra
+
+import pathlib
+import logging
+import click
+import json
+
+__version__ = "1.1.3"
+
+@click.version_option(prog_name="TDBRecord", version=__version__)
+@click.group()
+def main():
+    """
+    TDB Record Application for OpenSourceUsers
+    """
+    pass
+
+@main.command()
+@click.option("--debug", is_flag=True, help="Enable debug mode")
+@click.option("--config", type=click.Path(exists=True, file_okay=True, dir_okay=False, path_type=pathlib.Path), help="Config file", default="config.json")
+@click.option("--logfile", type=click.Path(file_okay=True, dir_okay=False, path_type=pathlib.Path), help="Log file", default="record.log")
+def start(debug, config, logfile):
+    """
+    Start TDBRecord server for multiple users.
+    """
+    tdbra.confPath = config
+    if debug: tdbra.logger.setLevel(logging.DEBUG)
+    tdbra.conf = json.loads(tdbra.confPath.read_text())
+
+    tdbra.logFilePath = logfile
+    tdbra.logger.addHandler(logging.FileHandler(tdbra.logFilePath))
+    tdbra.logger.handlers[1].setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s # %(message)s'))
+    tdbra.logger.handlers[1].setLevel(logging.DEBUG)
+    logging.getLogger("streamlink.plugins.afreeca").addHandler(logging.FileHandler(tdbra.logFilePath))
+    logging.getLogger("streamlink.plugins.afreeca").handlers[1].setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s - afreecatv # %(message)s'))
+    logging.getLogger("streamlink.plugins.afreeca").handlers[1].setLevel(logging.INFO)
+    tdbra.logger.info("TDBRecord started!")
+
+    tdbra.config.check_config()
+    tdbra.command.start()
+
+@main.command()
+@click.option("--debug", is_flag=True, help="Enable debug mode")
+@click.option("--remote-streamlink", help="Remote streamlink Url", default="")
+@click.option("--download-path", type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=pathlib.Path), help="Download folder", default="download/")
+@click.option("--ffmpeg", type=str, help="FFmpeg path", default="ffmpeg")
+@click.argument(
+    "user",
+    type=str,
+    required=True,
+)
+@click.argument(
+    "platform",
+    type=str,
+    required=True,
+)
+def record(debug, user, platform, remote_streamlink, download_path, ffmpeg):
+    """
+    Standalone record command. Only record one user.
+    """
+
+    if debug: tdbra.loglevel = logging.DEBUG
+    if remote_streamlink: tdbra.conf["remote_streamlink"] = remote_streamlink
+    if ffmpeg: tdbra.conf["ffmpeg"] = ffmpeg
+    else: tdbra.conf["ffmpeg"] = "ffmpeg"
+
+    if download_path: tdbra.downloadPath = download_path
+    tdbra.command.record(user, platform)
+
+@main.command()
+def config():
+    """
+    Generate config file
+    """
+    config = pathlib.Path("config.json")
+    if config.exists():
+        print("Config file already exists")
+        return
+    config.write_text(json.dumps(tdbra.config.DEFAULT_CONFIG, indent=4))
+    tdbra.logger.info("Config file generated")
---
+++ packages/TDBRecord/TDBRecord-1.1.3-py3-none-any.whl/TDBRecord/m3u8/__init__.py
@@ -0,0 +1,103 @@
+import TDBRecord as tdbra
+
+from subprocess import Popen, PIPE, DEVNULL
+from streamlink import Streamlink
+from requests import get
+from pathlib import Path
+import time
+
+session = Streamlink()
+
+urlstore = {
+    "twitch": "https://twitch.tv/{user}",
+    "afreecatv": "https://play.afreecatv.com/{user}",
+}
+
+def worker(user: str, platform: str) -> None:
+    """
+    Standalone video downloader
+    """
+    logger = tdbra.data[f"{user}.{platform}"]["logger"]
+    logger.setLevel(tdbra.logger.level)
+    downloadPath = tdbra.downloadPath / f"{user}.{platform}/"
+    downloadPath.mkdir(parents=True, exist_ok=True)
+    filename = f"{platform}.{user}.{time.strftime('%y%m%d.%H%M%S')}"
+
+    downloadFullPath = downloadPath / f"{filename}.mp4"
+    m3uPlaylistUri = stream(user, platform)
+    logger.info("FFMPEG Downloader started.")
+
+    # Downloader
+    while True:
+        if tdbra.data[f"{user}.{platform}"]["exit"]:
+            logger.info("FFMPEG Downloader stopped.")
+            del(tdbra.data[f"{user}.{platform}"])
+            return
+
+        m3ud = get(m3uPlaylistUri)
+        if "twitch-ad-quartile" in m3ud.text:
+            logger.debug("Waiting for AD to start...")
+            time.sleep(2)
+            continue
+        break
+
+    logger.info("AD finished. Start recording...")
+    logger.debug(f"{tdbra.conf['ffmpeg']} -y -hide_banner -loglevel error -i {m3uPlaylistUri} -c copy {downloadFullPath}")
+    tdbra.downloadPath.mkdir(parents=True, exist_ok=True)
+    command = [
+        tdbra.conf["ffmpeg"],
+        "-y",
+        "-hide_banner",
+        "-loglevel",
+        "error",
+        "-i",
+        m3uPlaylistUri,
+        "-c",
+        "copy",
+        downloadFullPath
+    ]
+    if tdbra.conf["proxy"]:
+        command.extend(["-http_proxy", tdbra.conf["proxy"]])
+    ffmpeg = Popen(command, stdin=PIPE)
+    while True:
+        if tdbra.data[f"{user}.{platform}"]["exit"]:
+            ffmpeg.communicate(input=b"q")
+            for i in range(6):
+                if ffmpeg.poll() is not None:
+                    logger.info(f"FFMPEG Downloader finished with status {ffmpeg.poll()}.")
+                    if downloadFullPath.exists(): downloadFullPath.unlink()
+                    del(tdbra.data[f"{user}.{platform}"])
+                    return
+                time.sleep(1)
+            ffmpeg.kill()
+            logger.warning("FFMPEG Downloader killed.")
+            if downloadFullPath.exists(): downloadFullPath.unlink()
+            del(tdbra.data[f"{user}.{platform}"])
+            return
+        if ffmpeg.poll() is not None:
+            logger.info(f"FFMPEG Downloader finished with status {ffmpeg.poll()}.")
+            tdbra.data[f"{user}.{platform}"]["end"] = True
+            break
+        time.sleep(1)
+    if tdbra.gc:
+        tdbra.autocheck(tdbra.ls, user, platform, downloadFullPath, logger)
+
+    del(tdbra.data[f"{user}.{platform}"])
+
+def stream(user: str, platform: str) -> str:
+    if tdbra.conf["remote_streamlink"] and platform == "twitch":
+        return get(f"{tdbra.conf['remote_streamlink']}/{user}?quality=best").text
+    else:
+        return session.streams(urlstore[platform].format(user=user))["best"].url
+
+def checkStatus(user: str, platform: str) -> bool:
+    if tdbra.conf["proxy"]:
+        session.set_option("http-proxy", tdbra.conf["proxy"])
+    try:
+        if session.streams(urlstore[platform].format(user=user)):
+            return True
+        else:
+            return False
+    except:
+        tdbra.logger.error(f"Failed to check status of {user}.{platform}")
+        return False
