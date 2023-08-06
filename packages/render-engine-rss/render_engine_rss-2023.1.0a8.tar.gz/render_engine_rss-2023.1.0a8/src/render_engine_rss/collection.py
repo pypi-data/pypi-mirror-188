bec36e240ea895921afc2f7a90d6c30200e26b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:31 +0000

    Add package render-engine-rss/2023.1.0a8/render_engine_rss-2023.1.0a8.tar.gz

---
+++ packages/render-engine-rss/render_engine_rss-2023.1.0a8.tar.gz/render_engine_rss-2023.1.0a8/src/render_engine_rss/collection.py
@@ -0,0 +1,22 @@
+from typing import Type
+from .parsers import RSSFeedPageParser
+import feedparser
+from render_engine.collection import Collection
+
+class RSSCollection(Collection):
+    PageParser = RSSFeedPageParser
+    content_path = str
+    sort_by = "published_parsed"
+    sort_reverse = True
+
+    def __init__(self):
+        self.content = feedparser.parse(self.content_path)
+        super().__init__()
+
+    @property
+    def pages(self):
+        """Entries for this would be """
+
+        for entry in self.content['entries']:
+            yield self.gen_page(content=entry)
+            
