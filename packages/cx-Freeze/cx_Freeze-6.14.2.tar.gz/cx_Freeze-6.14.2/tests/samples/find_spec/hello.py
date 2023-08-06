19da463f6849c92fa30a7795246a816536006798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:22 +0000

    Add package cx-Freeze/6.14.2/cx_Freeze-6.14.2.tar.gz

---
+++ packages/cx-Freeze/cx_Freeze-6.14.2.tar.gz/cx_Freeze-6.14.2/tests/samples/find_spec/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+from __future__ import annotations
+
+import dummypackage.dummymodule  # noqa
+
+print("Hi, I'm a program.")
+
+raise Exception("This exception is fine.")
