6b5ba1d03ccd94f21109c86b6f5c9a12128ced19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:15 +0000

    Add package pyytdata/0.3.0/pyytdata-0.3.0.tar.gz

---
+++ packages/pyytdata/pyytdata-0.3.0.tar.gz/pyytdata-0.3.0/pyytdata/util/__init__.py
@@ -0,0 +1,3 @@
+from .fetcher import ChannelDataFetcher, VideoDataFetcher
+from .info import ChannelInfo, ChannelStatInfo, VideoInfo, VideoStatInfo
+from .ytdata import YtChannelInfo, YtChannelStat, YtVideoInfo, YtVideoStat
