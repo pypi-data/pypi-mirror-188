7e7ecec5019406aaf2cf2f29a7b8b10d12bac52c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:22 +0000

    Add package wafermap-plot/0.1.0/wafermap-plot-0.1.0.tar.gz

---
+++ packages/wafermap-plot/wafermap-plot-0.1.0.tar.gz/wafermap-plot-0.1.0/wafermap_plot/wafermap_plot.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class WaferMapPlot():
+
+    def __init__(self):
+        super()
+
+    def plot(self, defect_points: List[Tuple[float, float]]) -> plt:
+
+        x = np.linspace(-100, 100, 100)
+        y = np.linspace(-100, 100, 100)
+        
+        X, Y = np.meshgrid(x, y)
+
+        F = X**2 + Y**2 - 100 * 100
+
+        fig, ax = plt.subplots()
+
+        ax.contour(X, Y, F, [0], colors="black")
+        ax.set_aspect(1)
+
+        ax.scatter(*zip(*defect_points), s=1, c="black", marker="s")
+
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+
+        plt.xlim(-110, 110)
+        plt.ylim(-110, 110)
+        
+        return plt
