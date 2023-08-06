fcd7417642aa64498b0ebdd3261675fbc97780f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:42 +0000

    Add package angr-management/9.2.36/angr-management-9.2.36.tar.gz

---
+++ packages/angr-management/angr-management-9.2.36.tar.gz/angr-management-9.2.36/angrmanagement/ui/dialogs/__init__.py
@@ -0,0 +1,6 @@
+from .analysis_options import AnalysisOptionsDialog
+from .load_binary import LoadBinary
+from .load_plugins import LoadPlugins
+from .load_docker_prompt import LoadDockerPrompt
+from .preferences import Preferences
+from .breakpoint import BreakpointDialog
---
+++ packages/angr-management/angr-management-9.2.36.tar.gz/angr-management-9.2.36/angrmanagement/ui/documents/__init__.py
@@ -0,0 +1,1 @@
+from .qcodedocument import QCodeDocument
