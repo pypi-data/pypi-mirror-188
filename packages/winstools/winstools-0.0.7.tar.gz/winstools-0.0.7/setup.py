9f3d825b6ad036b1aba0da2484de56a4cc00c26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:54 +0000

    Add package winstools/0.0.7/winstools-0.0.7.tar.gz

---
+++ packages/winstools/winstools-0.0.7.tar.gz/winstools-0.0.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name='winstools',
+	version='0.0.7',
+	packages=['winstools'],
+	package_dir={'winstools': '#py'},
+	url='',
+	license='',
+	author='Winstang',
+	author_email='',
+	description='HoHo'
+)
