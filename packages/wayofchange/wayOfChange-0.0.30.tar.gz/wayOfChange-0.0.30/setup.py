9096d5df603649c02ac14a09b3338b75c7455f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:47 +0000

    Add package wayofchange/0.0.30/wayOfChange-0.0.30.tar.gz

---
+++ packages/wayofchange/wayOfChange-0.0.30.tar.gz/wayOfChange-0.0.30/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='wayOfChange',
+      version='0.0.30',
+      description='Put description here',
+      author='Edrihan Levesque',
+      author_email='Edrihan@gmail.com',
+      url='https://github.com/wayofchange',
+      packages=find_packages(where=['/src'],exclude=['test']),
+      py_modules = ['wayOfChange'],
+
+      #install_requires=['numpy',],#Put this in pyproject.toml
+      #data_files={
+      #  "wayOfChange": ["*.ttf"],
+      #  "wayOfChange": ["fonts/*.ttf"],
+      #              },'''
+      package_data={'wayOfChange': ['*.ttf']},
+     )
