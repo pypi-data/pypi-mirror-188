8b32fb7bb2385eefb8a200121e6b5949d42cfcdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:46 +0000

    Add package pie4t/0.1.0/pie4t-0.1.0.tar.gz

---
+++ packages/pie4t/pie4t-0.1.0.tar.gz/pie4t-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+# with open("README.md", "r",encoding="utf8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="pie4t",
+    version="0.1.0",
+    author="Wen-Hung, Chang 張文宏",
+    author_email="beardad1975@nmes.tyc.edu.tw",
+    description="Physics Impulse Engine wrapper library for Teenagers",
+    long_description="Physics Impulse Engine wrapper library for Teenagers",
+    long_description_content_type="text/markdown",
+    url="https://github.com/beardad1975/pie4t",
+    #packages=setuptools.find_packages(),
+    platforms=["Windows"],
+    python_requires=">=3.8",
+    packages=['pie4t','物理模組'],
+    install_requires = ['arcade~=2.6.0', 'pymunk~=6.4.0'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: Microsoft :: Windows",
+            #"Operating System :: MacOS",
+            #"Operating System :: POSIX :: Linux",
+            "Natural Language :: Chinese (Traditional)",
+        ],
+)
