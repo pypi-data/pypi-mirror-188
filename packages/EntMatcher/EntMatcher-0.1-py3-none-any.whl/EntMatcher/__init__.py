16b0838a642bdaf66fe5e597b8ec90246ea44dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:39 +0000

    Add package EntMatcher/0.1/EntMatcher-0.1-py3-none-any.whl

---
+++ packages/EntMatcher/EntMatcher-0.1-py3-none-any.whl/EntMatcher/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from .dinf import DInf
+from .base import BaseMatcher
+from .csls import CSLS
+from .rinf import RInf
+from .sinkhorn import Sinkhorn
+from .hungarian import Hun
+from .smat import SMat
+from .rl import RL
+
+name = "EntMatcher"
