6d8bc4ad37741eed555e6f3a39d4df26de4d0262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:44 +0000

    Add package spatbox/0.1.1/spatbox-0.1.1-py2.py3-none-any.whl

---
+++ packages/spatbox/spatbox-0.1.1-py2.py3-none-any.whl/spatbox/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1.1",
+__author__ = "SpatLyu"
+
+
+import spatbox.plot
+import spatbox.geocode
+import spatbox.corrdinate
+import spatbox.datasets 
+import spatbox.downscale 
+import spatbox.raster
+import spatbox.vector
+
