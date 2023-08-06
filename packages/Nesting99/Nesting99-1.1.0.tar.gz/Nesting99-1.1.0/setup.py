bdc97d459d1ba30649b477c7dc4f351ae771f789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:49 +0000

    Add package Nesting99/1.1.0/Nesting99-1.1.0.tar.gz

---
+++ packages/Nesting99/Nesting99-1.1.0.tar.gz/Nesting99-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'Nesting99',
+    version = '1.1.0',
+    py_modules = ['nesting99'],
+    author = 'Adrian',
+    author_email = 'shad.w0w@outlook.com',
+    url = 'https://github.com/shadw0w23',
+    description = 'Simple printer of nested lists',
+)
