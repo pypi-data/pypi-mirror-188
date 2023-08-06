1133327836327d1118e291e420350df9b64d9f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:01 +0000

    Add package moreezpython/0.0.2/moreezpython-0.0.2.tar.gz

---
+++ packages/moreezpython/moreezpython-0.0.2.tar.gz/moreezpython-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="moreezpython",
+    version="0.0.2",
+    packages=["file"],  # permet de récupérer tout les fichiers
+    description="Python More easy",
+    url="https://github.com/Maelus-999?tab=repositories",
+    author="maelus999",
+    license="MIT",
+    python_requires=">=2.7"
+)
