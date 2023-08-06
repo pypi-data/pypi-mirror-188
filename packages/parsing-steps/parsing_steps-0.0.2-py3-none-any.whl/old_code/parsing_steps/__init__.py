0802265422622b5ff2058c0a4dbe7d36062f3495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:36 +0000

    Add package parsing-steps/0.0.2/parsing_steps-0.0.2-py3-none-any.whl

---
+++ packages/parsing-steps/parsing_steps-0.0.2-py3-none-any.whl/old_code/parsing_steps/__init__.py
@@ -0,0 +1,4 @@
+__title__ = "Parsing steps"
+__version__ = "0.0.2"
+__author__ = "Viacheslav Loievskyi"
+__license__ = "Apache Software License"
---
+++ packages/parsing-steps/parsing_steps-0.0.2-py3-none-any.whl/parsing_steps/__init__.py
@@ -0,0 +1,7 @@
+__title__ = "Parsing steps"
+__version__ = "0.0.2"
+__author__ = "Viacheslav Loievskyi"
+__license__ = "Apache Software License"
+
+
+from .base import BaseParsingStep
