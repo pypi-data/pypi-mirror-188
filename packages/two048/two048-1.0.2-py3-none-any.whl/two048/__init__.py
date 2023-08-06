ddf9af8a80c0dc65f0a568ab4eba24f68b17366b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:00 +0000

    Add package two048/1.0.2/two048-1.0.2-py3-none-any.whl

---
+++ packages/two048/two048-1.0.2-py3-none-any.whl/two048/__init__.py
@@ -0,0 +1,11 @@
+__title__ = "two048"
+__author__ = "airo.pi_"
+__license__ = "MIT"
+__version__ = "1.0.2"
+
+from .main import Direction as Direction
+from .main import Movement as Movement
+from .main import Tile as Tile
+from .main import Two048 as Two048
+
+__all__ = ["Direction", "Movement", "Two048", "Tile"]
