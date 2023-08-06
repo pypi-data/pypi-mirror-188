9d3b868b85d15bf3f8874ac211188a5da9a1831c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:11 +0000

    Add package eanoui/1.1.23/eanoui-1.1.23.tar.gz

---
+++ packages/eanoui/eanoui-1.1.23.tar.gz/eanoui-1.1.23/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from setuptools import setup
+import eanoui
+
+if sys.version_info < (3, 0):
+
+    long_description = "\n".join([
+        open('README.rst', 'r').read(),
+    ])
+else:
+    long_description = "\n".join([
+        open('README.rst', 'r', encoding='utf-8').read(),
+    ])
+
+setup(
+    name='django_eanoui',
+    version=eanoui.get_version(),
+    packages=['eanoui'],
+    zip_safe=False,
+    include_package_data=True,
+    url='https://www.eano.com',
+    license='Apache License 2.0',
+    author='Hongbin',
+    long_description=long_description,
+    author_email='hongbin@eano.com',
+    description='django admin theme 后台模板 based on simpleui',
+    install_requires=['django'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
