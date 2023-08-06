b632eb5781ed3cc74714d7a57dc1b35b6c171507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:42 +0000

    Add package AiBot.py/1.2.0/AiBot.py-1.2.0.tar.gz

---
+++ packages/AiBot.py/AiBot.py-1.2.0.tar.gz/AiBot.py-1.2.0/test/test_web.py
@@ -0,0 +1,22 @@
+import time
+
+from AiBot import WebBotMain
+
+
+class CustomWebScript(WebBotMain):
+    log_level = "DEBUG"
+
+    def script_main(self):
+        self.goto("https://www.qq.com")
+        time.sleep(2)
+        self.new_page("https://www.baidu.com")
+        time.sleep(2)
+        self.set_element_value("//*[@id='kw']", "RPA_办公自动化—Aibote")
+        time.sleep(2)
+        result = self.execute_script('(()=>"aibote rpa")()')
+        print(result)  # aibote rpa
+
+
+if __name__ == '__main__':
+    # 监听 6666 号端口
+    CustomWebScript.execute(9999)
