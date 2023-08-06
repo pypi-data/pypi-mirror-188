836a9af42101af7d0f7709ba3b3b7a27e37950e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:46 +0000

    Add package streamlit-on-machine-data/0.0.3/streamlit-on-machine-data-0.0.3.tar.gz

---
+++ packages/streamlit-on-machine-data/streamlit-on-machine-data-0.0.3.tar.gz/streamlit-on-machine-data-0.0.3/st_on_machine_data/__init__.py
@@ -0,0 +1,52 @@
+import os
+import streamlit as st
+import streamlit.components.v1 as components
+import base64
+
+WORD_CLOUD_IMAGE = ""
+with open("wordcloud.jpg", "rb") as img_file:
+    WORD_CLOUD_IMAGE = base64.b64encode(img_file.read())
+#print(WORD_CLOUD_IMAGE)
+
+_RELEASE = True
+
+if _RELEASE:
+
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(root_dir, "frontend/build")
+
+    _on_hover_tabs = components.declare_component(
+        "st_on_machine_data",
+        path = build_dir
+    )
+
+else:
+    _on_hover_tabs = components.declare_component(
+    "on_machine_data",
+    url="http://localhost:3001"
+    )
+
+def on_machine_data(menu, group_names, text_groups,summaries,topics,generated_number_groups, word_cloud_image, default_choice=1, key=None):
+    
+    component_value = _on_hover_tabs(menu=menu, group_names=group_names, text_groups=text_groups,summaries=summaries,topics=topics,generated_number_groups=generated_number_groups, word_cloud_image=word_cloud_image, key=key)
+    
+    return component_value
+
+if not _RELEASE:
+    st.markdown('<style>' + open('./style.css').read() + '</style>', unsafe_allow_html=True) # Load the on hover side bar css file
+    tabs = on_machine_data(menu=["World","Hello","Test"], 
+                              group_names="Group Name", 
+                              text_groups=['list 1','list 2','list 3','list 4'],
+                              summaries="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)",
+                              topics='dffdsfsfsfsfsd',
+                              generated_number_groups=10,
+                              word_cloud_image=WORD_CLOUD_IMAGE,
+                              key="1") ## create tabs for on hover navigation bar
+    
+    print(tabs)
+
+
+
+
+
+        
---
+++ packages/streamlit-on-machine-data/streamlit-on-machine-data-0.0.3.tar.gz/streamlit-on-machine-data-0.0.3/st_on_machine_data/frontend/build/precache-manifest.2ac25ed9b17c58f60e78635bb81b966c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569d9f9cf7e14efd703f43268d7cbf8e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d32a79ff59aeaa70816",
+    "url": "./static/css/main.1bc19a94.chunk.css"
+  },
+  {
+    "revision": "1d74644774110ea971a3",
+    "url": "./static/js/2.487a829e.chunk.js"
+  },
+  {
+    "revision": "abb38f03b4873ee4a8daa94530086add",
+    "url": "./static/js/2.487a829e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d32a79ff59aeaa70816",
+    "url": "./static/js/main.16d89a83.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
