03c3a54d64594501fb02d19ce64ebf8098bafee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:07 +0000

    Add package riscemu/2.0.5/riscemu-2.0.5-py3-none-any.whl

---
+++ packages/riscemu/riscemu-2.0.5-py3-none-any.whl/riscemu/__init__.py
@@ -0,0 +1,28 @@
+"""
+RiscEmu (c) 2021 Anton Lydike
+
+SPDX-License-Identifier: MIT
+
+This package aims at providing an all-round usable RISC-V emulator and debugger
+
+It contains everything needed to run assembly files, so you don't need any custom compilers or toolchains
+"""
+
+from .types.exceptions import RiscemuBaseException, LaunchDebuggerException, InvalidSyscallException, LinkerException, \
+    ParseException, NumberFormatException, InvalidRegisterException, MemoryAccessException, OutOfMemoryException
+
+from .instructions import *
+
+from .MMU import MMU
+from .registers import Registers
+from .syscall import SyscallInterface, Syscall
+from .CPU import CPU, UserModeCPU
+from .debug import launch_debug_session
+
+from .config import RunConfig
+
+from .parser import tokenize, parse_tokens, AssemblyFileLoader
+
+__author__ = "Anton Lydike <Anton@Lydike.com>"
+__copyright__ = "Copyright 2022 Anton Lydike"
+__version__ = '2.0.5'
