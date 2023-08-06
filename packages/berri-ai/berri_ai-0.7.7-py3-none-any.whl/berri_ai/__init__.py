6eb17c43193f9bde52777ff5285c9acac68db88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:12 +0000

    Add package berri-ai/0.7.7/berri_ai-0.7.7-py3-none-any.whl

---
+++ packages/berri-ai/berri_ai-0.7.7-py3-none-any.whl/berri_ai/__init__.py
@@ -0,0 +1,49 @@
+__version__ = '0.7.7'
+from google.colab import _message
+import ast 
+import re
+import os 
+import traceback
+import time
+import pipreqs
+import subprocess
+
+def get_dependencies(path):
+    subprocess.run(["pipreqs", path])
+
+def deploy():
+  print("Begun deployment.. ")
+
+  print("Converting notebook to python and generating requirements.txt 🐍")
+
+  # Obtain the notebook JSON as a string
+  notebook_json_string = _message.blocking_request('get_ipynb', request='', timeout_sec=5)
+
+  # save to temporary file
+  lines = []
+  for cell in notebook_json_string["ipynb"]["cells"]:
+    for line in cell["source"]:
+      if line.startswith("!pip install"):
+        continue
+      elif not line.startswith("!"):
+        lines.append(line)
+  
+  f = open("agent_code.py", "w")
+  for line in lines:
+    if "initialize_agent(" in line:
+      initialization_line = line.split("=", 1)
+      initialization_line = "agent = " + initialization_line[1]
+      print(initialization_line)
+      f.write(initialization_line + "\n")
+    else:
+      f.write(line + "\n")
+  f.close()
+  
+  get_dependencies("./")    
+  print("Building docker image 😱")
+
+  print("Deploying endpoint ... ⌛️")
+  
+  # assume you're in a google colab 
+  print("hello world")
+  
