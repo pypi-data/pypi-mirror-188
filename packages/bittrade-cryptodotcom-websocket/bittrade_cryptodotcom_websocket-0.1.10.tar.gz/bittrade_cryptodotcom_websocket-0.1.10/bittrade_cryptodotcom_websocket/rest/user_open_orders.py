cfd0f7db8163fc2164ce8e64a6735d6f9fc2903a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:52 +0000

    Add package bittrade-cryptodotcom-websocket/0.1.10/bittrade_cryptodotcom_websocket-0.1.10.tar.gz

---
+++ packages/bittrade-cryptodotcom-websocket/bittrade_cryptodotcom_websocket-0.1.10.tar.gz/bittrade_cryptodotcom_websocket-0.1.10/bittrade_cryptodotcom_websocket/rest/user_open_orders.py
@@ -0,0 +1,42 @@
+from typing import Optional, cast
+from reactivex import Observable, operators
+from ccxt import cryptocom
+
+from ..connection import wait_for_response
+from ..events import MethodName
+from ..models import (
+    CryptodotcomRequestMessage,
+    CryptodotcomResponseMessage,
+    EnhancedWebsocketBehaviorSubject,
+    OrderDict,
+)
+from ..operators.stream.response_messages import extract_data
+
+
+def to_open_orders_entries(exchange: cryptocom):
+    def to_open_orders_entries_(message: CryptodotcomResponseMessage):
+        return cast(list[dict], exchange.parse_orders(message.result["data"]))
+
+    return to_open_orders_entries_
+
+
+def get_user_open_orders_factory(
+    messages: Observable[CryptodotcomResponseMessage],
+    socket: EnhancedWebsocketBehaviorSubject,
+):
+    def get_user_open_orders(
+        instrument: Optional[str] = "",
+    ) -> Observable[list[OrderDict]]:
+        params = {"instrument_name": instrument} if instrument else {}
+        return messages.pipe(
+            wait_for_response(
+                socket.value.send_message(
+                    CryptodotcomRequestMessage(
+                        MethodName.GET_OPEN_ORDERS, params=params
+                    )
+                )
+            ),
+            extract_data(),
+        )
+
+    return get_user_open_orders
