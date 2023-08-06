b35d4ed3e4adaaa41bdff8771cd02da99b054add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:05 +0000

    Add package PyExpUtils-andnp/3.3.2/PyExpUtils_andnp-3.3.2-py3-none-any.whl

---
+++ packages/PyExpUtils-andnp/PyExpUtils_andnp-3.3.2-py3-none-any.whl/PyExpUtils/results/tools.py
@@ -0,0 +1,31 @@
+from typing import Any, Dict
+import numpy as np
+import pandas as pd
+
+def collapseRuns(df: pd.DataFrame):
+    cols = list(df.columns)
+    header = list(filter(lambda c: c not in ['data', 'run'], cols))
+
+    df = df.groupby(header)['data'].apply(lambda x: np.array(list(x))).reset_index()
+    return df
+
+def subsetDF(df: pd.DataFrame, conds: Dict[str, Any]):
+    mask = _buildMask(df, conds)
+    return df[mask].reset_index(drop=True)
+
+# ------------------------
+# -- Internal Utilities --
+# ------------------------
+def _buildMask(df: pd.DataFrame, conds: Dict[str, Any]):
+    mask = np.ones(len(df), dtype=bool)
+    for key, cond in conds.items():
+        if isinstance(cond, dict):
+            mask = mask | _buildMask(df, cond)
+
+        elif isinstance(cond, list):
+            mask = mask & (df[key].isin(cond))
+
+        elif key in df:
+            mask = mask & (df[key] == cond)
+
+    return mask
