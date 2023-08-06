47ca01306acdc03ae891d50c7b29cbdd30eed82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:42 +0000

    Add package djangoldp-needle/0.1.14/djangoldp_needle-0.1.14.tar.gz

---
+++ packages/djangoldp-needle/djangoldp_needle-0.1.14.tar.gz/djangoldp_needle-0.1.14/djangoldp_needle/djangoldp_urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.urls import path
+from .models import Annotation, Tag, AnnotationTarget, NeedleActivity, AnnotationIntersectionRead
+from .views import AnnotationViewset, AnnotationTargetViewset, TagViewset, AnnotationTargetIntersectionViewset, \
+    AnnotationIntersectionsAfterViewset, AnnotationIntersectionsBeforeViewset, AnnotationIntersectionReadViewset
+
+urlpatterns = [
+    url(r'^annotationtargets/', AnnotationTargetViewset.urls(model_prefix="annotationtarget", model=AnnotationTarget)),
+    url(r'^annotationintersectionreads/', AnnotationIntersectionReadViewset.urls(model_prefix="annotationintersectionread", model=AnnotationIntersectionRead)),
+    path('annotationtargetsintersection/<url>/<date>', AnnotationTargetIntersectionViewset.as_view({'get': 'list'}, model=AnnotationTarget)),
+    path('annotationintersections/before/<url>/<date>',
+        AnnotationIntersectionsBeforeViewset.as_view({'get': 'list'}, model=Annotation)),
+    path('annotationintersections/after/<url>/<date>',
+        AnnotationIntersectionsAfterViewset.as_view({'get': 'list'}, model=Annotation)),
+    url(r'^users/(?P<slug>[\w\-\.]+)/yarn/', AnnotationViewset.urls(model_prefix="yarn", model=Annotation)),
+    url(r'^users/(?P<slug>[\w\-\.]+)/tags', TagViewset.urls(model_prefix="tags", model=Tag)),
+]
