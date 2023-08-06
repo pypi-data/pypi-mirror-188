5132f4638e97ca600c243b08f98d2ed24087b2de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:06 +0000

    Add package reverse-diagrams/0.2.3/reverse_diagrams-0.2.3-py3-none-any.whl

---
+++ packages/reverse-diagrams/reverse_diagrams-0.2.3-py3-none-any.whl/graph_sso.py
@@ -0,0 +1,30 @@
+
+    with Cluster('Groups'):
+
+        with Cluster("AWSControlTowerAdmins"):
+
+                gg_0= [User("velez94@protonma\nil.com"),]
+
+        with Cluster("AWSAccountFactory"):
+
+                gg_1= [User("velez94@protonma\nil.com"),]
+
+        gg_2= Users("AWSLogArchiveVie\nwers")
+
+        gg_3= Users("AWSLogArchiveAdm\nins")
+
+        gg_4= Users("AWSAuditAccountA\ndmins")
+
+        gg_5= Users("AWSSecurityAudit\nPowerUsers")
+
+        gg_6= Users("AWSSecurityAudit\nors")
+
+        gg_7= Users("AWSServiceCatalo\ngAdmins")
+
+        with Cluster("DevSecOps_Admins"):
+
+                gg_8= [User("DevSecOpsAdm"),]
+
+        with Cluster("SecOps_Adms"):
+
+                gg_9= [User("w.alejovl+secops\n-labs@gmail.com"),]
