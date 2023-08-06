5698fb23b84ac48c0445649f9118e6f546f19db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:15 +0000

    Add package scora-ldap-extractor/1.2.0/scora-ldap-extractor-1.2.0.tar.gz

---
+++ packages/scora-ldap-extractor/scora-ldap-extractor-1.2.0.tar.gz/scora-ldap-extractor-1.2.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from pkgver import package_version
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+install_reqs = [
+    "ldap3>=2.9.1",
+    "boto3>=1.26.57",
+    "python-dotenv>=0.21.1",
+    "environs>=9.5.0",
+    "numpy>=1.24.1",
+]
+
+setup(
+    name="scora-ldap-extractor",
+    version=package_version,
+    description="Scora Ldap Extractor, see docs for more details.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=["*.env"]),
+    package_dir={"scora_ldap_extractor": "src"},
+    install_requires=install_reqs,
+    python_requires=">=3.8",
+    author="Oncase",
+    author_email="contato@oncase.com.br",
+    maintainer="Guilherme Morone",
+    maintainer_email="guilherme.morone@oncase.com.br",
+    url="https://github.com/oncase/scora-ldap-extractor",
+    license="MIT",
+    keywords="scora",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
