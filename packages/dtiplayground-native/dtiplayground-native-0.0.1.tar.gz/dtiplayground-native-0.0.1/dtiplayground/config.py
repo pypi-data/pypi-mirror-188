1476106ba75c419c59f95c625464d7ae0094f17a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:20 +0000

    Add package dtiplayground-native/0.0.1/dtiplayground-native-0.0.1.tar.gz

---
+++ packages/dtiplayground-native/dtiplayground-native-0.0.1.tar.gz/dtiplayground-native-0.0.1/dtiplayground/config.py
@@ -0,0 +1,24 @@
+
+INFO = {
+  "dtiplayground": { 
+    "version" : "0.5.5b4"
+  },
+  "dmriplayground": {
+    "version" : "0.2.1b1"
+  },
+  "dmriprep": {
+    "version" : "0.5.3b1"
+  },
+  "dmriatlas": {
+    "version" : "0.1.2b2"
+  },
+  "dmriautotract" : {
+    "version" : "0.0.2b1"
+  },
+  "dmrifiberprofile" : {
+    "version" : "0.0.2b1"
+  },
+  "dtiplayground-native" : {
+    "version" : "0.0.1"
+  },  
+}
