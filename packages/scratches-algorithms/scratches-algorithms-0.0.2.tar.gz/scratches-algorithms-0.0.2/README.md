e17d1e5c15a0d6bc8fff549a9f00f8629e8405e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:44 +0000

    Add package scratches-algorithms/0.0.2/scratches-algorithms-0.0.2.tar.gz

---
+++ packages/scratches-algorithms/scratches-algorithms-0.0.2.tar.gz/scratches-algorithms-0.0.2/README.md
@@ -0,0 +1,56 @@
+###### scratches-algorithms
+
+![PyPI Version](https://img.shields.io/pypi/v/scratches-algorithms?color=success) ![Python Version](https://img.shields.io/pypi/pyversions/scratches-algorithms?color=informational) ![License](https://img.shields.io/pypi/l/scratches-algorithms?color=red) 
+
+
+[![GitHub Stars](https://img.shields.io/github/stars/notjik/scratches-algorithms?style=social)](https://github.com/notjik/scratches-algorithms/stargazers) \
+[![GitHub Forks](https://img.shields.io/github/forks/notjik/scratches-algorithms?style=social)](https://github.com/notjik/scratches-algorithms/network/members) \
+[![GitHub Issues](https://img.shields.io/github/issues/notjik/scratches-algorithms?style=social)](https://github.com/notjik/scratches-algorithms/issues) \
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/notjik/scratches-algorithms?style=social)](https://github.com/notjik/scratches-algorithms/pulls)
+
+<details><summary> 
+<strong>English</strong>
+</summary>
+
+<div style="margin-bottom: 50px;">
+
+**Scratches Algorithms** – this package is a Python client library with classic sorting and search algorithms and simple utilities for working with numbers, number systems and code performance control.
+
+You can find the source code on **[GitHub](https://github.com/notjik/scratches-algorithms)**.
+
+### Installation
+
+Install the current version from [PyPI](https://pypi.org/project/scratches-algorithms/):
+
+```shell
+pip install scratches-algorithms
+```
+
+
+
+
+</div>
+
+</details>
+
+<details><summary> 
+<strong>Русский</strong>
+</summary>
+
+<div style="margin-bottom: 50px;">
+
+**Scratch Algorithms** – это пакет, который представляет собой клиентскую библиотеку Python с классическими алгоритмами сортировки и поиска и простыми утилитами для работы с числами, системами счисления и контроля производительности кода.
+
+Вы можете найти исходный код на **[Github](https://github.com/notjik/scratches-algorithms)**.
+
+### Установка
+
+Установите текущую версию с [PyPI](https://pypi.org/project/scratches-algorithms/):
+
+```shell
+pip устанавливает scratch-алгоритмы
+```
+
+</div>
+
+</details>
---
+++ packages/scratches-algorithms/scratches-algorithms-0.0.2.tar.gz/scratches-algorithms-0.0.2/src/scratches_algorithms/utils.py
@@ -0,0 +1,77 @@
+"""
+author: notjik
+license: MIT License
+
+copyright: (C) 2023 notjik
+"""
+from time import perf_counter
+
+__all__ = ['NumbersProperties',
+           'NumeralSystem',
+           'PerformanceTracking']
+
+
+class NumbersProperties:
+    @staticmethod
+    # TODO: Search for all divisors
+    # TODO: Поиск всех делителей
+    def divisors(number: int, nontrivial: bool = False) -> list:
+        result = []
+        i = 1 if not nontrivial else 2
+        while i ** 2 <= number:
+            if i ** 2 == number:
+                result.append(i)
+            elif number % i == 0:
+                result += [i, number // i]
+            i += 1
+        return result
+
+    @staticmethod
+    # TODO: Finding the n-th term of the Fibonacci number
+    # TODO: Нахождение n-го члена числа Фибоначчи
+    def fibonacci(number: int) -> int:
+        return round((((1 + 5 ** 0.5) / 2) ** number - ((1 - 5 ** 0.5) / 2) ** number) / 5 ** 0.5)
+
+    @staticmethod
+    # TODO: Checking a number for simplicity
+    # TODO: Проверка числа на простоту
+    def is_prime(number: int) -> bool:
+        if number < 2:
+            return False
+        i = 2
+        while i * i <= number:
+            if number % i == 0:
+                return False
+            i += 1
+        return True
+
+    @staticmethod
+    # TODO: Checking the number by square
+    # TODO: Проверка числа на квадрат
+    def is_square(n: int) -> bool:
+        return (n ** 0.5) % 1 == 0
+
+
+class NumeralSystem:
+    @staticmethod
+    # TODO: Conversion from decimal to any other number system (up to 36)
+    # TODO: Перевод из десятичной системы счисления в любую другую (до 36)
+    def to_base(number: int, base: int) -> str:
+        alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+        result = alphabet[number % base]
+        while number >= base:
+            number //= base
+            result += alphabet[number % base]
+        return result[::-1]
+
+
+class PerformanceTracking:
+    # TODO: Initialization of the class, the beginning of the countdown
+    # TODO: Инициализация класса, начало отсчёта
+    def __init__(self) -> None:
+        self.start = perf_counter()
+
+    # TODO: Object deletion, end of countdown
+    # TODO: Удаление объекта, окончание отсчёта
+    def __del__(self) -> None:
+        print('\nThe program was completed in {} second!'.format(perf_counter() - self.start))
