38ca87ce65baa7e9a44f86c7945fcc10b5e5a245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:34 +0000

    Add package vja/0.0.3/vja-0.0.3-py3-none-any.whl

---
+++ packages/vja/vja-0.0.3-py3-none-any.whl/vja/login.py
@@ -0,0 +1,41 @@
+from configparser import NoOptionError, NoSectionError
+
+from vja import config
+from vja.apiclient import ApiClient
+
+from vja import VjaError
+
+api_client: ApiClient = None
+
+
+def get_config():
+    return config.get_parser()
+
+
+def do_login(config=None) -> ApiClient:
+    """Create and initialize (including authentication) an API client."""
+    if not config:
+        config = get_config()
+
+    try:
+        api_url = config.get("application", "api_url")
+    except (NoSectionError, NoOptionError):
+        raise VjaError("Login url not specified in %s.Dying." % config.get_path())
+
+    client = ApiClient(api_url=api_url)
+
+    client.authenticate()
+
+    return client
+
+
+def get_client() -> ApiClient:
+    global api_client
+    if api_client:
+        return api_client
+    try:
+        api_client = do_login()
+        return api_client
+    except VjaError as e:
+        print(e)
+        exit(1)
