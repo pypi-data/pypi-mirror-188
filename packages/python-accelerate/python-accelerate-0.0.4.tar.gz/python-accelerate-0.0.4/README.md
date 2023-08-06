7264b865b541c0badd6052e0d0e254d103b7cda5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:59 +0000

    Add package python-accelerate/0.0.4/python-accelerate-0.0.4.tar.gz

---
+++ packages/python-accelerate/python-accelerate-0.0.4.tar.gz/python-accelerate-0.0.4/README.md
@@ -0,0 +1,6 @@
+# Python-accelerate
+A utility package I developed for myself, to make my life as a coder/data-scientist easier.
+
+**Dependencies:** Because this package will end up with too many dependencies, I have decided to not add them as required when setting up
+the package. Instead, the user of the package should install the necessary dependencies depending on the sub-modules 
+they want to use.
---
+++ packages/python-accelerate/python-accelerate-0.0.4.tar.gz/python-accelerate-0.0.4/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Numpy build options can be modified with a site.cfg file.
+See site.cfg.example for a template and more information.
+"""
+
+import os
+from pathlib import Path
+import setuptools
+import sys
+
+
+def setup_package():
+    package_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+    setup_params = dict(
+        name=package_name,
+        version='0.0.4',
+        maintainer='Mathew Stylianidis',
+        description='All-around utility functions for the Python programmer.',
+        long_description=Path('README.md').read_text(encoding='utf-8'),
+        long_description_content_type='text/markdown',
+        author='Mathew Stylianidis',
+        download_url=f'https://pypi.python.org/pypi/{package_name}',
+        project_urls={
+            "Bug Tracker": f'https://github.com/MathewStylianidis/{package_name}/issues',
+            "Source Code": f'https://github.com/MathewStylianidis/{package_name}',
+        },
+        license='MIT License',
+        classifiers=[
+            "Programming Language :: Python :: 3.9",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        test_suite='pytest',
+        python_requires='>=3.9',
+    )
+    setuptools.setup(**setup_params)
+
+
+if __name__ == '__main__':
+    setup_package()
+
