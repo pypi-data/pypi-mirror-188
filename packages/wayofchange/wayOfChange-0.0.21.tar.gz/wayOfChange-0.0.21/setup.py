b2b73c0ba0ccd330dd5fb4fd7a25b46f9dc4a5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:53 +0000

    Add package wayofchange/0.0.21/wayOfChange-0.0.21.tar.gz

---
+++ packages/wayofchange/wayOfChange-0.0.21.tar.gz/wayOfChange-0.0.21/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='wayOfChange',
+      version='0.0.24',
+      description='Put description here',
+      author='Edrihan Levesque',
+      author_email='Edrihan@gmail.com',
+      url='https://github.com/wayofchange',
+      packages=find_packages(exclude=['test']),
+     )
