ab3fe3be69f944ee4eab012c0e24c8e4c1795efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:03 +0000

    Add package eipiphany-seda/0.1.0.dev20230127045411/eipiphany-seda-0.1.0.dev20230127045411.tar.gz

---
+++ packages/eipiphany-seda/eipiphany-seda-0.1.0.dev20230127045411.tar.gz/eipiphany-seda-0.1.0.dev20230127045411/src/eipiphany_seda/component/queue_source.py
@@ -0,0 +1,36 @@
+from multiprocessing import Process
+
+from eipiphany_core.framework.base.source import Source
+from eipiphany_core.message.exchange import Exchange
+
+
+class QueueSource(Source):
+
+  def __init__(self, queue, concurrent_consumers=1):
+    self.__queue = queue
+    self.__concurrent_consumers = concurrent_consumers
+    self.__source_wrapper = None
+
+  def set_source_wrapper(self, source_wrapper):
+    self.__source_wrapper = source_wrapper
+
+  def start(self):
+    consumer_process = []
+    for i in range(self.__concurrent_consumers):
+      p = Process(target=self.__source_wrapper.wait_for_events)
+      p.daemon = True
+      p.start()
+      consumer_process.append(p)
+    return consumer_process
+
+  def wait_for_event(self):
+    return Exchange(self.__queue.get())
+
+  def event_success(self, exchange):
+    self.__queue.task_done()
+    pass
+
+  def event_failure(self, err, exchange):
+    self.__queue.task_done()
+    pass
+
