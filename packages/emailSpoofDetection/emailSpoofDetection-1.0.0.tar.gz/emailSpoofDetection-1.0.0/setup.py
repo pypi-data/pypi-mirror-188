a316ad15cfeb673520e76cefc4f262f983d8d617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:22 +0000

    Add package emailSpoofDetection/1.0.0/emailSpoofDetection-1.0.0.tar.gz

---
+++ packages/emailSpoofDetection/emailSpoofDetection-1.0.0.tar.gz/emailSpoofDetection-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="emailSpoofDetection",
+    version="1.0.0",
+    author="Arun Govindasamy",
+    description="To identify and prevent email spoofing attacks",
+    url= "https://github.com/govindasamyarun/emailSpoofDetection.git",
+    keywords = ["security", "spoof", "email parser", "email header analysis"], 
+    packages=["emailSpoofDetection"]
+)
