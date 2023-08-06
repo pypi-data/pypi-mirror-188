32daf43aaafaddccd15822c1f87b484530b92ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package fgc-tools/0.0.26/fgc_tools-0.0.26-py3-none-any.whl

---
+++ packages/fgc-tools/fgc_tools-0.0.26-py3-none-any.whl/fgc_tools/fgccreator.py
@@ -0,0 +1,65 @@
+from .fgcdrawer import FGCDrawer
+from bitarray import bitarray
+from .libs.hamming import *
+from .libs.binarytools import *
+from .libs.commonfunctions import CommonFunctions
+
+
+class FGCCreator:
+    """FGC Creator is used to create a Fancy Galaxy Code (FGC).
+    """
+
+    # FG Code constants
+    VERSION_BITS = 4
+    VERSION = 1
+
+    @staticmethod
+    def create_fgc(data, output_file, color_inner, color_outer, color_background, write_data_as_text) -> None:
+        """Creates an fgc according to the given parameters."""
+        all_data: bitarray = bitarray()
+
+        CommonFunctions.print_seperation_line("=")
+        print("Version:  %i" % FGCCreator.VERSION)
+        print("Data:     %s" % data)
+
+        # Convert version and data to bitarray
+        version_bits = byte_to_bitarray(FGCCreator.VERSION, FGCCreator.VERSION_BITS)
+        all_data.extend(version_bits)
+        data_bits2 = ''.join(format(i, '08b') for i in bytearray(data, encoding='utf-8'))
+        data_bits = bitarray(data_bits2)
+        all_data.extend(data_bits)
+
+        print("Binary:   ", end="")
+        print_bitarray(all_data)
+
+        # Add Hamming error correction bits
+        str_data = all_data.to01()
+        str_data = [int(bit) for bit in str_data]
+        all_data_encoded = bitarray(hamming_code(str_data))
+
+        print("Hamming:  ", end="")
+        print_bitarray(all_data_encoded)
+
+        str_data = all_data_encoded.to01()
+        str_data = [int(bit) for bit in str_data]
+        all_data_decoded = bitarray(hamming_decode(str_data))
+
+        sanity_check_passed = (all_data_decoded == all_data)
+        print(f"Sanity:   {'PASSED' if sanity_check_passed else 'FAILED! PLEASE CHECK INPUT AND HAMMING CODE.'}.")
+        if not sanity_check_passed:
+            print("Decoded:  ", end="")
+            print_bitarray(all_data_decoded)
+
+
+        CommonFunctions.print_seperation_line("=")
+
+        # Draw the actual simple vector graphic (svg)
+        FGCDrawer.draw_fgc(
+            data, 
+            all_data_encoded, 
+            output_file, 
+            color_inner=color_inner, 
+            color_outer=color_outer,
+            color_background=color_background,
+            write_data_as_text=write_data_as_text
+        )
---
+++ packages/fgc-tools/fgc_tools-0.0.26-py3-none-any.whl/test_reader.py
@@ -0,0 +1,59 @@
+from fgc_tools import FGCReader
+import matplotlib.pyplot as plt
+from fgc_tools.cvfunctions import *
+from fgc_tools.readresult import ReadResult
+
+
+def main():
+    print("FGC Reader started")
+
+    test_images = [
+        { "img": 'static/test_images/1.jpg', "content": "Level1" },
+        { "img": 'static/test_images/2.jpg', "content": "Level2 which is harder" },
+        { "img": 'static/test_images/3.jpg', "content": "Level3 which is super hard" },
+        { "img": 'static/test_images/4.jpg', "content": "Meister der Galaxie." },
+        { "img": 'static/test_images/5.jpg', "content": "Such pretty colors!" },
+        { "img": 'static/test_images/6.jpg', "content": "Eine URL: https://google.com" },
+        { "img": 'static/test_images/7.jpg', "content": "🥐" },
+        { "img": 'static/test_images/8.jpg', "content": "Tasse." },
+        { "img": 'static/test_images/9.jpg', "content": "github.com/felixmark/fgc" },
+        { "img": 'static/test_images/10.jpg', "content": "Support emojis everywhere 🥹😂" },
+        { "img": 'static/test_images/11.jpg', "content": "Baum 🌳" },
+        { "img": 'static/test_images/12.jpg', "content": "FGC" },
+    ]
+    passed_cnt = 0
+    failed_cnt = 0
+    total_time = 0
+
+    for test_image in test_images:
+        print("="*60)
+        print("Test image: %s" % test_image["img"])
+        print("-"*60)
+        read_result:ReadResult = FGCReader.read_image(test_image["img"])
+        show_image("Output of: " + test_image["img"], read_result.output_img)
+
+        print("-"*60)
+        print("Bit length:  %i" % len(read_result.raw_binary_string))
+        print("Version:     %s" % str(read_result.version))
+        print("Read Time:   %.3fs" % read_result.read_time)
+        print("Test data:   %s" % str(list(test_image["content"])))
+        print("Read data:   %s" % str(list(read_result.text)))
+        print("Result:      ", end="")
+        if read_result.text == test_image["content"]:
+            print("PASSED.")
+            passed_cnt += 1
+        else:
+            print("FAILED.")
+            failed_cnt += 1
+        
+        total_time += read_result.read_time 
+    
+    print("="*60)
+    print("TOTAL PASSED:  %i" % passed_cnt)
+    print("TOTAL FAILED:  %i" % failed_cnt)
+    print("AVERAGE TIME:  %.3fs" % (total_time / len(test_images)))
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()
