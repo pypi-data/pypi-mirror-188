2448549e820ca57b1185074fe550f9f49caafe9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:25 +0000

    Add package seedCrypto/1.1.0/seedCrypto-1.1.0.tar.gz

---
+++ packages/seedCrypto/seedCrypto-1.1.0.tar.gz/seedCrypto-1.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='seedCrypto',
+    version='1.1.0',
+    license="MIT",
+    description='kisa seed encryption and decryption',
+    author='blluv',
+    author_email='biluv@protonmail.com',
+    url="https://github.com/blluv/seedCrypto",
+    packages=setuptools.find_packages(),
+)
