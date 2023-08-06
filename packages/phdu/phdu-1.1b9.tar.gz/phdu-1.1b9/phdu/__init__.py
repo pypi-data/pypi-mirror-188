b5c18cc9babfca82d2f351f0d25440c503abd2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:28 +0000

    Add package phdu/1.1b9/phdu-1.1b9.tar.gz

---
+++ packages/phdu/phdu-1.1b9.tar.gz/phdu-1.1b9/phdu/__init__.py
@@ -0,0 +1,8 @@
+from tidypath import savedata, savefig, add_arg, modify_arg, delete_arg
+from . import np_utils
+from . import pd_utils
+from . import storage
+from . import plots
+from . import stats
+from .stats.rtopy import resample
+from .stats.test import permutation
