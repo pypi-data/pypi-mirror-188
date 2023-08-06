0b00d0aade959d9ea5422c8216122d8b2f6aa5bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:43 +0000

    Add package stockman/0.0.1/stockman-0.0.1-py3-none-any.whl

---
+++ packages/stockman/stockman-0.0.1-py3-none-any.whl/stockman/__init__.py
@@ -0,0 +1,29 @@
+"""
+      Author:  sml2h3
+      Date:    2023/1/18
+      File:    __init__
+      Project: PyCharm
+"""
+from . import configs
+from .tasks import init_data
+from .tasks import install_db
+
+
+class Stock:
+    def __init__(self, config: str = ""):
+        configs.init_config(config)
+
+    @staticmethod
+    def generate_config():
+        configs.make_config()
+
+    @staticmethod
+    def install():
+        install_db()
+
+    @staticmethod
+    def init():
+        init_data()
+
+    def set_limits(self, feature=None, limit_time=(0, 0)):
+        pass
---
+++ packages/stockman/stockman-0.0.1-py3-none-any.whl/stockman/utils/exceptions.py
@@ -0,0 +1,30 @@
+"""
+      Author:  sml2h3                              
+      Date:    2023/1/19                             
+      File:    exceptions                             
+      Project: PyCharm                     
+"""
+
+
+class ConfigTypeError(BaseException):
+    pass
+
+
+class ConfigNotFound(BaseException):
+    pass
+
+
+class ConfigFileNotFound(BaseException):
+    pass
+
+
+class NotInstalled(BaseException):
+    pass
+
+
+class InstallDuplicated(BaseException):
+    pass
+
+
+class OverTimesLimits(BaseException):
+    pass
