3ed3ca427b63f71021a84ff016addddc90be7984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:03 +0000

    Add package recsagent/1.0.0/recsagent-1.0.0.tar.gz

---
+++ packages/recsagent/recsagent-1.0.0.tar.gz/recsagent-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open('README.md','r') as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'recsagent',
+    version='1.0.0',
+    description='somepackage',
+    py_modules=["prepagent"],
+    install_requires = [
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'sklearn',
+        'tqdm',
+        'helperagent==4.0.0',
+        'datetime',
+        'xgboost',
+        'statsmodels',
+        'interpret',
+        'shap'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    extras_require={
+        "dev":[
+            "pytest",
+        ],
+    },
+)
