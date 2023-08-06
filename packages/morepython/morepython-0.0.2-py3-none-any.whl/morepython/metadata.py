b12470e64fbfafed090b600cad4cfa5352436e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:46 +0000

    Add package morepython/0.0.2/morepython-0.0.2-py3-none-any.whl

---
+++ packages/morepython/morepython-0.0.2-py3-none-any.whl/morepython/metadata.py
@@ -0,0 +1,10 @@
+"""Set metadata for morepython"""
+
+__title__ = 'morepython'
+__program__ = 'morepython'
+__summary__ = "A collection of useful methods and classes."
+__author__ = 'xenova'
+__email__ = 'admin@xenova.com'
+__copyright__ = '2023 xenova'
+__url__ = 'https://github.com/xenova/morepython'
+__version__ = '0.0.2'
