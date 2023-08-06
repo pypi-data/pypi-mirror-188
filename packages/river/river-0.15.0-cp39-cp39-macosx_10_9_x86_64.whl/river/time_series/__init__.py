56f607307a9aafa92b56cfa8265f7ba3797cdefb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:54 +0000

    Add package river/0.15.0/river-0.15.0-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/river/river-0.15.0-cp39-cp39-macosx_10_9_x86_64.whl/river/time_series/__init__.py
@@ -0,0 +1,17 @@
+"""Time series forecasting."""
+from . import base
+from .evaluate import evaluate, iter_evaluate
+from .holt_winters import HoltWinters
+from .metrics import ForecastingMetric, HorizonAggMetric, HorizonMetric
+from .snarimax import SNARIMAX
+
+__all__ = [
+    "base",
+    "evaluate",
+    "iter_evaluate",
+    "ForecastingMetric",
+    "HorizonAggMetric",
+    "HorizonMetric",
+    "HoltWinters",
+    "SNARIMAX",
+]
