af08a774df9725802d9fe32b8e0a9199f7640750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:53 +0000

    Add package functown/1.0.0/functown-1.0.0-py3-none-any.whl

---
+++ packages/functown/functown-1.0.0-py3-none-any.whl/functown/insights/base.py
@@ -0,0 +1,19 @@
+from typing import List
+import logging
+
+from functown.utils import BaseDecorator
+
+
+class InsightsDecorator(BaseDecorator):
+    """Base Class for Application insights decorators."""
+
+    def __init__(self, instrumentation_key: str, added_kw: List[str], **kwargs):
+        super().__init__(None, added_kw=added_kw, **kwargs)
+
+        if instrumentation_key is None:
+            logging.warning(
+                "No instrumentation key provided. "
+                "No data will be sent to Application Insights."
+            )
+
+        self.instrumentation_key = instrumentation_key
