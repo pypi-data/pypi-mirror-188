59f4a0a88e5ff17e6ffdf3e20496c07b81275bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:51 +0000

    Add package apache-airflow-providers-mnb/1.0.2/apache_airflow_providers_mnb-1.0.2-py3-none-any.whl

---
+++ packages/apache-airflow-providers-mnb/apache_airflow_providers_mnb-1.0.2-py3-none-any.whl/airflow/providers/mnb/__init__.py
@@ -0,0 +1,7 @@
+def get_provider_info():
+    return {
+        "package-name": "apache-airflow-providers-mnb", # Required
+        "name": "Exchange rates by MNB (Hungarian National Bank)", # Required
+        "description": "Exchange rates by MNB (Hungarian National Bank).", # Required
+        "versions": ["1.0.2"] # Required
+    }
