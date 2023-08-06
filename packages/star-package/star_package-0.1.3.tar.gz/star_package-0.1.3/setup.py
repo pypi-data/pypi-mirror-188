8b650ffd435fca4fb8f44e5b587e0e3822affff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:18 +0000

    Add package star-package/0.1.3/star_package-0.1.3.tar.gz

---
+++ packages/star-package/star_package-0.1.3.tar.gz/star_package-0.1.3/setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='star_package',
+    author='liu yang',
+    version='0.1.3',
+    license='MIT',
+
+    description='some tools',
+    long_description='''some tools''',
+
+    author_email='otakzliu@163.com',
+    packages=setuptools.find_packages(),
+
+    # 依赖包
+    install_requires=[
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries'
+    ],
+    zip_safe=True,
+)
+
+
