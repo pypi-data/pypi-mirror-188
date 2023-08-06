fcd7f325ac34f12399cb78be5e2bd8308ecc0792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:34 +0000

    Add package filter-strings/0.0.8/filter-strings-0.0.8.tar.gz

---
+++ packages/filter-strings/filter-strings-0.0.8.tar.gz/filter-strings-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup(
+    name="filter-strings",
+    version="0.0.8",
+    author="jaytrairat",
+    author_email="jaytrairat@outlook.com",
+    description="Filter strings from a file based on regex patterns.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaytrairat/python-filter-strings",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
