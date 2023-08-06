560ac4d86a3ad1b92f704849909f4647164c266a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:11 +0000

    Add package bruhcolor/0.0.46/bruhcolor-0.0.46-py3-none-any.whl

---
+++ packages/bruhcolor/bruhcolor-0.0.46-py3-none-any.whl/bruhcolor/__init__.py
@@ -0,0 +1,19 @@
+from bruhcolor.bruhcolor import ATTRIBUTES, RESET_8, RESET_256, COLORS_8, COLORS_256, HIGHLIGHTS_8, HIGHLIGHTS_256, bruhcolored8, bruhcolored256, color_codes8, color_codes256, bruhcolorwrapper, colors8, colors256, VERSION
+
+__all__ = [
+    "ATTRIBUTES",
+    "COLORS_8",
+    "COLORS_256",
+    "HIGHLIGHTS_8",
+    "HIGHLIGHTS_256",
+    "RESET_8",
+    "RESET_256",
+    "bruhcolored8",
+    "bruhcolored256",
+    "bruhcolorwrapper",
+    "colors8",
+    "colors256",
+    "color_codes8",
+    "color_codes256",
+    "VERSION"
+]
