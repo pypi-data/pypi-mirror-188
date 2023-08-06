7b035df47cde422abcdc97832d495ebaa03433ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:39 +0000

    Add package otterop-lang/0.1.0.post2/otterop_lang-0.1.0.post2.tar.gz

---
+++ packages/otterop-lang/otterop_lang-0.1.0.post2.tar.gz/otterop_lang-0.1.0.post2/otterop/lang/string.py
@@ -0,0 +1,24 @@
+class String:
+
+    def __init__(self, _wrapped):
+        self._wrapped = _wrapped
+
+    @staticmethod
+    def length():
+        return len(self._wrapped)
+
+    @staticmethod
+    def wrap(_wrapped):
+        return String(_wrapped)
+
+    def __str__(self):
+        return self._wrapped
+
+    def compare_to(self, other):
+        if not isinstance(other, String):
+            return 1
+        if self._wrapped < other._wrapped:
+            return -1
+        elif self._wrapped > other._wrapped:
+            return 1
+        return 0
