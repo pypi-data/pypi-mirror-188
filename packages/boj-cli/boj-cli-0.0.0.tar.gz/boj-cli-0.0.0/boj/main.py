36d158047f61ceeba21065834e4f2a095f5abb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:38 +0000

    Add package boj-cli/0.0.0/boj-cli-0.0.0.tar.gz

---
+++ packages/boj-cli/boj-cli-0.0.0.tar.gz/boj-cli-0.0.0/boj/main.py
@@ -0,0 +1,23 @@
+from boj import args_resolver
+from boj.commands import login, submit, problem, init, run, random
+import traceback
+
+command_dict = {
+    "login": login.execute,
+    "submit": submit.execute,
+    "problem": problem.execute,
+    "init": init.execute,
+    "run": run.execute,
+    "random": random.execute,
+}
+
+
+def entry():
+    parser = args_resolver.create_parser()
+    try:
+        args = parser.parse_args()
+        command_dict[args.command](args)
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
+        exit(1)
