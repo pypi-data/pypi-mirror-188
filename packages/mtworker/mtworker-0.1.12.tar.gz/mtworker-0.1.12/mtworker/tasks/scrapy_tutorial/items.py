686854243366abc6283e8f1b134cd4823ea8c6ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:58 +0000

    Add package mtworker/0.1.12/mtworker-0.1.12.tar.gz

---
+++ packages/mtworker/mtworker-0.1.12.tar.gz/mtworker-0.1.12/mtworker/tasks/scrapy_tutorial/items.py
@@ -0,0 +1,13 @@
+import scrapy
+from scrapy.item import Item, Field
+# from scrapy.contrib.djangoitem import DjangoItem
+# from scrapy_djangoitem import DjangoItem
+# from .. import models
+
+class ArticleItem(Item):
+    title=Field(max_length=64, null=False, blank=False, default=None, verbose_name = "标题")
+    subtitle=Field(max_length=256, null=False, blank=False, default="", verbose_name = "副标题")
+    sumary= Field(max_length=64, null=False, blank=False, default="", verbose_name = "咱要")
+    link=Field(verbose_name="网址")
+    html_content = Field(null=False, blank=False, default="", verbose_name = "基于html标签的正文")
+    text_content = Field(null=False, blank=False, default="", verbose_name = "文本")
