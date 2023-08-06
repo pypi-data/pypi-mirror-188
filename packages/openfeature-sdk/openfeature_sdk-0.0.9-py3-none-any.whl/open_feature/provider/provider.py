7f4e82ab4541e36a3a5a7266003814fcb36508f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:00 +0000

    Add package openfeature-sdk/0.0.9/openfeature_sdk-0.0.9-py3-none-any.whl

---
+++ packages/openfeature-sdk/openfeature_sdk-0.0.9-py3-none-any.whl/open_feature/provider/provider.py
@@ -0,0 +1,62 @@
+import typing
+from abc import abstractmethod
+
+from open_feature.evaluation_context.evaluation_context import EvaluationContext
+from open_feature.flag_evaluation.flag_evaluation_details import FlagEvaluationDetails
+from open_feature.hooks.hook import Hook
+from open_feature.provider.metadata import Metadata
+
+
+class AbstractProvider:
+    @abstractmethod
+    def get_metadata(self) -> Metadata:
+        pass
+
+    @abstractmethod
+    def get_provider_hooks(self) -> typing.List[Hook]:
+        return []
+
+    @abstractmethod
+    def resolve_boolean_details(
+        self,
+        flag_key: str,
+        default_value: bool,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagEvaluationDetails[bool]:
+        pass
+
+    @abstractmethod
+    def resolve_string_details(
+        self,
+        flag_key: str,
+        default_value: str,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagEvaluationDetails[str]:
+        pass
+
+    @abstractmethod
+    def resolve_integer_details(
+        self,
+        flag_key: str,
+        default_value: int,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagEvaluationDetails[int]:
+        pass
+
+    @abstractmethod
+    def resolve_float_details(
+        self,
+        flag_key: str,
+        default_value: float,
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagEvaluationDetails[float]:
+        pass
+
+    @abstractmethod
+    def resolve_object_details(
+        self,
+        flag_key: str,
+        default_value: typing.Union[dict, list],
+        evaluation_context: typing.Optional[EvaluationContext] = None,
+    ) -> FlagEvaluationDetails[typing.Union[dict, list]]:
+        pass
