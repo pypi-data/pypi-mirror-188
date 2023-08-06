683702f46f4885f152c0620d528381537dd16a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:32 +0000

    Add package SoAuth/1.0.4/SoAuth-1.0.4.tar.gz

---
+++ packages/SoAuth/SoAuth-1.0.4.tar.gz/SoAuth-1.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+long_description = open("README.md").read()
+
+setup(
+    name='SoAuth',
+    version='1.0.4',
+    description='Flask middleware for SoAuth',
+    url='https://github.com/sojs-coder/SoAuth-Py',
+    author='SoJS',
+    author_email='sojs_coder@protonmail.com',
+    license='ISC',
+    packages=['soauth'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'requests',
+        'flask'
+    ],
+    zip_safe=False
+)
