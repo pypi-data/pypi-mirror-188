fad1682f18f12a38d694cc911b1f13b18b001ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:57 +0000

    Add package space-wrap/0.0.3/space_wrap-0.0.3.tar.gz

---
+++ packages/space-wrap/space_wrap-0.0.3.tar.gz/space_wrap-0.0.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='space_wrap',
+    version='0.0.3',
+    description='Automated Spacy wrapper to turn plain text into Spacy doc objects',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    py_modules=['space_wrap'],
+    author='Evan Anthony',
+    author_email='anthonyevanm@gmail.com',
+    keywords=['Spacy', 'tokenizer', 'NLP'],
+    url='https://github.com/eanthony1224',
+    download_url='https://pypi.org/project/space_wrap/'
+)
+
+install_requires = [
+   'pandas',
+    'spacy'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
+
