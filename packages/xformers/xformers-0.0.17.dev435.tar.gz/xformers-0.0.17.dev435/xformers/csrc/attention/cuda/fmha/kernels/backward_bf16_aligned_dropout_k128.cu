96f5eb6c56dcc1797d980c696fdf0cc45bdf2fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:20:25 +0000

    Add package xformers/0.0.17.dev435/xformers-0.0.17.dev435.tar.gz

---
+++ packages/xformers/xformers-0.0.17.dev435.tar.gz/xformers-0.0.17.dev435/xformers/csrc/attention/cuda/fmha/kernels/backward_bf16_aligned_dropout_k128.cu
@@ -0,0 +1,22 @@
+// This file is auto-generated. See "generate_kernels.sh"
+#include "backward.h"
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM50(
+    cutlass::bfloat16_t,
+    true,
+    true,
+    128);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM70(
+    cutlass::bfloat16_t,
+    true,
+    true,
+    128);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM75(
+    cutlass::bfloat16_t,
+    true,
+    true,
+    128);
+INSTANTIATE_ATTENTION_KERNEL_BACKWARD_SM80(
+    cutlass::bfloat16_t,
+    true,
+    true,
+    128);
