d7bc4aef9a8964ef27135c745ce15bbef9ccf786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:24 +0000

    Add package RNDMMAHDICK/0.3/RNDMMAHDICK-0.3.tar.gz

---
+++ packages/RNDMMAHDICK/RNDMMAHDICK-0.3.tar.gz/RNDMMAHDICK-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="RNDMMAHDICK",
+version='0.3',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['mahdihh'],
+install_requires=[])
+
+# python setup.py bdist_wheel
+#  python setup.py pytest
