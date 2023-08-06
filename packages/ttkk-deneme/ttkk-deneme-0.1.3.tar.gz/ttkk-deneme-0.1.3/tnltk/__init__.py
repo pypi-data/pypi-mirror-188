52163009564fda758ef602887e174c376bd8af92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:53 +0000

    Add package ttkk-deneme/0.1.3/ttkk-deneme-0.1.3.tar.gz

---
+++ packages/ttkk-deneme/ttkk-deneme-0.1.3.tar.gz/ttkk-deneme-0.1.3/tnltk/__init__.py
@@ -0,0 +1,5 @@
+from tnltk.normalizer.deasciifier import Deasciifier
+from tnltk.normalizer import normalizer
+from tnltk.sentence_splitter.sentence_splitter import SentenceSplitter
+
+__version__ = "0.1.3"
