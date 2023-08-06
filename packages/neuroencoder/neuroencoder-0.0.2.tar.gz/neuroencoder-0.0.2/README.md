09cf889a70e753e37a0f3b797412e29a83a9c762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:07 +0000

    Add package neuroencoder/0.0.2/neuroencoder-0.0.2.tar.gz

---
+++ packages/neuroencoder/neuroencoder-0.0.2.tar.gz/neuroencoder-0.0.2/README.md
@@ -0,0 +1,7 @@
+# Neuroencoder
+
+A package for encoding and decoding neural data.
+
+(Pre-Alpha, work in progress, not ready for use)
+
+(c) 2023 by avocardio
---
+++ packages/neuroencoder/neuroencoder-0.0.2.tar.gz/neuroencoder-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+  name='neuroencoder',
+  version='0.0.2',
+  description='A package for encoding neural data, including spike trains, LFP, and EEG',
+  url='https://github.com/avocardio/neuroencoder',
+  author='M.K.',
+  author_email='mkalcher@uos.de',
+  packages=['neuroencoder'],
+  long_description=open('README.md').read(),
+  install_requires=[
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'pandas',
+    'mne',
+    'yasa',
+  ],
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Healthcare Industry',
+    'Intended Audience :: Science/Research',
+    'License :: Other/Proprietary License',
+    'Programming Language :: Python :: 3',
+  ],
+)
