56cc6c6c464264b3b000477f178c4bece652a227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:49 +0000

    Add package robin-sd-upload/0.1.38/robin_sd_upload-0.1.38-py3-none-any.whl

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.38-py3-none-any.whl/robin_sd_upload/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+from .arg_parse import arg_parse
+from .logger import setup_logger
+
+home_dir = os.path.expanduser("~")
+config_file = home_dir + "/.config/robin/software_deployment.yaml"
+
+log_filename = 'upload.log'
+
+def main():
+    setup_logger(log_filename)
+    arg_parse(config_file)
+
+if __name__ == "__main__":
+    main()
