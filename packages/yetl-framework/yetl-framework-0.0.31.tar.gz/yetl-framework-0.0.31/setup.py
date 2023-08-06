1a7dfc52faaf5d98a4d661df6d49a19d20d06146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:32 +0000

    Add package yetl-framework/0.0.31/yetl-framework-0.0.31.tar.gz

---
+++ packages/yetl-framework/yetl-framework-0.0.31.tar.gz/yetl-framework-0.0.31/setup.py
@@ -0,0 +1,56 @@
+import pathlib
+from setuptools import setup, find_packages
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="yetl-framework",
+    version="0.0.31",
+    description="yet (another spark) etl framework",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://www.yetl.io/",
+    project_urls={
+        "GitHub": "https://github.com/sibytes/yetl",
+        "Documentation": "https://www.yetl.io/",
+    },
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=[
+        "yetl",
+        "yetl.cli",
+        "yetl.cli.project",
+        "yetl.flow",
+        "yetl.flow.audit",
+        "yetl.flow.context",
+        "yetl.flow.dataflow",
+        "yetl.flow.dataset",
+        "yetl.flow.exceptions",
+        "yetl.flow.file_system",
+        "yetl.flow.parser",
+        "yetl.flow.pipeline_repo",
+        "yetl.flow.save",
+        "yetl.flow.schema_repo",
+        "yetl.workflow",
+    ],
+    install_requires=[
+        "pyyaml",
+        "pyspark",
+        "regex",
+        "delta-spark",
+        "Jinja2",
+        "typer",
+        "pydantic",
+    ],
+    zip_safe=False,
+)
