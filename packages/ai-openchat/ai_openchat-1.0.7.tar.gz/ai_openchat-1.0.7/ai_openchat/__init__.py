80c8e963bebba6b256d9f1b5999324da2bea0f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:22 +0000

    Add package ai-openchat/1.0.7/ai_openchat-1.0.7.tar.gz

---
+++ packages/ai-openchat/ai_openchat-1.0.7.tar.gz/ai_openchat-1.0.7/ai_openchat/__init__.py
@@ -0,0 +1,11 @@
+from ai_openchat.openai import AsyncOpenAI
+from .base import model
+from .base import openai
+from .base import session
+
+__all__ = (
+    'AsyncOpenAI',
+    'model',
+    'openai',
+    'session'
+)
