9b5653f61c59d78b303d85b49e0a056dcfc7c4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:59 +0000

    Add package bordercollie/0.0.14/bordercollie-0.0.14-py3-none-any.whl

---
+++ packages/bordercollie/bordercollie-0.0.14-py3-none-any.whl/bordercollie/apps/workflow.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import codefast as cf
+from celery import Celery
+
+from simauth import OnlineAuth
+from bordercollie.auth import auth 
+
+try:
+    broker = auth.celery_backend
+except:
+    broker = OnlineAuth('https://cf.ddot.cc/api').auth('broker')
+app = Celery('collie_workflow', broker=broker, backend=broker)
+
+app.conf.result_expires = 10
+
+
+@app.task
+def shell(cmd: str) -> str:
+    try:
+        return cf.shell(cmd)
+    except Exception as e:
+        return str(e)
+    
+
