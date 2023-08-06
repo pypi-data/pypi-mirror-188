49926ab8f31964ce4b39c9a7c018a2bf9bb32f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:54 +0000

    Add package plotex/0.0.13/plotex-0.0.13.tar.gz

---
+++ packages/plotex/plotex-0.0.13.tar.gz/plotex-0.0.13/src/plotex/utils/hashing.py
@@ -0,0 +1,22 @@
+import hashlib
+import base64
+
+
+def hash_url(url):
+    """generate a hash for the url
+
+    Args:
+        url: the url string
+
+    Returns:
+        the generated hash
+    """
+    # Take the hash of the URL
+    url_hash = hashlib.sha1(url.encode('utf-8')).hexdigest()
+    # Encode the hash in base64
+    file_name = base64.b64encode(url_hash.encode('utf-8')).decode('utf-8')
+    # Remove any characters that are not safe for use in a file name
+    file_name = file_name.replace('/', '_').replace('+', '-')
+    # Truncate the file name to a maximum length
+    file_name = file_name[:10]
+    return file_name
