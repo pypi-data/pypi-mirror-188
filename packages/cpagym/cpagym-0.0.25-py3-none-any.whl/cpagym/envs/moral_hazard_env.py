ba26dd02f111dfb506e17f020fcd8aa964d6eb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:00 +0000

    Add package cpagym/0.0.25/cpagym-0.0.25-py3-none-any.whl

---
+++ packages/cpagym/cpagym-0.0.25-py3-none-any.whl/cpagym/envs/moral_hazard_env.py
@@ -0,0 +1,27 @@
+import gymnasium as gym
+from gymnasium import spaces
+import numpy as np
+class Moral_HazardEnv(gym.Env):
+    metadata = {"render.modes": ["human"]}
+    def __init__(self):
+        self.observation_space = spaces.Discrete(2)
+        self.action_space = spaces.Discrete(2)
+        self.reward_range = (-1, 1)
+        self.additional_info = None
+    def step(self, action):
+        observation = self.observation_space.sample()
+        reward = 1.0
+        terminated = True
+        info = {}
+        truncated = False
+        return observation, reward, terminated, truncated, info
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        observation = self.observation_space.sample()
+        info = {}
+        #print(observation)
+        return observation,info  # reward, done, info can't be included
+    def render(self, mode="human"):
+        pass
+    def close(self):
+        pass
