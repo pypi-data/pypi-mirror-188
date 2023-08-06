23a94e0f4dcc290d858e1fd7857be5f224ee1dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:57 +0000

    Add package qdrant-client/0.11.10/qdrant_client-0.11.10.tar.gz

---
+++ packages/qdrant-client/qdrant_client-0.11.10.tar.gz/qdrant_client-0.11.10/qdrant_client/http/__init__.py
@@ -0,0 +1,17 @@
+import inspect
+
+from pydantic import BaseModel
+
+from qdrant_client.http.api_client import (
+    ApiClient as ApiClient,
+    AsyncApis as AsyncApis,
+    SyncApis as SyncApis,
+)  # noqa F401
+from qdrant_client.http.models import models as models
+
+
+for model in inspect.getmembers(models, inspect.isclass):
+    if model[1].__module__ == "qdrant_client.http.models.models":
+        model_class = model[1]
+        if issubclass(model_class, BaseModel):
+            model_class.update_forward_refs()
