98d540c622537e13c47dad96515d5ffee1265214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:48 +0000

    Add package custom-card-component/0.0.1/custom_card_component-0.0.1.tar.gz

---
+++ packages/custom-card-component/custom_card_component-0.0.1.tar.gz/custom_card_component-0.0.1/custom_card_component/frontend/build/precache-manifest.548d3303618cc8d0addf9c660f5248be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9830697d0e2fed4a6a15021a5f0b1ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "48d2d42ef0283c8857a4",
+    "url": "./static/js/2.69e5eb1f.chunk.js"
+  },
+  {
+    "revision": "2c41efe49968baecc5f58fc671d668d2",
+    "url": "./static/js/2.69e5eb1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe474ee5354f629b65e",
+    "url": "./static/js/main.ed718770.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
