92c956211258a8c7cc8a52edfa4e7567b89dcf9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:43 +0000

    Add package PySide2-Fluent-Widgets/0.0.2/PySide2_Fluent_Widgets-0.0.2-py3-none-any.whl

---
+++ packages/PySide2-Fluent-Widgets/PySide2_Fluent_Widgets-0.0.2-py3-none-any.whl/qfluentwidgets/common/icon.py
@@ -0,0 +1,41 @@
+# coding:utf-8
+from PySide2.QtCore import QPoint, QRect, QRectF, QSize, Qt
+from PySide2.QtGui import QIcon, QIconEngine, QImage, QPainter, QPixmap
+from PySide2.QtSvg import QSvgRenderer
+
+from .config import qconfig
+
+
+class PixmapIconEngine(QIconEngine):
+    """ Pixmap icon engine """
+
+    def __init__(self, iconPath):
+        self.iconPath = iconPath
+        super().__init__()
+
+    def paint(self, painter: QPainter, rect, mode, state):
+        painter.setRenderHints(QPainter.Antialiasing |
+                               QPainter.SmoothPixmapTransform)
+        if not self.iconPath.lower().endswith('svg'):
+            painter.drawImage(rect, QImage(self.iconPath))
+        else:
+            renderer = QSvgRenderer(self.iconPath)
+            renderer.render(painter, QRectF(rect))
+
+    def pixmap(self, size: QSize, mode: QIcon.Mode, state):
+        pixmap = QPixmap(size)
+        pixmap.fill(Qt.transparent)
+        self.paint(QPainter(pixmap), QRect(QPoint(0, 0), size), mode, state)
+        return pixmap
+
+
+class Icon(QIcon):
+
+    def __init__(self, iconPath):
+        self.iconPath = iconPath
+        super().__init__(PixmapIconEngine(iconPath))
+
+
+def getIconColor():
+    """ get the color of icon based on theme """
+    return "white" if qconfig.theme == 'dark' else 'black'
