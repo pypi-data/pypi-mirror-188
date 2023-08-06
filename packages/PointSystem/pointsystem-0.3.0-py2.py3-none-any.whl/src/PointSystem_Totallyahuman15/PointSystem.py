c60be8832f3d7c32050a94c625bd318edcdba035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:53 +0000

    Add package PointSystem/0.3.0/pointsystem-0.3.0-py2.py3-none-any.whl

---
+++ packages/PointSystem/pointsystem-0.3.0-py2.py3-none-any.whl/src/PointSystem_Totallyahuman15/PointSystem.py
@@ -0,0 +1,46 @@
+class points:
+    def __init__(self, tokenAmount: int, *,  classifier: str = None, front: bool = False):
+        self.points = tokenAmount
+        if classifier == None:
+            self.classifier = None
+        else:
+            if front == True:
+                self.classifier = [classifier + ": ", ""]
+            else:
+                self.classifier = ["", " " + classifier]
+
+    def add(self, add: int):
+        self.points = self.points + add
+
+    def sub(self, sub: int):
+        self.points = self.points - sub
+
+    def mult(self, mult: int):
+        self.points = self.points * mult
+
+    def div(self, div: int):
+        self.points = self.points / div
+
+    def set(self, amount: int = None):
+        self.points = amount
+
+    def round(self):
+        self.points = int(self.points)
+
+    def float(self):
+        self.points = float(self.points)
+
+    def reset(self):
+        self.points = 0
+
+    def setClassifier(self, classifier: str, front: bool):
+        if front == True:
+            self.classifier = classifier + ": " + str(self.points)
+        else:
+            self.classifier = str(self.points) + classifier
+
+    def show(self):
+        if self.classifier == None:
+            print(self.points)
+        else:
+            print(str(self.points).join(self.classifier))
