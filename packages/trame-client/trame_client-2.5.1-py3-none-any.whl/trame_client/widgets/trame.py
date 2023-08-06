81804facaca4fb3419d089f51a456e8a1c3ef527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:32 +0000

    Add package trame-client/2.5.1/trame_client-2.5.1-py3-none-any.whl

---
+++ packages/trame-client/trame_client-2.5.1-py3-none-any.whl/trame_client/widgets/trame.py
@@ -0,0 +1,128 @@
+from .core import AbstractElement
+
+__all__ = [
+    "Connect",
+    "Loading",
+    "ServerTemplate",
+    "StateResolver",
+    "Style",
+]
+
+
+# -----------------------------------------------------------------------------
+# TrameApp
+# -----------------------------------------------------------------------------
+# SKIP: built-in client not to be use within a server template
+
+# -----------------------------------------------------------------------------
+# TrameConnect
+# -----------------------------------------------------------------------------
+class Connect(AbstractElement):
+    """
+    Container managing the handshake and connection to a trame server
+
+    :param name: Application name
+    :type name: str
+
+    :param config: Dict object capturing all the key/value pair needed to start
+                   or configure the connection.
+    :type name: {}
+
+    :param use_url: Should we process the URL to extend the connection config
+    :type use_url: bool
+
+    :param trame_template_change: Event notification when a template has changed
+    :param state_change: Event notification when something in the state has changed
+    """
+
+    def __init__(self, children=None, **kwargs):
+        super().__init__("trame-connect", children, **kwargs)
+        self._attr_names += [
+            "name",
+            "config",
+            ("use_url", "useUrl"),
+        ]
+        self._event_names += [
+            ("trame_template_change", "trameTemplateChange"),
+            ("state_change", "stateChange"),
+        ]
+
+
+# -----------------------------------------------------------------------------
+# TrameLoading
+# -----------------------------------------------------------------------------
+class Loading(AbstractElement):
+    """
+    Three circle spinning with a message
+
+    :param message: Content of the message
+    :type name: str
+    """
+
+    def __init__(self, children=None, **kwargs):
+        super().__init__("trame-loading", children, **kwargs)
+        self._attr_names += [
+            "message",
+        ]
+
+
+# -----------------------------------------------------------------------------
+# TrameServerTemplate
+# -----------------------------------------------------------------------------
+class ServerTemplate(AbstractElement):
+    """
+    Template content presentation
+
+    :param name: Name of the template to display. (default: 'main')
+    :type name: str
+
+    :param use_url: If true, will use the `{url_key}=` from the url to override
+                    the `name=` attribute.
+    :type use_url: bool
+
+    :param url_key: Name of the key to extract from url for use_url. (default: 'ui')
+    :type url_key: str
+    """
+
+    def __init__(self, children=None, **kwargs):
+        super().__init__("trame-server-template", children, **kwargs)
+        self._attr_names += [
+            ("name", "templateName"),
+            ("use_url", "useUrl"),
+            ("url_key", "urlKey"),
+        ]
+
+
+# -----------------------------------------------------------------------------
+# TrameStateResolver
+# -----------------------------------------------------------------------------
+class StateResolver(AbstractElement):
+    """Provide means to extract sub-state for its slots"""
+
+    def __init__(self, children=None, **kwargs):
+        super().__init__("trame-state-resolver", children, **kwargs)
+        self._attr_names += [
+            "names",
+        ]
+
+
+# -----------------------------------------------------------------------------
+# TrameStyle
+# -----------------------------------------------------------------------------
+class Style(AbstractElement):
+    """Provide means to inject global CSS rules"""
+
+    def __init__(self, css_content=None, **kwargs):
+        super().__init__("trame-style", **kwargs)
+        self._key = f"trame__inline_style_{self._id}"
+        self._attributes["_css"] = f':css="{self._key}"'
+        self.server.state.setdefault(self._key, css_content)
+
+    def update(self, css_content):
+        """Update style content"""
+        self.server.state[self._key] = css_content
+
+    @property
+    def var_name(self):
+        """Name the the state variable used by this widget"""
+        return self._key
