4cb6d1a78ac9292372f7912d802dd4a106dd64ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:22 +0000

    Add package OGCReader/0.0.3/OGCReader-0.0.3.tar.gz

---
+++ packages/OGCReader/OGCReader-0.0.3.tar.gz/OGCReader-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='OGCReader',
+    version='0.0.3',
+    packages=[''],
+    url='https://github.com/williamccondori/OGCReader',
+    license='MIT',
+    author='William Condori',
+    author_email='williamccondori@gmail.com',
+    description='OGC Reader'
+)
