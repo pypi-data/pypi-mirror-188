a8a934d9c3fecf2a6b3cbfa8561beb0024b4c4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:02 +0000

    Add package spatbox/0.1.6/spatbox-0.1.6.tar.gz

---
+++ packages/spatbox/spatbox-0.1.6.tar.gz/spatbox-0.1.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="spatbox",
+    version="0.1.6",
+    description="A Python Library For GeoSpatial Data Proressing And Modeling",
+    author="SpatLyu",
+    author_email="3180929657@qq.com",
+    url= "https://github.com/SpatLyu/SpatBox",
+    license="MIT license", 
+    packages=find_packages(include=["spatbox", "spatbox.*"]),
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ packages/spatbox/spatbox-0.1.6.tar.gz/spatbox-0.1.6/spatbox/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.6",
+__author__ = "SpatLyu"
+
+import spatbox as stx
+import spatbox.datasets 
+import spatbox.vector
+import spatbox.raster 
+
+
+
