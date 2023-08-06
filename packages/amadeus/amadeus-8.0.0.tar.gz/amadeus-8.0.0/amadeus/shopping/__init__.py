0a717ffa209eaee35528a973c21a3d47ab8362f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:08 +0000

    Add package amadeus/8.0.0/amadeus-8.0.0.tar.gz

---
+++ packages/amadeus/amadeus-8.0.0.tar.gz/amadeus-8.0.0/amadeus/shopping/__init__.py
@@ -0,0 +1,10 @@
+from ._flight_dates import FlightDates
+from ._flight_destinations import FlightDestinations
+from ._flight_offers_search import FlightOffersSearch
+from ._hotel_offer_search import HotelOfferSearch
+from ._hotel_offers_search import HotelOffersSearch
+from ._activities import Activities
+
+__all__ = ['FlightDates', 'FlightDestinations',
+           'HotelOffersSearch', 'HotelOfferSearch',
+           'FlightOffersSearch', 'Activities']
