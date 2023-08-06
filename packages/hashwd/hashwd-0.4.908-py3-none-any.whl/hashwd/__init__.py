e6e16b77ffd60caccb3c6ec8fa12c5802c0b01cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:06 +0000

    Add package hashwd/0.4.908/hashwd-0.4.908-py3-none-any.whl

---
+++ packages/hashwd/hashwd-0.4.908-py3-none-any.whl/hashwd/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+from pyperclip import copy
+from hashwd.hashwd import generate_password, clear_clipboard
+from hashwd.pass_defaults import DEFAULT_WORDS, DEFAULT_NUMBERS, DEFAULT_SYMBOLS, MIN_WORDS, MAX_WORDS, MIN_NUMBERS, MAX_NUMBERS, MIN_SYMBOLS, MAX_SYMBOLS, DICTIONARY_FILE
