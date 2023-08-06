2d6761a4ee1cbae90e63c4143ab103e3ad7ecdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:49 +0000

    Add package rhoknp/1.1.2/rhoknp-1.1.2-py3-none-any.whl

---
+++ packages/rhoknp/rhoknp-1.1.2-py3-none-any.whl/rhoknp/cohesion/coreference.py
@@ -0,0 +1,210 @@
+import logging
+from typing import TYPE_CHECKING, List, Optional, Set
+
+from rhoknp.cohesion.argument import ExophoraArgument
+from rhoknp.cohesion.exophora import ExophoraReferent
+
+if TYPE_CHECKING:
+    from rhoknp.units.base_phrase import BasePhrase
+
+logger = logging.getLogger(__name__)
+
+
+class Entity:
+    """共参照におけるエンティティ．
+
+    Args:
+        eid: エンティティ ID．
+        exophora_referent: 自身が外界照応の照応先に対応するなら照応先の種類. 対応しないなら None.
+    """
+
+    def __init__(self, eid: int, exophora_referent: Optional[ExophoraReferent] = None) -> None:
+        self.eid = eid  #: エンティティ ID．
+        self.exophora_referent = exophora_referent  #: 外界照応の照応先．対応するものがなければ None．
+        self.mentions: Set["BasePhrase"] = set()  #: このエンティティを参照するメンションの集合．
+        self.mentions_nonidentical: Set["BasePhrase"] = set()  #: このエンティティを≒関係で参照するメンションの集合．
+
+    @property
+    def mentions_all(self) -> Set["BasePhrase"]:
+        """nonidentical を含めたこのエンティティを参照する全てのメンションの集合．"""
+        return self.mentions | self.mentions_nonidentical
+
+    def add_mention(self, mention: "BasePhrase", nonidentical: bool = False) -> None:
+        """このエンティティを参照するメンションを追加．
+
+        Args:
+            mention: 追加対象のメンション．
+            nonidentical: メンションが nonidentical（"≒" 付きでアノテーションされている）なら True．
+
+        .. note::
+            identical なメンションが追加されたとき，すでに nonidentical なメンションとして登録されていたら，
+            identical なメンションとして上書きする．
+        """
+        if nonidentical:
+            if mention in self.mentions_all:
+                return
+            mention.entities_nonidentical.add(self)
+            self.mentions_nonidentical.add(mention)
+        else:
+            if mention in self.mentions_nonidentical:
+                self.remove_mention(mention)
+            mention.entities.add(self)
+            self.mentions.add(mention)
+
+    def remove_mention(self, mention: "BasePhrase") -> None:
+        """このエンティティを参照するメンションを削除．
+
+        Args:
+            mention: 削除対象のメンション．
+        """
+        if mention in self.mentions:
+            self.mentions.remove(mention)
+            mention.entities.remove(self)
+        if mention in self.mentions_nonidentical:
+            self.mentions_nonidentical.remove(mention)
+            mention.entities_nonidentical.remove(self)
+
+    def __str__(self) -> str:
+        if self.exophora_referent:
+            return str(self.exophora_referent)
+        if self.mentions:
+            return list(self.mentions)[0].__str__()
+        elif self.mentions_nonidentical:
+            return list(self.mentions_nonidentical)[0].__str__()
+        else:
+            return ""
+
+    def __repr__(self) -> str:
+        items = [repr(self.eid)]
+        items += [repr(m.text) for m in self.mentions]
+        items += [repr(m.text) for m in self.mentions_nonidentical]
+        if self.exophora_referent:
+            items.append(repr(self.exophora_referent))
+        return f"<{self.__module__}.{self.__class__.__name__}: {', '.join(items)}>"
+
+    def __hash__(self) -> int:
+        return hash(self.eid)
+
+
+class EntityManager:
+    """文書全体のエンティティを管理．"""
+
+    def __init__(self) -> None:
+        self.entities: List[Entity] = []  #: エンティティのリスト．
+
+    def get_or_create_entity(
+        self, exophora_referent: Optional[ExophoraReferent] = None, eid: Optional[int] = None
+    ) -> Entity:
+        """自身が参照するエンティティを作成．
+
+        exophora_referent が singleton entity だった場合を除き，新しく Entity のインスタンスを作成して返す．
+        singleton entity とは，「著者」や「不特定:人１」などの文書中に必ず一つしか存在しないような entity．
+        一方で，「不特定:人」や「不特定:物」は複数存在しうるので singleton entity ではない．
+        eid を指定しない場合，最後に作成した entity の次の eid を選択．
+
+        Args:
+            exophora_referent: 外界照応における照応先．対応するものがなければ None．
+            eid: エンティティ ID．None の場合自動で割り振る．
+
+        Returns:
+             Entity: 作成されたエンティティ．
+        """
+        if exophora_referent is not None and exophora_referent.is_singleton is True:
+            entities = [e for e in self.entities if exophora_referent == e.exophora_referent]
+            # すでに singleton entity が存在した場合，新しい entity は作らずにその entity を返す
+            if entities:
+                assert len(entities) == 1  # singleton entity が1つしかないことを保証
+                return entities[0]
+        eids: List[int] = [e.eid for e in self.entities]
+        if eid in eids:
+            _eid = eid
+            eid = max(eids) + 1
+            logger.warning(f"{_eid} is already used. use eid: {eid} instead.")
+        elif eid is None or eid < 0:
+            eid = max(eids) + 1 if eids else 0
+        entity = Entity(eid, exophora_referent=exophora_referent)
+        self.entities.append(entity)
+        return entity
+
+    def merge_entities(
+        self,
+        source_mention: "BasePhrase",
+        target_mention: Optional["BasePhrase"],
+        source_entity: Entity,
+        target_entity: Entity,
+        nonidentical: bool,
+    ) -> None:
+        """2つのエンティティをマージ．
+
+        source_mention と source_entity, target_mention と target_entity の間には mention が張られているが，
+        source と target 間には張られていないので，add_mention する．
+        source_entity と target_entity が同一のエンティティであり，exophor も同じか片方が None ならば target_entity の方を削除する．
+
+        Args:
+            source_mention: ソース側メンション．
+            target_mention: ターゲット側メンション．メンションが存在しない場合は None．
+            source_entity: ソース側エンティティ．
+            target_entity: ターゲット側エンティティ．
+            nonidentical: ソース側メンションとターゲット側メンションの関係が nonidentical なら True．
+        """
+        nonidentical_tgt = (target_mention is not None) and target_mention.is_nonidentical_to(target_entity)
+        if source_entity not in source_mention.entities_all:
+            return
+        nonidentical_src = source_mention.is_nonidentical_to(source_entity)
+        if source_entity is target_entity:
+            if not nonidentical:
+                # source_entity (target_entity), source_mention, target_mention の三角形のうち2辺が identical ならもう1辺も identical
+                if (not nonidentical_src) and nonidentical_tgt:
+                    assert target_mention is not None
+                    source_entity.add_mention(target_mention, nonidentical=False)
+                if nonidentical_src and (not nonidentical_tgt):
+                    source_entity.add_mention(source_mention, nonidentical=False)
+            return
+        if target_mention is not None:
+            source_entity.add_mention(target_mention, nonidentical=(nonidentical or nonidentical_src))
+        target_entity.add_mention(source_mention, nonidentical=(nonidentical or nonidentical_tgt))
+        # source_entity と target_entity が同一でない可能性が捨てきれない場合，target_entity は削除しない
+        if nonidentical_src or nonidentical or nonidentical_tgt:
+            return
+        # source_entity と target_entity が同一でも exophor が異なれば target_entity は削除しない
+        if (
+            source_entity.exophora_referent is not None
+            and target_entity.exophora_referent is not None
+            and source_entity.exophora_referent != target_entity.exophora_referent
+        ):
+            return
+        # 以下 target_entity を削除する準備
+        if source_entity.exophora_referent is None:
+            source_entity.exophora_referent = target_entity.exophora_referent
+        for tm in target_entity.mentions_all:
+            source_entity.add_mention(tm, nonidentical=tm.is_nonidentical_to(target_entity))
+        # argument も eid を持っているので eid が変わった場合はこちらも更新
+        pas_list = source_mention.document.pas_list
+        for arg in [arg for pas in pas_list for args in pas.get_all_arguments(relax=False).values() for arg in args]:
+            if isinstance(arg, ExophoraArgument) and arg.eid == target_entity.eid:
+                arg.eid = source_entity.eid
+        self.delete_entity(target_entity)  # delete target entity
+
+    def delete_entity(self, entity: Entity) -> None:
+        """エンティティを削除．
+
+        対象エンティティを `EntityManager` およびそのエンティティを参照するすべてのメンションから削除する．
+        エンティティIDには欠番が生まれる可能性がある．
+
+        Args:
+            entity: 削除対象のエンティティ．
+        """
+        if entity not in self.entities:
+            return
+        for mention in entity.mentions_all:
+            entity.remove_mention(mention)
+        self.entities.remove(entity)
+
+    def __repr__(self) -> str:
+        return f"<{self.__module__}.{self.__class__.__name__}: {len(self.entities)}>"
+
+    def __getitem__(self, eid: int) -> Entity:
+        es = [e for e in self.entities if e.eid == eid]
+        if len(es) == 0:
+            raise KeyError(f"entity ID: {eid} not found.")
+        return es[0]
---
+++ packages/rhoknp/rhoknp-1.1.2-py3-none-any.whl/rhoknp/units/base_phrase.py
@@ -0,0 +1,382 @@
+import logging
+import re
+
+try:
+    from functools import cached_property  # type: ignore
+except ImportError:
+    from cached_property import cached_property
+from typing import TYPE_CHECKING, Any, List, Optional, Set
+
+from rhoknp.cohesion.coreference import Entity
+from rhoknp.cohesion.exophora import ExophoraReferent
+from rhoknp.cohesion.pas import CaseInfoFormat, Pas
+from rhoknp.cohesion.predicate import Predicate
+from rhoknp.cohesion.rel import CASE_TYPES, COREF_TYPES, RelMode, RelTag, RelTagList
+from rhoknp.props.dependency import DepType
+from rhoknp.props.feature import FeatureDict
+from rhoknp.props.memo import MemoTag
+from rhoknp.units.morpheme import Morpheme
+from rhoknp.units.unit import Unit
+
+if TYPE_CHECKING:
+    from rhoknp.units.clause import Clause
+    from rhoknp.units.document import Document
+    from rhoknp.units.phrase import Phrase
+    from rhoknp.units.sentence import Sentence
+
+logger = logging.getLogger(__name__)
+
+
+class BasePhrase(Unit):
+    """基本句クラス．"""
+
+    PAT = re.compile(
+        rf"^\+( (?P<pid>-1|\d+)(?P<dtype>[{''.join(e.value for e in DepType)}]))?( {FeatureDict.PAT.pattern})?$"
+    )
+    count = 0
+
+    def __init__(
+        self,
+        parent_index: Optional[int],
+        dep_type: Optional[DepType],
+        features: Optional[FeatureDict] = None,
+        rel_tags: Optional[RelTagList] = None,
+        memo_tag: Optional[MemoTag] = None,
+    ) -> None:
+        super().__init__()
+
+        # parent unit
+        self._phrase: Optional["Phrase"] = None
+
+        # child units
+        self._morphemes: Optional[List[Morpheme]] = None
+
+        self.parent_index: Optional[int] = parent_index  #: 係り先の基本句の文内におけるインデックス．
+        self.dep_type: Optional[DepType] = dep_type  #: 係り受けの種類．
+        self.features: FeatureDict = features or FeatureDict()  #: 素性．
+        self.rel_tags: RelTagList = rel_tags or RelTagList()  #: 基本句間関係．
+        self.memo_tag: MemoTag = memo_tag or MemoTag()  #: タグ付けメモ．
+        self.pas: Optional["Pas"] = None  #: 述語項構造．
+        self.entities: Set[Entity] = set()  #: 参照しているエンティティ．
+        self.entities_nonidentical: Set[Entity] = set()  #: ≒で参照しているエンティティ．
+
+        self.index = self.count  #: 文内におけるインデックス．
+        BasePhrase.count += 1
+
+    def __post_init__(self) -> None:
+        super().__post_init__()
+
+        # Parse the PAS tag.
+        if "述語項構造" in self.features:
+            pas_string = self.features["述語項構造"]
+            assert isinstance(pas_string, str)
+            pas = Pas.from_pas_string(self, pas_string, format_=CaseInfoFormat.PAS)
+        elif "格解析結果" in self.features:
+            pas_string = self.features["格解析結果"]
+            assert isinstance(pas_string, str)
+            pas = Pas.from_pas_string(self, pas_string, format_=CaseInfoFormat.CASE)
+        else:
+            pas = Pas(Predicate(self))
+        self.pas = pas
+
+        # Parse the rel tag if this unit is a piece of a document.
+        if self.sentence.has_document is False:
+            logger.info("post-processing of rel tags was skipped because there is no document.")
+            return
+        for rel_tag in self.rel_tags:
+            if rel_tag.sid == "":
+                rel_tag = RelTag(
+                    type=rel_tag.type,
+                    target=rel_tag.target,
+                    sid=self.sentence.sid,  # The target is considered to be in the same sentence.
+                    base_phrase_index=rel_tag.base_phrase_index,
+                    mode=rel_tag.mode,
+                )
+            if rel_tag.type in CASE_TYPES:
+                self._add_pas(rel_tag)
+            elif rel_tag.type in COREF_TYPES and rel_tag.mode in (None, RelMode.AND):  # ignore "OR" and "?"
+                self._add_coreference(rel_tag)
+            else:
+                logger.warning(f"{rel_tag} is ignored.")
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, type(self)) is False:
+            return False
+        if self.parent_unit != other.parent_unit:
+            return False
+        return self.index == other.index
+
+    @cached_property
+    def global_index(self) -> int:
+        """文書全体におけるインデックス．"""
+        if self.index > 0:
+            return self.sentence.base_phrases[self.index - 1].global_index + 1
+        if self.sentence.index == 0:
+            return self.index
+        return self.document.sentences[self.sentence.index - 1].base_phrases[-1].global_index + 1
+
+    @property
+    def parent_unit(self) -> Optional["Phrase"]:
+        """上位の言語単位（文節）．未登録なら None．"""
+        return self._phrase
+
+    @property
+    def child_units(self) -> Optional[List[Morpheme]]:
+        """下位の言語単位（形態素）．解析結果にアクセスできないなら None．"""
+        return self._morphemes
+
+    @property
+    def document(self) -> "Document":
+        """文書．
+
+        Raises:
+            AttributeError: 解析結果にアクセスできない場合．
+        """
+        return self.phrase.document
+
+    @property
+    def sentence(self) -> "Sentence":
+        """文．"""
+        return self.phrase.sentence
+
+    @property
+    def clause(self) -> "Clause":
+        """節．
+
+        Raises:
+            AttributeError: 解析結果にアクセスできない場合．
+        """
+        return self.phrase.clause
+
+    @property
+    def phrase(self) -> "Phrase":
+        """文節．"""
+        assert self._phrase is not None
+        return self._phrase
+
+    @phrase.setter
+    def phrase(self, phrase: "Phrase") -> None:
+        """文節．
+
+        Args:
+            phrase: 文節．
+        """
+        self._phrase = phrase
+
+    @property
+    def morphemes(self) -> List[Morpheme]:
+        """形態素のリスト．"""
+        assert self._morphemes is not None
+        return self._morphemes
+
+    @morphemes.setter
+    def morphemes(self, morphemes: List[Morpheme]) -> None:
+        """形態素のリスト．
+
+        Args:
+            morphemes: 形態素のリスト．
+        """
+        for morpheme in morphemes:
+            morpheme.base_phrase = self
+        self._morphemes = morphemes
+
+    @property
+    def head(self) -> Morpheme:
+        """主辞の形態素．"""
+        feature_to_priority = {"内容語": 0, "準内容語": 1, "基本句-主辞": 2}
+        head = self.morphemes[0]
+        current_priority = -1
+        for morpheme in self.morphemes:
+            if not morpheme.features:
+                continue
+            for feature, priority in feature_to_priority.items():
+                if feature in morpheme.features and priority > current_priority:
+                    head = morpheme
+                    current_priority = priority
+        return head
+
+    @property
+    def parent(self) -> Optional["BasePhrase"]:
+        """係り先の基本句．ないなら None．
+
+        Raises:
+            AttributeError: 解析結果にアクセスできない場合．
+        """
+        if self.parent_index is None:
+            raise AttributeError("parent_index has not been set")
+        if self.parent_index == -1:
+            return None
+        return self.sentence.base_phrases[self.parent_index]
+
+    @cached_property
+    def children(self) -> List["BasePhrase"]:
+        """この基本句に係っている基本句のリスト．
+
+        Raises:
+            AttributeError: 解析結果にアクセスできない場合．
+        """
+        return [base_phrase for base_phrase in self.sentence.base_phrases if base_phrase.parent == self]
+
+    @property
+    def entities_all(self) -> Set[Entity]:
+        """nonidentical も含めた参照している全エンティティの集合．"""
+        return self.entities | self.entities_nonidentical
+
+    @classmethod
+    def from_knp(cls, knp_text: str) -> "BasePhrase":
+        """基本句クラスのインスタンスを KNP の解析結果から初期化．
+
+        Args:
+            knp_text: KNP の解析結果．
+        """
+        first_line, *lines = knp_text.split("\n")
+        match = cls.PAT.match(first_line)
+        if match is None:
+            raise ValueError(f"malformed base phrase line: {first_line}")
+        parent_index = int(match["pid"]) if match["pid"] is not None else None
+        dep_type = DepType(match["dtype"]) if match["dtype"] is not None else None
+        features = FeatureDict.from_fstring(match["feats"] or "")
+        rel_tags = RelTagList.from_fstring(match["feats"] or "")
+        memo_tag = MemoTag.from_fstring(match["feats"] or "")
+        base_phrase = cls(parent_index, dep_type, features, rel_tags, memo_tag)
+
+        morphemes: List[Morpheme] = []
+        for line in lines:
+            if line.strip() == "":
+                continue
+            morphemes.append(Morpheme.from_jumanpp(line))
+        base_phrase.morphemes = morphemes
+        return base_phrase
+
+    def to_knp(self) -> str:
+        """KNP フォーマットに変換．"""
+        ret = "+"
+        if self.parent_index is not None:
+            assert self.dep_type is not None
+            ret += f" {self.parent_index}{self.dep_type.value}"
+        if self.rel_tags or self.memo_tag or self.features:
+            ret += " "
+            ret += self.rel_tags.to_fstring()
+            if self.memo_tag:
+                ret += self.memo_tag.to_fstring()
+            ret += self.features.to_fstring()
+        ret += "\n"
+        ret += "".join(morpheme.to_knp() for morpheme in self.morphemes)
+        return ret
+
+    def get_coreferents(self, include_nonidentical: bool = False, include_self: bool = False) -> Set["BasePhrase"]:
+        """この基本句と共参照している基本句の集合を返却．
+
+        Args:
+            include_nonidentical: nonidentical なメンションを含めるなら True．
+            include_self: 自身を含めるなら True．
+
+        Returns:
+            共参照している基本句の集合．
+        """
+        mentions: Set["BasePhrase"] = set()
+        for entity in self.entities:
+            mentions.update(entity.mentions)
+        if include_nonidentical is True:
+            for entity in self.entities_nonidentical:
+                mentions.update(entity.mentions)
+        if include_self is False and self in mentions:
+            mentions.remove(self)
+        return mentions
+
+    def is_nonidentical_to(self, entity: Entity) -> bool:
+        """エンティティに対して自身が nonidentical な場合に True を返す．
+
+        Args:
+            entity: エンティティ．
+        """
+        if entity in self.entities:
+            return False
+        else:
+            assert entity in self.entities_nonidentical, f"non-referring entity: {entity}"
+            return True
+
+    def add_entity(self, entity: Entity, nonidentical: bool = False) -> None:
+        """エンティティを追加．
+
+        Args:
+            entity: 追加するエンティティ．
+            nonidentical: nonidentical なメンションなら True．
+        """
+        if nonidentical:
+            self.entities_nonidentical.add(entity)
+        else:
+            self.entities.add(entity)
+        entity.add_mention(self, nonidentical=nonidentical)
+
+    def _add_pas(self, rel_tag: RelTag) -> None:
+        """述語項構造を追加．"""
+        entity_manager = self.document.entity_manager
+        assert self.pas is not None
+        if rel_tag.sid is not None:
+            arg_base_phrase = self._get_target_base_phrase(rel_tag)
+            if arg_base_phrase is None:
+                return
+            if not arg_base_phrase.entities:
+                arg_base_phrase.add_entity(entity_manager.get_or_create_entity())
+            self.pas.add_argument(rel_tag.type, arg_base_phrase, mode=rel_tag.mode)
+        else:
+            if rel_tag.target == "なし":
+                self.pas.set_arguments_optional(rel_tag.type)
+                return
+            # exophora
+            entity = entity_manager.get_or_create_entity(ExophoraReferent(rel_tag.target))
+            self.pas.add_special_argument(rel_tag.type, rel_tag.target, eid=entity.eid, mode=rel_tag.mode)
+
+    def _add_coreference(self, rel_tag: RelTag) -> None:
+        """共参照関係を追加．"""
+        entity_manager = self.document.entity_manager
+        # create source entity
+        if not self.entities:
+            self.add_entity(entity_manager.get_or_create_entity())
+
+        nonidentical: bool = rel_tag.type.endswith("≒")
+        if rel_tag.sid is not None:
+            target_base_phrase = self._get_target_base_phrase(rel_tag)
+            if target_base_phrase is None:
+                return
+            if target_base_phrase == self:
+                logger.warning(f"{self.sentence.sid}: coreference with self found: {self}")
+                return
+            # create target entity
+            if not target_base_phrase.entities:
+                target_base_phrase.add_entity(entity_manager.get_or_create_entity())
+            for source_entity in self.entities_all:
+                for target_entity in target_base_phrase.entities_all:
+                    entity_manager.merge_entities(self, target_base_phrase, source_entity, target_entity, nonidentical)
+        else:
+            # exophora
+            for source_entity in self.entities_all:
+                target_entity = entity_manager.get_or_create_entity(exophora_referent=ExophoraReferent(rel_tag.target))
+                entity_manager.merge_entities(self, None, source_entity, target_entity, nonidentical)
+
+    def _get_target_base_phrase(self, rel_tag: RelTag) -> Optional["BasePhrase"]:
+        """rel が指す基本句を取得．"""
+        sentences = [sent for sent in self.document.sentences if sent.sid == rel_tag.sid]
+        if not sentences:
+            logger.warning(f"{self.sentence.sid}: relation with unknown sid found: {rel_tag.sid}")
+            return None
+        sentence = sentences[0]
+        assert rel_tag.base_phrase_index is not None
+        if rel_tag.base_phrase_index >= len(sentence.base_phrases):
+            logger.warning(f"{self.sentence.sid}: index out of range")
+            return None
+        target_base_phrase = sentence.base_phrases[rel_tag.base_phrase_index]
+        if not (set(rel_tag.target) & set(target_base_phrase.text)):
+            logger.warning(
+                f"{self.sentence.sid}: rel target mismatch; '{rel_tag.target}' vs '{target_base_phrase.text}'"
+            )
+        return target_base_phrase
+
+    def __hash__(self) -> int:
+        return hash((self.global_index, self.sentence.sid))
+
+    @staticmethod
+    def is_base_phrase_line(line: str) -> bool:
+        """基本句行なら True を返す．"""
+        return BasePhrase.PAT.match(line) is not None
