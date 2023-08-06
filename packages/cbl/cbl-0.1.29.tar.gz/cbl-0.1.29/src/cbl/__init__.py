085dba7ba0f0692a5a832d5e08f35feb2f73ca18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:08 +0000

    Add package cbl/0.1.29/cbl-0.1.29.tar.gz

---
+++ packages/cbl/cbl-0.1.29.tar.gz/cbl-0.1.29/src/cbl/__init__.py
@@ -0,0 +1,8 @@
+# info@codebasedlearning.dev, a.voss@fh-aachen.de
+
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))  # enables relative local imports
+
+#import versioning
+import info
---
+++ packages/cbl/cbl-0.1.29.tar.gz/cbl-0.1.29/src/cbl/info/info_impl.py
@@ -0,0 +1,84 @@
+# a.voss@fh-aachen.de
+
+import platform
+import sys
+import dataclasses
+
+#__version__ = "0.1.23"      # package version, also dynamically used by pyproject.toml
+#_name = "cbl"      # package name, cannot set dynamically same way, so cmp. with pyproject.toml
+
+#def _get_version():
+#    return __version
+
+@dataclasses.dataclass
+class PackageInfo:
+    name: str
+    version: str
+    summary: str
+
+import importlib.metadata
+#from importlib.metadata import version
+#print(version('cbl-tools'))
+#print(wh['Name'])
+#print(wh['Version'])
+
+wh = importlib.metadata.metadata('cbl')
+def package_info() -> PackageInfo:
+    return PackageInfo(wh['Name'], wh['Version'], wh['Summary'])
+
+
+@dataclasses.dataclass
+class PlatformInfo:
+    platform: str
+    machine: str
+    processor: str
+
+
+def platform_info() -> PlatformInfo:
+    return PlatformInfo(platform.platform(), platform.machine(), platform.processor())
+
+
+@dataclasses.dataclass
+class PythonInfo:
+    executable: str
+    version: str
+    implementation: str
+
+
+def python_info() -> PythonInfo:
+    return PythonInfo(sys.executable, platform.python_version(), platform.python_implementation())
+
+
+# @dataclasses.dataclass
+# class PackageInfo:
+#     full_name: str
+#     version: str
+#
+#
+# def package_info() -> PackageInfo:
+#     return PackageInfo("cbl_test_package.info", "2.0.2")  # "1.0.1"
+
+
+if __name__ == "__main__":
+    print(f"Platform Info: '{platform_info()}'")
+    print(f"Python Info: '{python_info()}'")
+    print(f"Package Info: '{package_info()}'")
+
+"""
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+https://choosealicense.com/licenses/mit/
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cbl_test_package==1.2.3
+
+### packages: build, twine
+
+python3 -m build  
+python3 -m twine upload --repository testpypi dist/* 
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cbl_test_package@1.2.3 
+
+
+python3 -m twine upload dist/* 
+python3 -m pip install cbl_test_package==1.2.3 
+pip install cbl-test-package==1.2.3
+pip install cbl-test-package==2.3.4
+"""
