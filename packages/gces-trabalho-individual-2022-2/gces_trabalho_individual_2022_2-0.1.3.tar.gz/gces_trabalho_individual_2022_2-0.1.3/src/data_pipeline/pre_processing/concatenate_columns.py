ab92e89802412740af46ff87e9de092facc38d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package gces-trabalho-individual-2022-2/0.1.3/gces_trabalho_individual_2022_2-0.1.3.tar.gz

---
+++ packages/gces-trabalho-individual-2022-2/gces_trabalho_individual_2022_2-0.1.3.tar.gz/gces_trabalho_individual_2022_2-0.1.3/src/data_pipeline/pre_processing/concatenate_columns.py
@@ -0,0 +1,16 @@
+from pandas import DataFrame
+
+
+def get_concatenated_column(csv, columns, column_name):
+    if isinstance(columns) == str:
+        csv[column_name] = csv[columns]
+        return DataFrame(csv[column_name])
+
+    for value in columns:
+        if not column_name in csv:
+            csv[column_name] = csv[value].astype(str)
+            continue
+
+        csv[column_name] = csv[column_name] + " " + csv[value].astype(str)
+
+    return DataFrame(csv[column_name])
---
+++ packages/gces-trabalho-individual-2022-2/gces_trabalho_individual_2022_2-0.1.3.tar.gz/gces_trabalho_individual_2022_2-0.1.3/src/parser/const.py
@@ -0,0 +1,1 @@
+model_type = {"ADDRESS": "address"}
