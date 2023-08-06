37ea9ee804501eb29b88b781aed98c1eb0fe4afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:21 +0000

    Add package mvvmQt/0.35.1/mvvmQt-0.35.1.tar.gz

---
+++ packages/mvvmQt/mvvmQt-0.35.1.tar.gz/mvvmQt-0.35.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+REQUIRED = ["pyquery", "qasync", "jinja2"]
+setup(
+    name = "mvvmQt" ,
+    version = "0.35.1" ,
+    description = "write qt like html" ,
+    author = "Norman",
+    author_email = "332535694@qq.com",
+    url = "https://gitee.com/zhiyang/py-qt-dom",
+    packages = find_packages(),
+    python_requires = '>=3.6.0',
+    install_requires = REQUIRED
+)
