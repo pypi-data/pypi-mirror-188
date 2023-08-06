07d9546234ae23162020041cd3ec255aa8d066d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:38 +0000

    Add package alpha-public-registry-grpc/1.0.3/alpha-public-registry-grpc-1.0.3.tar.gz

---
+++ packages/alpha-public-registry-grpc/alpha-public-registry-grpc-1.0.3.tar.gz/alpha-public-registry-grpc-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='alpha-public-registry-grpc',
+    version='1.0.3',
+    packages=[''],
+    url='https://gitlab.com/alphaticks/alpha-public-registry-grpc',
+    license='copyright',
+    author='Alphaticks',
+    description='gRPC code for alpha public registry client'
+)
