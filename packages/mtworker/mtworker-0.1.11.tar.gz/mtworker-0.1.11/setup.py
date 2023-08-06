5e7b7fa7f25feecf32a6a316e940943ee4519c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package mtworker/0.1.11/mtworker-0.1.11.tar.gz

---
+++ packages/mtworker/mtworker-0.1.11.tar.gz/mtworker-0.1.11/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mtworker', 'mtworker.cmds', 'mtworker.tasks']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['celery[redis]>=5.2.7,<6.0.0',
+ 'httpx>=0.23.3,<0.24.0',
+ 'playwright>=1.29.1,<2.0.0',
+ 'requests>=2.28.2,<3.0.0',
+ 'typer[all]>=0.7.0,<0.8.0']
+
+setup_kwargs = {
+    'name': 'mtworker',
+    'version': '0.1.11',
+    'description': '',
+    'long_description': '# Example Package\n',
+    'author': 'a',
+    'author_email': 'a@a.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
