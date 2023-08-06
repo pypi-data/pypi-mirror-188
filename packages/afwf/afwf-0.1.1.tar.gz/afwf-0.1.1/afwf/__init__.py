396d3c1812d53ede76ff3ea0493619016d6d8a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:23 +0000

    Add package afwf/0.1.1/afwf-0.1.1.tar.gz

---
+++ packages/afwf/afwf-0.1.1.tar.gz/afwf-0.1.1/afwf/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+"""
+Alfred Workflow Script Filter power tool.
+"""
+
+from ._version import __version__
+
+__short_description__ = "Alfred Workflow Script Filter power tool.."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+try:
+    from .item import (
+        Icon,
+        Text,
+        Item,
+        VarKeyEnum,
+        VarValueEnum,
+        ModEnum,
+    )
+    from .script_filter import ScriptFilter
+    from .handler import Handler
+    from .workflow import Workflow
+    from .icon import IconFileEnum
+except ImportError as e:
+    print(e)
