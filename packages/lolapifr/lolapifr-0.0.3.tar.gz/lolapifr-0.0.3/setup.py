9c71c6b6c442b1a3e0ebc78d0ec78eadddc5f542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:03 +0000

    Add package lolapifr/0.0.3/lolapifr-0.0.3.tar.gz

---
+++ packages/lolapifr/lolapifr-0.0.3.tar.gz/lolapifr-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'API de League of Legends en français'
+
+DOCU = 'tkt'
+
+setup(
+    name='lolapifr',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=DOCU,
+    author='Manolo',
+    author_email='emmanuelardoin@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests>2.28.0',
+        'python-dotenv>0.21.0',
+    ],
+)
