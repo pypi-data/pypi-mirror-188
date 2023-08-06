5edac374f4a3e5d76387b74a58a8786b5083cec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:07 +0000

    Add package robin-sd-upload/0.1.27/robin_sd_upload-0.1.27.tar.gz

---
+++ packages/robin-sd-upload/robin_sd_upload-0.1.27.tar.gz/robin_sd_upload-0.1.27/robin_sd_upload/push_software.py
@@ -0,0 +1,36 @@
+import os
+import requests
+import json
+from .get_bearer_token import get_bearer_token
+
+def push_software(config_file, fpath, radarType, version_name):
+    print('Getting the bearer token...')
+    api_url = config_file['api_url']
+    bearer_token = get_bearer_token(config_file)
+    headers = {
+        'Authorization': 'Bearer ' + bearer_token,
+    }
+    if not bearer_token:
+        return "Bearer token is empty"
+    else:
+        print('bearer_token: ', bearer_token)
+
+    # check if can open file path
+    if os.path.isfile(fpath):
+        print("ZIP file exists: " + fpath)
+    else:
+        return "ZIP not exist: " + fpath
+        
+    files = {
+        'file': (fpath, open(fpath, 'rb'))
+    }
+
+    values = {
+        'destination': json.dumps(radarType),
+        'versionName': json.dumps(version_name)
+    }
+    
+    print('Uploading software...')
+
+    response = requests.post(api_url+'/api/softwares/softwarefiles', headers=headers, data=values, files=files)
+    return response.json()
