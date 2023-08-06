493d77fd7bcf20476aefedbf2e485f0a7969501e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:54 +0000

    Add package lawLib/0.4.5/lawLib-0.4.5.tar.gz

---
+++ packages/lawLib/lawLib-0.4.5.tar.gz/lawLib-0.4.5/scrapper/data_scrapper.py
@@ -0,0 +1,8 @@
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+class DataScrapper:
+    def __init__(self, source) -> None:
+        self.source:str = {section: dict(config.items(section)) for section in config.sections()}['URLS'][source.lower()]
