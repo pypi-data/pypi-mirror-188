a3c38f50a54860a847b65310665c72a2b6f706e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:09 +0000

    Add package penn/0.0.1/penn-0.0.1-py3-none-any.whl

---
+++ packages/penn/penn-0.0.1-py3-none-any.whl/penn/voicing.py
@@ -0,0 +1,30 @@
+import torch
+
+import penn
+
+
+###############################################################################
+# Voiced/unvoiced
+###############################################################################
+
+
+def interpolate(pitch, periodicity, value=penn.DEFAULT_VOICING_THRESHOLD):
+    """Fill unvoiced regions via linear interpolation"""
+    # Threshold periodicity
+    voiced = threshold(periodicity, value)
+
+    # Pitch is linear in base-2 log-space
+    pitch = torch.log2(pitch)
+
+    # Interpolate
+    pitch[~voiced] = penn.interpolate(
+        torch.where(~voiced[0])[0][None],
+        torch.where(voiced[0])[0][None],
+        pitch[voiced][None])
+
+    return 2 ** pitch
+
+
+def threshold(periodicity, value=penn.DEFAULT_VOICING_THRESHOLD):
+    """Threshold periodicity to produce voiced/unvoiced classifications"""
+    return periodicity > value
