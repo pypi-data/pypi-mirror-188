7c895ab397680ad0d993ef9ee7a083f9a104c0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:15 +0000

    Add package gig-nuuuwan/3.0.6/gig-nuuuwan-3.0.6.tar.gz

---
+++ packages/gig-nuuuwan/gig-nuuuwan-3.0.6.tar.gz/gig-nuuuwan-3.0.6/setup.py
@@ -0,0 +1,34 @@
+"""Setup."""
+
+import setuptools
+
+DIST_NAME = "gig"
+VERSION = "3.0.6"
+setuptools.setup(
+    name="%s-nuuuwan" % DIST_NAME,
+    version=VERSION,
+    author="Nuwan I. Senaratna",
+    author_email="nuuuwan@gmail.com",
+    description="Generalized information graph.",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/nuuuwan/%s" % DIST_NAME,
+    project_urls={
+        "Bug Tracker": "https://github.com/nuuuwan/%s/issues" % DIST_NAME,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=[
+        "utils-nuuuwan",
+        "fuzzywuzzy",
+        "python-Levenshtein",
+    ],
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
