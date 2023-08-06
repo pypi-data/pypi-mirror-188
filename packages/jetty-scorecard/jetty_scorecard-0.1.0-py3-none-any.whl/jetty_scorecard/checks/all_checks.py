2822b9acec1cb8de4e994f255974d12aa311d124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:40 +0000

    Add package jetty-scorecard/0.1.0/jetty_scorecard-0.1.0-py3-none-any.whl

---
+++ packages/jetty-scorecard/jetty_scorecard-0.1.0-py3-none-any.whl/jetty_scorecard/checks/all_checks.py
@@ -0,0 +1,28 @@
+from jetty_scorecard import env
+from jetty_scorecard.checks import (
+    backup_account_admin,
+    has_network_policy,
+    shadow_future_grants,
+    overuse_of_admin_roles,
+    password_only_login,
+    inactive_users,
+)
+
+
+def register(env: env.SnowflakeEnvironment):
+    """Register the checks specified in the following list
+
+    Args:
+        env (env.SnowflakeEnvironment): Environment to register the checks with
+    """
+    check_list = [
+        overuse_of_admin_roles.create(),
+        backup_account_admin.create(),
+        shadow_future_grants.create(),
+        has_network_policy.create(),
+        password_only_login.create(),
+        inactive_users.create(),
+    ]
+
+    for check in check_list:
+        env.register_check(check)
