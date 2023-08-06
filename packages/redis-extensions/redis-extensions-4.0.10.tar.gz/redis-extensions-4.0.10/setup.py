7b616b543cdf797b99ac51c2acb210a55849ce1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:05 +0000

    Add package redis-extensions/4.0.10/redis-extensions-4.0.10.tar.gz

---
+++ packages/redis-extensions/redis-extensions-4.0.10.tar.gz/redis-extensions-4.0.10/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+version = '4.0.10'
+
+
+setup(
+    name='redis-extensions',
+    version=version,
+    keywords='Redis Extensions',
+    description='Redis-extensions is a collection of custom extensions for Redis-py.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/redisclub/redis-extensions-py',
+
+    author='Hackathon',
+    author_email='kimi.huang@brightcells.com',
+
+    packages=['redis_extensions'],
+    py_modules=[],
+    python_requires='>=3.6',
+    install_requires=['TimeConvert', 'redis>=4.0.2', 'shortuuid', 'verification-code', 'graphic-verification-code<=1.0.3'],
+
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
