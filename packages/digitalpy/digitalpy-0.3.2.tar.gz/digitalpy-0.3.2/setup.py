89e7b1663f20807566df7d2bf5d1d68e8d7e4f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:02 +0000

    Add package digitalpy/0.3.2/digitalpy-0.3.2.tar.gz

---
+++ packages/digitalpy/digitalpy-0.3.2.tar.gz/digitalpy-0.3.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='digitalpy',
+      version='0.3.2',
+      description="A python implementation of the aphrodite's specification, heavily based on WCMF",
+      author='Natha Paquette',
+      author_email='natha.paquette@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      install_requires=[
+            "rule-engine",
+            "pyzmq",
+      ],
+      extras_require={
+            "DEV": ["pytest"],
+      },
+      packages=find_packages(include=["digitalpy", "digitalpy.*", "*.json", "*.ini", "*.conf"])
+     )
