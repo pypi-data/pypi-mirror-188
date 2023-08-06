1844b4bb5794dc26805937df99afc4bd00871acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:40 +0000

    Add package example-jaeb/0.0.1/example_jaeb-0.0.1.tar.gz

---
+++ packages/example-jaeb/example_jaeb-0.0.1.tar.gz/example_jaeb-0.0.1/analysis/from_df.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def get_value_by_columns(df, column):
+    return df[column]
---
+++ packages/example-jaeb/example_jaeb-0.0.1.tar.gz/example_jaeb-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="example_jaeb",
+    version="0.0.1",
+    license="MIT",
+    author="abba",
+    author_email="jaebin9274@cogniterm.com",
+    description="sample",
+    long_description=open("README.md").read(),
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
