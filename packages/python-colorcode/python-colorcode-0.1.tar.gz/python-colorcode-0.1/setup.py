ae54b2e5616d6b4bc89323b66ab8f512d3d89079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:54 +0000

    Add package python-colorcode/0.1/python-colorcode-0.1.tar.gz

---
+++ packages/python-colorcode/python-colorcode-0.1.tar.gz/python-colorcode-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='python-colorcode',
+  version='0.1',
+  description='A sample Python package',
+  author='Anshu Pal',
+  author_email='anshupal257@gmail.com',
+  packages=['python-colorcode'],
+  install_requires=[
+    'webcolors',
+  ],
+)
