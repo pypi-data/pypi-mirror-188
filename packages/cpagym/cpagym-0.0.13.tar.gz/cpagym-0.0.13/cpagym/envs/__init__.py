9ee4b06edc3d2d20f81fb22a6edf9d73b77eba5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:45 +0000

    Add package cpagym/0.0.13/cpagym-0.0.13.tar.gz

---
+++ packages/cpagym/cpagym-0.0.13.tar.gz/cpagym-0.0.13/cpagym/envs/__init__.py
@@ -0,0 +1,24 @@
+from gym.envs.registration import register
+
+register(
+    id='Inventory-v0',
+    entry_point='cpagym.envs.grid_world:InventoryEnv',
+)
+
+register(
+    id='forex-v0',
+    entry_point='cpagym.envs.forex_env:ForexEnv',
+)
+
+register(
+    id='stocks-v0',
+    entry_point='cpagym.envs.stocks_env:StocksEnv',
+)
+register(
+    id='trading-v0',
+    entry_point='cpagym.envs.trading_env:TradingEnv',
+)
+register(
+    id='AsymmetricInfo',
+    entry_point='cpagym.envs.asymmetricInfo_env:AsymmetricInfoEnv',
+)
