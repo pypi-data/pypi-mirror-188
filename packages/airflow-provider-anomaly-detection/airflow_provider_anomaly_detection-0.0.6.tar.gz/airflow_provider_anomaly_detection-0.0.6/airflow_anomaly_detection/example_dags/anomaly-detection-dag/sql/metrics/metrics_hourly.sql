0c6e021caaa3840dcefb9e2c636961e081999cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:14 +0000

    Add package airflow-provider-anomaly-detection/0.0.6/airflow_provider_anomaly_detection-0.0.6.tar.gz

---
+++ packages/airflow-provider-anomaly-detection/airflow_provider_anomaly_detection-0.0.6.tar.gz/airflow_provider_anomaly_detection-0.0.6/airflow_anomaly_detection/example_dags/anomaly-detection-dag/sql/metrics/metrics_hourly.sql
@@ -0,0 +1,46 @@
+/*
+
+This example query generates a batch of metrics for the last hour.
+The metrics are generated randomly.
+
+The outputs of the query must be in the following format:
+- metric_timestamp: The timestamp of the metric.
+- metric_batch_name: The name of the batch of metrics.
+- metric_name: The name of the metric.
+- metric_value: The value of the metric.
+
+*/
+
+with
+
+frontend_events as
+(
+SELECT 
+  timestamp('{{ ts }}') as metric_timestamp,
+  'fe_pageviews_last1h' as metric_name,
+  rand()*1000 as metric_value 
+),
+
+backend_events as
+(
+SELECT 
+  timestamp('{{ ts }}') as metric_timestamp,
+  'be_events_last1h' as metric_name,
+  rand()*1000 as metric_value 
+),
+
+metrics_hourly as
+(
+select * from frontend_events
+union all
+select * from backend_events
+)
+
+select
+  metric_timestamp,
+  'metrics_hourly' as metric_batch_name,
+  metric_name,
+  safe_cast(metric_value as float64) as metric_value 
+from 
+  metrics_hourly
+;
