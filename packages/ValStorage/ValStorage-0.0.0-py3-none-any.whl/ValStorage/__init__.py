2522ae802e85b1d0f13cbd562f262834a9c8b783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:11 +0000

    Add package ValStorage/0.0.0/ValStorage-0.0.0-py3-none-any.whl

---
+++ packages/ValStorage/ValStorage-0.0.0-py3-none-any.whl/ValStorage/__init__.py
@@ -0,0 +1,9 @@
+from .settings import get_settings
+from .storage import *
+
+__all__ = [
+    "save_to_drive", "read_from_drive",
+    "json_write", "json_read",
+    "create_path", "list_dir", "utils_path",
+    "get_settings",
+]
