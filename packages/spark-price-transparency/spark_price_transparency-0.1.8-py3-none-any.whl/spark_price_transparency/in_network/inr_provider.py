181635ce3fd5242df5584c8e3bc2d9e9db5cacea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:35 +0000

    Add package spark-price-transparency/0.1.8/spark_price_transparency-0.1.8-py3-none-any.whl

---
+++ packages/spark-price-transparency/spark_price_transparency-0.1.8-py3-none-any.whl/spark_price_transparency/in_network/inr_provider.py
@@ -0,0 +1,18 @@
+from ..table_stream_tgt import TableStreamTgt
+from pyspark.sql.types import ArrayType, StructType, StructField, StringType, LongType
+
+
+class Inr_provider(TableStreamTgt):
+
+    provider_groups = ArrayType(StructType([
+                                  StructField("npi", ArrayType(StringType()), False),
+                                  StructField("tin", StructType([
+                                    StructField("type", StringType(), False),
+                                    StructField("value", StringType(), False)]), False)]))
+
+    definition = \
+        [("file_name",         StringType(),    False, "File name of in network rate json"),
+         ("batch_id",          LongType(),      True,  "Streaming ingest batchId"),
+         ("provider_group_id", LongType(),      False, "Publisher defined id reference code"),
+         ("provider_groups",   provider_groups, True,  "Group of providers as organized by publisher"),
+         ("location",          StringType(),    True,  "URL of download if not provided in provider_groups")]
