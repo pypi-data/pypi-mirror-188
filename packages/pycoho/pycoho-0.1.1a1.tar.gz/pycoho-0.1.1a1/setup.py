d3b3c11fc6e255217b73f39c504896c75bfbbbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:42 +0000

    Add package pycoho/0.1.1a1/pycoho-0.1.1a1.tar.gz

---
+++ packages/pycoho/pycoho-0.1.1a1.tar.gz/pycoho-0.1.1a1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pycoho']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'pycoho',
+    'version': '0.1.1a1',
+    'description': 'A Python wrapper for the UK Companies House API',
+    'long_description': '# pycoho\nA Python wrapper for the UK Companies House API\n',
+    'author': 'Armand Rego',
+    'author_email': 'armand@weirdsheeplabs.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/regoawt/pycoho',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
