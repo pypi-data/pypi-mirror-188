ca099ddd3a3a67f1faa165e846ef58ee92659c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:31 +0000

    Add package PyQt6-Fluent-Widgets/0.0.1/PyQt6-Fluent-Widgets-0.0.1.tar.gz

---
+++ packages/PyQt6-Fluent-Widgets/PyQt6-Fluent-Widgets-0.0.1.tar.gz/PyQt6-Fluent-Widgets-0.0.1/qfluentwidgets/__init__.py
@@ -0,0 +1,17 @@
+"""
+PyQt6-Fluent-Widgets
+===================
+A fluent design widgets library based on PyQt6.
+
+Documentation is available in the docstrings and
+online at https://pyqt-fluent-widgets.readthedocs.io.
+
+:copyright: (c) 2023 by zhiyiYo.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.0.1"
+
+from .components import *
+from .common import *
+from ._rc import resource
