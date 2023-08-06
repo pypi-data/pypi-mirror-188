169798ee7e61517c63b9a4a8a12247d8304daee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:07 +0000

    Add package apispec-pydantic-plugin/0.2.0/apispec_pydantic_plugin-0.2.0.tar.gz

---
+++ packages/apispec-pydantic-plugin/apispec_pydantic_plugin-0.2.0.tar.gz/apispec_pydantic_plugin-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['apispec_pydantic_plugin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['apispec>=6.0.2,<7', 'pydantic>=1.10.4,<2']
+
+setup_kwargs = {
+    'name': 'apispec-pydantic-plugin',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Kevin Kirsche',
+    'author_email': 'kevin.kirsche@one.verizon.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
