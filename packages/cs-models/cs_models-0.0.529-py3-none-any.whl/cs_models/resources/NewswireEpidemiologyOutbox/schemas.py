e18747f44b1ba9181fad11b738b6703d248edd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:45 +0000

    Add package cs-models/0.0.529/cs_models-0.0.529-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.529-py3-none-any.whl/cs_models/resources/NewswireEpidemiologyOutbox/schemas.py
@@ -0,0 +1,13 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class NewswireEpidemiologyOutboxResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    news_id = fields.Integer(required=True)
+    updated_at = fields.DateTime()
