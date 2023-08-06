4838b6ab5d1dd97e7d9aabee618165d963a5817f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:09 +0000

    Add package async-extensions/1.1.0/async_extensions-1.1.0.tar.gz

---
+++ packages/async-extensions/async_extensions-1.1.0.tar.gz/async_extensions-1.1.0/async_extensions/__init__.py
@@ -0,0 +1,9 @@
+"""Asynchronous extensions."""
+
+__description__ = "Asynchronous extensions."
+__url__ = "https://github.com/nekitdev/async-extensions"
+
+__title__ = "async_extensions"
+__author__ = "nekitdev"
+__license__ = "MIT"
+__version__ = "1.1.0"
