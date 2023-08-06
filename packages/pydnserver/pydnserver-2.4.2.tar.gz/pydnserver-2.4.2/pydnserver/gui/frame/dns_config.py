6ffa555f2758cb88297a509c02c29f651270e870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:47 +0000

    Add package pydnserver/2.4.2/pydnserver-2.4.2.tar.gz

---
+++ packages/pydnserver/pydnserver-2.4.2.tar.gz/pydnserver-2.4.2/pydnserver/gui/frame/dns_config.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+from uiutil.tk_names import EW, HORIZONTAL
+from uiutil import BaseLabelFrame, Button
+from ..window.forwarders import ForwarderConfigWindow
+from ..window.zone import ZoneConfigWindow
+
+
+class DNSConfigFrame(BaseLabelFrame):
+
+    BUTTON_WIDTH = 12
+
+    def __init__(self,
+                 title=u'DNS Config:',
+                 zone_address_list=None,
+                 *args,
+                 **kwargs):
+
+        self.zone_address_list = zone_address_list
+
+        super(DNSConfigFrame, self).__init__(title=title,
+                                             layout=HORIZONTAL,
+                                             *args,
+                                             **kwargs)
+
+        Button(text=u"Forwarders",
+               width=self.BUTTON_WIDTH,
+               sticky=EW,
+               command=self.launch_forwarder_config)
+
+        Button(text=u"Zone",
+               width=self.BUTTON_WIDTH,
+               sticky=EW,
+               command=self.launch_zone_config)
+
+        self.nice_grid()
+
+    def launch_forwarder_config(self):
+        ForwarderConfigWindow(fixed=True,
+                              parent_geometry=self.parent.winfo_toplevel().winfo_geometry())
+
+    def launch_zone_config(self):
+        ZoneConfigWindow(fixed=True,
+                         parent_geometry=self.parent.winfo_toplevel().winfo_geometry(),
+                         address_list=self.zone_address_list)
---
+++ packages/pydnserver/pydnserver-2.4.2.tar.gz/pydnserver-2.4.2/pydnserver/gui/frame/server.py
@@ -0,0 +1,69 @@
+# encoding: utf-8
+
+import logging
+from uiutil import Position, BaseFrame, Button, Separator
+from uiutil.tk_names import NORMAL, EW, showerror, HORIZONTAL
+from ...dns_server import DNSServer
+from ..frame.dns_config import DNSConfigFrame
+from ..._constants import (START_SERVERS,
+                           STOP_SERVERS)
+
+
+class DNSServerFrame(BaseFrame):
+    BUTTON_WIDTH = 20
+
+    def __init__(self,
+                 server=DNSServer,
+                 server_kwargs=None,
+                 zone_address_list=None,
+                 *args,
+                 **kwargs):
+
+        BaseFrame.__init__(self,
+                           layout=HORIZONTAL,
+                           *args,
+                           **kwargs)
+
+        if server_kwargs is None:
+            server_kwargs = {}
+
+        self.server = server(**server_kwargs)
+
+        self.dns_config = DNSConfigFrame(row=Position.NEXT,
+                                         sticky=EW,
+                                         zone_address_list=zone_address_list)
+
+        Separator()
+
+        self.start_stop_button = Button(state=NORMAL,
+                                        value=START_SERVERS,
+                                        width=self.BUTTON_WIDTH,
+                                        command=self.start_stop,
+                                        row=Position.NEXT,
+                                        sticky=EW,
+                                        tooltip=u'')
+
+    def start_stop(self):
+        if self.start_stop_button.value == START_SERVERS:
+            self.start()
+        else:
+            self.stop()
+
+    def start(self):
+
+        try:
+            self.server.start()
+            self.start_stop_button.value = STOP_SERVERS
+
+        except Exception as err:
+            logging.exception(err)
+            logging.fatal(u'Unexpected Exception.')
+
+            showerror(title=u'Server startup failed!',
+                      message=u'Failed to start DNS Server: {err}'.format(err=err))
+
+            self.stop()
+
+    def stop(self):
+        self.server.stop()
+        self.start_stop_button.value = START_SERVERS
