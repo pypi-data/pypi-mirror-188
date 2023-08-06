026ebeb8eee2768f453a62741a759336c340978e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:50 +0000

    Add package quantum-lab/0.1.1/quantum_lab-0.1.1-py3-none-any.whl

---
+++ packages/quantum-lab/quantum_lab-0.1.1-py3-none-any.whl/quantum_lab/jobs.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+def create(userID, uploadType, shot, resourceId, filePath, volumeName):
+    url = "http://sdt-api.qcc.svc.cluster.local"
+    url_path = f"{url}/qcc/job"
+
+    headers = {
+        "Content-Type": "application/json",
+        "email": userID,
+    }
+
+    data = json.dumps({
+        "uploadType": uploadType,
+        "shot": shot,
+        "resourceId": resourceId,
+        "filePath": filePath,
+        "volumeName": volumeName
+    })
+
+    response = requests.post(f"{url_path}", headers=headers, data=data) 
+
+    print("[Info] Created job / ",response.text, flush=True)
+
+    return response.text
+
+def getList(url):
+    url_path = f"{url}/qcc/jobs"
+
+    # params = {
+    #     "volumeName": volumeName,
+    #     "hostPath": hostPath
+    # }
+
+    response = requests.get(f"{url_path}") # , params=params)
+
+    print("[Job LIST] ", response.text, flush=True)
+
+    return response.text
+
+# def getJob():
+# def delete():
+
