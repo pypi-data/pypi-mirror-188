a9c98d274694d8aca7947cde8c0b49fe6eec4c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:33 +0000

    Add package genr/0.0.2/Genr-0.0.2.tar.gz

---
+++ packages/genr/Genr-0.0.2.tar.gz/Genr-0.0.2/Genr/test.py
@@ -0,0 +1,18 @@
+from openai_wrapper import OpenAIWrapper
+
+
+#With default parameters
+
+api_key = "<API-KEY>"
+openai = OpenAIWrapper(api_key)
+text = "OpenAI is an artificial intelligence research laboratory consisting of the for-profit OpenAI LP and its parent company, the non-profit OpenAI Inc. The company is dedicated to promoting and developing friendly AI in a way that benefits humanity as a whole."
+summary = openai.get_summary(text)
+# print(text)
+
+#With additional parameters
+
+# api_key = "<API-KEY>"
+# openai = OpenAIWrapper(api_key)
+# text = "OpenAI is an artificial intelligence research laboratory consisting of the for-profit OpenAI LP and its parent company, the non-profit OpenAI Inc. The company is dedicated to promoting and developing friendly AI in a way that benefits humanity as a whole."
+# summary = openai.get_summary(text, temperature=0.7, max_tokens=120, model="text-davinci-003")
+# print(text)
