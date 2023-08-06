74b669667a1ec5f3c93d4ec617f99e7e40f4e137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:36 +0000

    Add package pandalchemize/0.1.1/pandalchemize-0.1.1-py3-none-any.whl

---
+++ packages/pandalchemize/pandalchemize-0.1.1-py3-none-any.whl/pandalchemize/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.1'
+
+from pandalchemize.sqldataframe import SqlDataFrame, read_sql
+from pandalchemize.sqltable import SqlTable, read_sqltable, read_sqltable_dataframe
+from pandalchemize.records import insert_record, insert_records
