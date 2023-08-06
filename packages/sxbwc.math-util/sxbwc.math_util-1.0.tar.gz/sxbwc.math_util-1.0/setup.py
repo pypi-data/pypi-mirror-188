78166b2b19b2cd02c6c3484119dcfdc67d39de86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:14 +0000

    Add package sxbwc.math-util/1.0/sxbwc.math_util-1.0.tar.gz

---
+++ packages/sxbwc.math-util/sxbwc.math_util-1.0.tar.gz/sxbwc.math_util-1.0/setup.py
@@ -0,0 +1,14 @@
+# encoding=utf-8
+from distutils.core import setup
+
+setup(
+    name="sxbwc.math_util",
+    version="1.0",
+    description="山西博微创数学计算工具包",
+    author="rongweifeng",
+    author_email="65489942@qq.com",
+    py_modules=[
+        'math_util.demo1',
+        'math_util.demo2',
+    ]
+)
