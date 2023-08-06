74f79aeca59122a890a50102234837ba232d28d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:08 +0000

    Add package ssoss/0.3.1/ssoss-0.3.1-py3-none-any.whl

---
+++ packages/ssoss/ssoss-0.3.1-py3-none-any.whl/ssoss/__init__.py
@@ -0,0 +1,6 @@
+from ssoss.dynamic_road_object import *
+from ssoss.motion_road_object import *
+from ssoss.process_road_objects import *
+from ssoss.process_video import *
+from ssoss.static_road_object import *
+
