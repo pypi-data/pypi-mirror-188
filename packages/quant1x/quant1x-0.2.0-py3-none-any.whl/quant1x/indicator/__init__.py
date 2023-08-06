9128a4d2d01c019c17ba0bfd06da9512de00a615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:10 +0000

    Add package quant1x/0.2.0/quant1x-0.2.0-py3-none-any.whl

---
+++ packages/quant1x/quant1x-0.2.0-py3-none-any.whl/quant1x/indicator/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from quant1x.indicator.f89k import *
+from quant1x.indicator.ma1x import *
+from quant1x.indicator.hjk import *
+from quant1x.indicator.cdtd import *
+from quant1x.indicator.p2fb import *
