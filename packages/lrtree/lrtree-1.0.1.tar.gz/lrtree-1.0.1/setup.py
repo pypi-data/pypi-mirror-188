3429d8fefad310eccbc34f2d95b02706396d2af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:37 +0000

    Add package lrtree/1.0.1/lrtree-1.0.1.tar.gz

---
+++ packages/lrtree/lrtree-1.0.1.tar.gz/lrtree-1.0.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import os
+import codecs
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def find_version(*file_paths):
+    with codecs.open(os.path.join(here, *file_paths), 'r') as fp:
+        version_file = fp.read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+with open('requirements.txt') as fp:
+    install_requires = [x.split("/")[-1] for x in fp.read().splitlines()[1:]]
+
+with open('requirements-dev.txt') as fp:
+    install_requires_dev = [x.split("/")[-1] for x in fp.read().splitlines()[1:]]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='lrtree',
+      version=find_version("lrtree", "__init__.py"),
+      description='lrtree: logistic regression trees',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://adimajo.github.io/lrtree",
+      packages=find_packages(exclude=["contrib", "docs", "tests", "examples", "venv"]),
+      author='Adrien Ehrhardt, Dmitry Gaynullin, Elise Bayraktar',
+      author_email='Groupe-recherche-operationnelle.GRO@credit-agricole-sa.fr',
+      install_requires=install_requires,
+      test_suite="pytest-runner",
+      test_requires=install_requires_dev,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+      ],
+      # scripts=["scripts/fit_data_reelles.py",
+      #          "scripts/traitement_data.py",
+      #          "scripts/validation.py"]
+      )
