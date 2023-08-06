36267c0c67dc61902fac49d73daf7705f814416a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package timetrans/2.0.1/timetrans-2.0.1.tar.gz

---
+++ packages/timetrans/timetrans-2.0.1.tar.gz/timetrans-2.0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['timetrans']
+install_requires = \
+['googletrans==3.1.0a0']
+
+setup_kwargs = {
+    'name': 'timetrans',
+    'version': '2.0.1',
+    'description': 'Time trans to transfer to a normal date and clearance ',
+    'long_description': 'Приветствую всёх наших любителей Python, думаю многим из вас надоедало то что когда вы получаете время используя Datetime мы получаем не особо красивые текста, что-же я решыл исправить это',
+    'author': 'Sad_Cat0326',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
