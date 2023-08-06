e5c049865e09cb4dc6fca0829d33cbc7787726f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:26 +0000

    Add package dls-utilpack/1.0.0/dls-utilpack-1.0.0.tar.gz

---
+++ packages/dls-utilpack/dls-utilpack-1.0.0.tar.gz/dls-utilpack-1.0.0/.dae-devops/prepare_git_dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ********** Please don't edit this file!
+# ********** It has been generated automatically by dae_devops version 0.4.0.
+# ********** For repository_name dls-utilpack
+
+me=${BASH_SOURCE}
+echo "${me}: installing 0 dependencies for repository_name dls-utilpack"
+
+function __install {
+    echo ${me}: "$@"
+    "$@"
+}
+
+
+# dae_devops_fingerprint adf670ea8d6c81160879488aaba2c112
