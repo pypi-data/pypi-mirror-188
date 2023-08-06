deb9fe921d0ade8ff07d6b6dcfd434e175a08a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:09 +0000

    Add package chatGPT-debugger/0.0.2/ChatGPT_debugger-0.0.2.tar.gz

---
+++ packages/chatGPT-debugger/ChatGPT_debugger-0.0.2.tar.gz/ChatGPT_debugger-0.0.2/chatGPT_debugger/callAPI.py
@@ -0,0 +1,27 @@
+import inspect
+import os
+import keyring
+from engine import get_response 
+from store_api_key import store_api_key
+
+def call_chatGPT(func,line_number,e):
+    save_permenant =  os.environ.get("save_flag")
+    source = inspect.getsource(func)
+    ques = f"""I'm Stuggling with an Error in line {line_number}, the error says \n {e}. and Here is the code 
+    {source} \n\n
+    please provide the answer with these informations:
+        01. why this error occurs (Note: Give Indepth Explanation)
+        02. how to fix this error
+        03. Corrected Code
+        
+    Also when you submitting the output don't write my notes again
+    """
+    ques = ques.replace("@debug" ,"")
+    
+    api_key,save_permenant = store_api_key()
+        
+    response = get_response(ques,api_key,save_permenant)
+
+    print("_" * 100)
+    return f"\033[32m{response}\033[0m"
+    print(f"_" * 100)
