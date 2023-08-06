34288abf1d397aa89a0ded9205934383c5c854d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:56 +0000

    Add package dfcon/0.0.1/dfcon-0.0.1-py3-none-any.whl

---
+++ packages/dfcon/dfcon-0.0.1-py3-none-any.whl/dfcon/__init__.py
@@ -0,0 +1,14 @@
+"""Initialize published package."""
+
+from .src.dfcon_filter import *
+from .src.directory import *
+from .src.filters import *
+
+__copyright__ = "Copyright (C) 2022 Tamon Mikawa"
+__version__ = "0.0.1"
+__license__ = "MIT License"
+__author__ = "Tamon Mikawa"
+__author_email__ = "mtamon.engineering@gmail.com"
+__url__ = "https://github.com/MTamon/dataFileController.git"
+
+__all__ = ["src.dfcon_filter", "src.directory", "src.filters"]
---
+++ packages/dfcon/dfcon-0.0.1-py3-none-any.whl/dfcon/src/__init__.py
@@ -0,0 +1,5 @@
+"""__init__.py"""
+
+from .dfcon_filter import DircFilter, FileFilter
+from .filters import Filter, OverlapedFilter, TiledFilter
+from .directory import Directory
