fbcca682bffe26d9ff0bcf5f047e344ae6064de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package eracore/0.0.1/eracore-0.0.1.tar.gz

---
+++ packages/eracore/eracore-0.0.1.tar.gz/eracore-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eracore']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'eracore',
+    'version': '0.0.1',
+    'description': 'Core',
+    'long_description': '# EraCore\n\n### This is a core for my package\n',
+    'author': 'Eragod',
+    'author_email': 'eragod1337@mail.ru',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
