c9739b27129e2486f5d8170157705ff83e1579d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:25 +0000

    Add package tagesschauscraper/0.1.0/tagesschauscraper-0.1.0.tar.gz

---
+++ packages/tagesschauscraper/tagesschauscraper-0.1.0.tar.gz/tagesschauscraper-0.1.0/tagesschauscraper/constants.py
@@ -0,0 +1,18 @@
+german_month_names = [
+    "",
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember",
+]
+
+data_dir = "data"
+log_dir = "logs"
