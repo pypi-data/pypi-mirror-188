83d3d8230dcc5cd555b4ae6a2339857c58446d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:11 +0000

    Add package csnlp/1.4.3/csnlp-1.4.3-py3-none-any.whl

---
+++ packages/csnlp/csnlp-1.4.3-py3-none-any.whl/csnlp/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Nlp", "MultistartNlp", "Solution", "scaling"]
+
+import csnlp.core.scaling as scaling
+from csnlp.core.solutions import Solution
+from csnlp.nlps.multistart_nlp import MultistartNlp
+from csnlp.nlps.nlp import Nlp
