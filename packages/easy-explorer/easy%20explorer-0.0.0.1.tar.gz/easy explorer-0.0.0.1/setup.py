6d8505e54cae88cb1373e6ae2c92b93126739e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:40 +0000

    Add package easy-explorer/0.0.0.1/easy%20explorer-0.0.0.1.tar.gz

---
+++ packages/easy-explorer/easy%20explorer-0.0.0.1.tar.gz/easy explorer-0.0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='easy explorer',
+    version='0.0.0.1',
+    packages=[''],
+    url='https://www.github.com/',
+    license='MIT',
+    author='Yvan',
+    author_email='yvan_chen2022@163.com',
+    description='easy explorer.'
+)
