cce3609d3cd2ffa3ff71c359ab21d3cf0a0eb0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:58 +0000

    Add package board-to-fen/0.0.25/board_to_fen-0.0.25.tar.gz

---
+++ packages/board-to-fen/board_to_fen-0.0.25.tar.gz/board_to_fen-0.0.25/board_to_fen/predict.py
@@ -0,0 +1,21 @@
+import io
+from .KerasNeuralNetwork import KerasNeuralNetwork
+from .utils import Decoder_FEN, Tiler
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+from board_to_fen import saved_models
+
+
+def get_fen_from_image(image_path, end_of_row='/', black_view=False) -> str:
+    decoder = Decoder_FEN()
+    net = KerasNeuralNetwork()
+    f = pkg_resources.open_text(saved_models, 'november_model')
+    net.load_model(f.name)
+    tiler = Tiler()
+    tiles = tiler.get_tiles(image_path=image_path)
+    predictions = net.predict(tiles=tiles)
+    fen = decoder.fen_decode(squares=predictions, end_of_row=end_of_row, black_view=black_view)
+    return fen
