fd57c1c3f33e45bfc949e3285e3d05e785078ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:20 +0000

    Add package fenics-basix/0.6.0/fenics_basix-0.6.0-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/fenics-basix/fenics_basix-0.6.0-cp38-cp38-macosx_11_0_arm64.whl/basix/include/basix/version.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Chris Richardson
+// FEniCS Project
+// SPDX-License-Identifier:    MIT
+
+#pragma once
+
+#define BASIX_VERSION 0.6.0
+#define BASIX_VERSION_MAJOR 0
+#define BASIX_VERSION_MINOR 6
+#define BASIX_VERSION_PATCH 0
