c974c395c9e44cf0e7349fd4c822cc09564352a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:44 +0000

    Add package shasha4/0.0.0.6/shasha4-0.0.0.6.tar.gz

---
+++ packages/shasha4/shasha4-0.0.0.6.tar.gz/shasha4-0.0.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shasha4',
+    version='0.0.0.6',
+    description='For Scraping Naver Finance',
+    url='https://github.com/MoonsRainbow/shasha4.git',
+    author='MoonsRainbow',
+    author_email='Moons_Rainbow@kakao.com',
+    license='MoonsRainbow',
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'tqdm',
+        'bs4',
+        'pandas',
+        'konlpy',
+        'wordcloud',
+        'matplotlib',
+        'numpy'
+    ]
+)
---
+++ packages/shasha4/shasha4-0.0.0.6.tar.gz/shasha4-0.0.0.6/src/shasha4/__init__.py
@@ -0,0 +1,19 @@
+from .KimHeeYeon import *
+from .YangHeeKwon import *
+from .Yong import *
+from .Jun import *
+
+__all__ = [
+    'fn_page_scrapping',
+    'fn_get_content',
+    'fn_most_viewed_news',
+    'wc1_get_noun',
+    'wc1_make_wc',
+    'wc2_get_noun',
+    'wc2_make_wc',
+    'Upjong',
+    'Theme',
+    'Groupsa',
+    'get_top_stock',
+    'get_stock_sise'
+]
