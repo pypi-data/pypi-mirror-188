aff2fffb8230ec87a4b1bc062efb1931aee2c03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:58 +0000

    Add package overssh/0.0.1/overssh-0.0.1-py3-none-any.whl

---
+++ packages/overssh/overssh-0.0.1-py3-none-any.whl/overssh/auth.pyi
@@ -0,0 +1,24 @@
+from .aliases import PathLike
+from _typeshed import Incomplete
+from typing import NamedTuple, Optional
+
+LikeSSHAuth: Incomplete
+
+class SSHAuth(NamedTuple):
+    hostaddr: str
+    hostport: int
+    hostname: Optional[str]
+    username: Optional[str]
+    password: Optional[str]
+    identity: Optional[str]
+    identity_password: Optional[str]
+    @property
+    def socket(self): ...
+    @property
+    def destination(self): ...
+    def enter_password(self, password: str) -> SSHAuth: ...
+    def enter_identity_password(self, password: str) -> SSHAuth: ...
+    @staticmethod
+    def from_file(hostname: str, path: Optional[PathLike] = ...) -> SSHAuth: ...
+    @staticmethod
+    def cast(auth: LikeSSHAuth) -> SSHAuth: ...
