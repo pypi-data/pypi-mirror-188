5490429b43d4ef70f4e95d54714c41f76f102e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:15 +0000

    Add package netbox-storage/0.0.0.0.432/netbox_storage-0.0.0.0.432.tar.gz

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.432.tar.gz/netbox_storage-0.0.0.0.432/netbox_storage/views/volume_group.py
@@ -0,0 +1,79 @@
+from netbox.views import generic
+
+from netbox_storage.filters import VolumeGroupFilter, PhysicalVolumeFilter
+from netbox_storage.forms import (
+    VolumeGroupImportForm,
+    VolumeGroupFilterForm,
+    VolumeGroupForm,
+    VolumeGroupBulkEditForm
+)
+
+from netbox_storage.models import VolumeGroup, PhysicalVolume
+from netbox_storage.tables import VolumeGroupTable, PhysicalVolumeTable
+from utilities.views import register_model_view, ViewTab
+
+
+class VolumeGroupListView(generic.ObjectListView):
+    queryset = VolumeGroup.objects.all()
+    filterset = VolumeGroupFilter
+    filterset_form = VolumeGroupFilterForm
+    table = VolumeGroupTable
+
+
+class VolumeGroupView(generic.ObjectView):
+    """Display VolumeGroup details"""
+
+    queryset = VolumeGroup.objects.all()
+
+
+class VolumeGroupEditView(generic.ObjectEditView):
+    """View for editing a VolumeGroup instance."""
+
+    queryset = VolumeGroup.objects.all()
+    form = VolumeGroupForm
+    default_return_url = "plugins:netbox_storage:volumegroup_list"
+
+
+class VolumeGroupDeleteView(generic.ObjectDeleteView):
+    queryset = VolumeGroup.objects.all()
+    default_return_url = "plugins:netbox_storage:volumegroup_list"
+
+
+class VolumeGroupBulkImportView(generic.BulkImportView):
+    queryset = VolumeGroup.objects.all()
+    model_form = VolumeGroupImportForm
+    table = VolumeGroupTable
+    default_return_url = "plugins:netbox_storage:volumegroup_list"
+
+
+class VolumeGroupBulkEditView(generic.BulkEditView):
+    queryset = VolumeGroup.objects.all()
+    filterset = VolumeGroupFilter
+    table = VolumeGroupTable
+    form = VolumeGroupBulkEditForm
+
+
+class VolumeGroupBulkDeleteView(generic.BulkDeleteView):
+    queryset = VolumeGroup.objects.all()
+    table = VolumeGroupTable
+
+
+@register_model_view(VolumeGroup, "physicalvolumes")
+class VolumeGroupPhysicalVolumeListView(generic.ObjectChildrenView):
+    queryset = VolumeGroup.objects.all()
+    child_model = PhysicalVolume
+    table = PhysicalVolumeTable
+    filterset = PhysicalVolumeFilter
+    template_name = "netbox_storage/volumegroup/physicalvolume.html"
+    hide_if_empty = False
+
+    tab = ViewTab(
+        label="Physical Volumes",
+        badge=lambda obj: obj.physical_volume_count(),
+        hide_if_empty=False,
+    )
+
+    def get_children(self, request, parent):
+        return PhysicalVolume.objects.filter(
+            vg=parent
+        )
---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.432.tar.gz/netbox_storage-0.0.0.0.432/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['netbox_storage',
+ 'netbox_storage.api',
+ 'netbox_storage.filters',
+ 'netbox_storage.forms',
+ 'netbox_storage.migrations',
+ 'netbox_storage.tables',
+ 'netbox_storage.views']
+
+package_data = \
+{'': ['*'],
+ 'netbox_storage': ['templates/netbox_storage/*',
+                    'templates/netbox_storage/drive/*',
+                    'templates/netbox_storage/inc/*',
+                    'templates/netbox_storage/partition/*',
+                    'templates/netbox_storage/physicalvolume/*',
+                    'templates/netbox_storage/volumegroup/*']}
+
+setup_kwargs = {
+    'name': 'netbox-storage',
+    'version': '0.0.0.0.432',
+    'description': 'Netbox Storage Plugin',
+    'long_description': '### Directory structure\n\n```\n+- api - The API type\n+- filters - Filters of the models\n+- forms - The ModelForm, ModelFilterForm, ModelImportForm, ModelBulkEditForm\n+- migrations - DB Django Migration\n+- tables - The ModelBaseTable, ModelTable, RelatedModelTable\n+- templates\n  +- netbox_disk - The detail view of each model\n    +- disk - The template content box in the Virtual Machine Model\n+- views - PhysicalvolumeListView, PhysicalvolumeView, PhysicalvolumeEditView, PhysicalvolumeDeleteView, \n           PhysicalvolumeBulkImportView, PhysicalvolumeBulkEditView, PhysicalvolumeBulkDeleteView\n```\n### Models\n#### ERM\n\n\n#### Drive\nThe drive has 4 parameter:\n\n| Name           |           Example Value           |\n|:---------------|:---------------------------------:|\n| Virtualmachine | test-vm (Link zu virtual machine) |\n| Identifer      |           Festplatte 1            |\n| Cluster        |   STOR2000000 (Link zu cluster)   |\n| Size           |               50GB                |\n| System         |                No                 |\n\n#### Filesystem\nThe filesystem has 1 parameter:\n\n| Name | Example Value |\n|:-----|:-------------:|\n| fs   |     EXT4      |\n\n#### Linux Volume\nThe linux volume has 1 parameter:\n\n| Name            |       Example Value       |\n|:----------------|:-------------------------:|\n| vg_name         |          docker           |\n| lv_name         |          docker           |\n| Path            |      /var/lib/docker      |\n| Filesystem      | NTFS (Link zu Filesystem) |\n\n\n#### Windows Volume\nThe linux volume has 1 parameter:\n\n| Name       |        Example Value        |\n|:-----------|:---------------------------:|\n| drive_name |          (D, E, F)          |\n| Filesystem |  NTFS (Link zu Filesystem)  |\n\n\n\nBasis:\n- pv:\n  - size\n  - Storage Cluster\n  - virtual_machine\n\nWindows Form:\n- drive_name (D, E, F)\n- filesystem (ntfs)\n\nLinux Form:\n- vg name\n- lv name\n- path\n- filesystem\n\n\nExtra Filesystem Model & als ChoiceField ausgeben\n\n# Build\npoetry publish --build\ndocker-compose build --no-cache && docker-compose build --no-cache && docker-compose up -d\n\n<a href="{% url \'plugins:netbox_storage:physicalvolume_add\' %}?drive={{ object.id }}&pv_name={{ object.device_name }}&return_url={{ object.get_absolute_url }}" class="btn btn-sm btn-primary" role="button">\n    <i class="mdi mdi-plus-thick"></i> Add Physical Volume\n</a>\n\ngit add . && git commit -m "0.0.0.0.272" && git push\n\n\n[\n  [\n    Drive, Partition[]\n  ], \n  [\n    Drive, Partition[]\n  ]\n]',
+    'author': 'Tim Rhomberg',
+    'author_email': 'timrhomberg@hotmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
