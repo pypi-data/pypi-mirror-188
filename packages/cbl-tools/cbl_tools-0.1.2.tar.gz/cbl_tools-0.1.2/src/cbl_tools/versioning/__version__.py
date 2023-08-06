cb59a985eece9d7a39c36201b32431f108377d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:53 +0000

    Add package cbl-tools/0.1.2/cbl_tools-0.1.2.tar.gz

---
+++ packages/cbl-tools/cbl_tools-0.1.2.tar.gz/cbl_tools-0.1.2/src/cbl_tools/versioning/__version__.py
@@ -0,0 +1,4 @@
+# info@codebasedlearning.dev, a.voss@fh-aachen.de
+
+__version__ = "0.1.2"  # package version, also dynamically used by pyproject.toml
+
---
+++ packages/cbl-tools/cbl_tools-0.1.2.tar.gz/cbl_tools-0.1.2/tests/cbl_tools/info/test_info.py
@@ -0,0 +1,14 @@
+# info@codebasedlearning.dev, a.voss@fh-aachen.de
+
+import unittest
+
+from cbl_tools.snippetinfo import intro, with_intro, note
+
+
+class SnippetInfoTestCase(unittest.TestCase):
+    def test_info(self):
+        print("huhu test info")
+
+
+if __name__ == '__main__':
+    unittest.main()
